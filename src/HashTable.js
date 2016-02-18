9da4b14e089020ec075fc73f30c6b329824a082d
davidpetri <petridw@gmail.com>
2016-02-18 14:23:34 -0800

    (feat) comments

--- src/HashTable.js
+++ src/HashTable.js
@@ -1,3 +1,10 @@
+/**
+* HashTable costructor
+*
+* construct a new hash table
+*
+* - You may modify this constructor as you need to achieve the challenges below.
+*/
 function HashTable() {
   this.SIZE = 16;
   
@@ -5,7 +12,14 @@ function HashTable() {
 }
 
 /**
-* set - Adds given value to the hash table with specified key
+* set - Adds given value to the hash table with specified key.
+*
+* - If the provided key has already been used to store another value, simply overwrite
+*   the existing value with the new value.
+* - If the hashed address already contains another key/value pair, you must handle
+*   the collision appropriately.
+* - If adding the new item will push the number of stored items to over 75% of
+*   the hash table's SIZE, then double the hash table's SIZE and rehash everything
 *
 * @param {string} key - key to be used to create hashed address
 * @param {string|number|boolean} value - value to be stored in hash table
@@ -18,6 +32,9 @@ HashTable.prototype.set = function(key, value) {
 /**
 * get - Retrieves a value stored in the hash table with a specified key
 *
+* - If more than one value is stored at the key's hashed address, then you must retrieve
+*   the correct value that was originally stored with the provided key
+*
 * @param {string} key - key to lookup in hash table
 * @return {string|number|boolean} The value stored with the specifed key in the
 * hash table
@@ -27,10 +44,15 @@ HashTable.prototype.get = function(key) {
 };
 
 /**
-* remove - Remove the key/value pair at the specified key in the hash table
+* remove - delete a key/value pair from the hash table
+*
+* - If the key does not exist in the hash table, return undefined
+* - If the result of removing the item drops the number of stored items to under
+*   25% of the hash table's SIZE, then reduce the hash table's SIZE by 1/2 and rehash
+*   everything
 *
 * @param {string} key - key to be found and deleted in hash table
-* @return {number} The new number of items stored in the hash table
+* @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
 
