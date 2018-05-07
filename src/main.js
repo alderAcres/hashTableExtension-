9330435cb8ffe53ddf4c93d1b5ae1a014463b39a
Shahrukh <jalilshahrukh@gmail.com>
2018-05-07 10:15:23 -0400

    Assessment 1.

--- src/main.js
+++ src/main.js
@@ -23,8 +23,31 @@ function HashTable() {
 * @param {string|number|boolean} value - value to be stored in hash table
 * @return {number} The new number of items stored in the hash table
 */
-HashTable.prototype.set = function(key, value) {
 
+//Not very performative, was attempting to implement an Object or Linked List as bucket
+//Can past test cases by indexing as an array
+//var item = [key, value]; 
+//this.storage[index] = item;
+HashTable.prototype.set = function(key, value) {
+  var index = new hashCode(key, this.SIZE); //index to store key value at
+  var item = [key, value]; //key value pair to be pushed to bucket
+  var bucket = this.storage[index]; //bucket with possibile index of key value pair
+
+  //var bucket = {}; 
+  //bucket[key] = value;
+
+  if (bucket) { 
+    for (let i = 0; i < bucket.length; i++) { 
+      if (bucket[i][0] === key) { 
+        bucket[i][1] = value; //if the key exists, set value with corresponding key
+      } else { 
+        bucket[key] = value; //otherwise set key value pair in bucket
+      }//end if
+    }//end for 
+  } else { 
+    bucket = item; //add item to new bucket
+    this.storage[index] = bucket; //store bucket at specificed index
+  }//end if else
 };
 
 /**
@@ -38,7 +61,16 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
+  var index = new hashCode(key, this.SIZE); //index with possible key
+  var bucket = this.storage[index]; //possible bucket that contains key
 
+  if (bucket) { 
+    for (let i = 0; i < bucket.length; i++) { 
+      if (bucket[i][0] === key) { 
+        return bucket[i][1]; //loops through the indexes of the bucket if key matches return that value
+      }//end if
+    }//end for
+  }//end if
 };
 
 /**
@@ -50,7 +82,25 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
+  var index = new hashCode(key, this.SIZE); //index with possible key
+  var bucket = this.storage[index]; //possible bucket that contains key
 
+  if (bucket) { 
+    for (let i = 0; i < bucket.length; i++) { 
+      if (bucket[i][0] === key) {
+        while (bucket[i] !== bucket.length) { //loop through from key index to end of bucket length
+          bucket[i][0] = bucket[i-1][0]; //shift should override current key
+          bucket[i][1] = bucket[i-1][0]; //shift should overrid current value
+          i+=1;//increment till bucket length
+        }//end while
+        delete bucket[arr.length][0]; //delete last key which would be duplicate of previous from shift
+        delete bucket[arr.length][1]; //delete last value which would be duplicate of previous from shift
+        break; //no need to loop through rest
+      }//end if 
+    }//end for
+  } else { 
+    return undefined; //if not found
+  }//end if else
 };
 
 
