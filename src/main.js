00559137048182b74f63a17fba3660fa3c217ca8
Clariz Mariano <clariz.mariano@gmail.com>
2018-06-11 10:16:30 -0700

    known issue: 3rd collission is still broken

--- src/main.js
+++ src/main.js
@@ -10,6 +10,12 @@ function HashTable() {
   
   this.storage = new Array(this.SIZE);
 }
+function LinkedList(key, value) {
+  this[key] = value;
+  console.log(LinkedList, this[key], value, "inside LL creation");
+  this.tail = null;
+}
+
 
 /**
 * set - Adds given value to the hash table with specified key.
@@ -24,6 +30,36 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
+  console.log(value, "ADD THIS");
+  // KNOWN ISSUE: adding a third value to the LL does not work yet :(
+  const convertedHash = hashCode(key, this.SIZE);
+
+// create new HastTable 
+  if (!this.storage[0]){
+    this.storage = new Array(this.SIZE);   
+    let newNode = new LinkedList(key, value);
+    this.storage[hashCode(key, this.SIZE)] = newNode;
+    return;
+  }
+
+  
+  // creates a LinkedList if index from hashcode is empty
+  // set the value to the index of the array, by using the hashCode
+  
+  
+  
+  // insert the key/pair values to LL if hashCode index is taken (to prevent collissions)
+  // add an LL, if tail is empty
+  while (!this.storage[convertedHash].tail){
+    // tail becomes new node
+    console.log(this.storage[convertedHash].tail, key, value);
+    this.storage[convertedHash].tail =  new LinkedList(key, value);
+    console.log(this.storage[convertedHash].tail, "THIS IS THE TAIL");
+    console.log(this.storage[convertedHash].tail.tail, "THIS IS THE TAIL'S TAIL");
+    
+    return;
+
+  } 
 
 };
 
@@ -38,6 +74,20 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
+  // in case of value has not been stored in the hash table yet
+  // console.log("in get");
+  const convertedHash = hashCode(key, this.SIZE);
+  if (!this.storage[convertedHash]) {
+    // console.log("Not found");
+    return
+  }
+  // base condition if key === to the this.key inside the LL
+  let finder = this.storage[convertedHash];
+
+  console.log(finder);
+  if (this.storage[convertedHash]) {
+    
+  }
 
 };
 
@@ -72,3 +122,10 @@ function hashCode(string, size) {
 
 // Do not remove!!
 module.exports = HashTable;
+
+let newArray = new HashTable;
+newArray.set("keystring", "111");
+newArray.set("keystring", "22222");
+newArray.set("keystring", "33333");
+newArray.get("keystring")
+console.log("FINAL VALUE", newArray);
