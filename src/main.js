9788e6392aed45e27271fc97fe9f9c1423f12524
Alex Mannix <alexleemannix@gmail.com>
2018-06-25 10:19:48 -0400

    complete Week 1 Assessment

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@
 * - You may modify this constructor as you need to achieve the challenges below.
 */
 function HashTable() {
-  this.SIZE = 16;
-  
+  this.SIZE = 3;
+
   this.storage = new Array(this.SIZE);
 }
 
@@ -24,8 +24,16 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
+let index = hashCode(key, this.SIZE)
+if (this.storage[index] === undefined){
+  this.storage[index] = {}
+  this.storage[index][key] = value
+} else {
+  this.storage[index][key] = value
+}
 
 };
+
 
 /**
 * get - Retrieves a value stored in the hash table with a specified key
@@ -38,8 +46,11 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+let index = hashCode(key, this.SIZE)
+return this.storage[index][key]
 };
+
+
 
 /**
 * remove - delete a key/value pair from the hash table
@@ -50,23 +61,32 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
+let index = hashCode(key, this.SIZE)
+if(!this.storage[index][key]) return undefined
+else {
+let removeEle = this.storage[index][key]
+delete this.storage[index]
+return removeEle
+}
 
 };
+
+
 
 
 // Do not modify
 function hashCode(string, size) {
   'use strict';
-  
+
   let hash = 0;
   if (string.length === 0) return hash;
-  
+
   for (let i = 0; i < string.length; i++) {
     const letter = string.charCodeAt(i);
     hash = ((hash << 5) - hash) + letter;
     hash = hash & hash; // Convert to 32bit integer
   }
-  
+
   return Math.abs(hash) % size;
 }
 
