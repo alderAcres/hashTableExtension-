e7ef838854ea74e18123d3adeb5a6b3e4540bdc6
andwong91 <andwong91@gmail.com>
2018-07-30 10:14:04 -0700

    Completed Week 1 Assessment and Extensions

--- src/extension.js
+++ src/extension.js
@@ -14,8 +14,117 @@
 */
 
 // PASTE AND MODIFY YOUR CODE BELOW
+function HashTable() {
+  this.SIZE = 16;
+  this.stored = 0;
+  this.storage = new Array(this.SIZE);
+}
 
+/**
+* set - Adds given value to the hash table with specified key.
+*
+* - If the provided key has already been used to store another value, simply overwrite
+*   the existing value with the new value.
+* - If the hashed address already contains another key/value pair, you must handle
+*   the collision appropriately.
+*
+* @param {string} key - key to be used to create hashed address
+* @param {string|number|boolean} value - value to be stored in hash table
+* @return {number} The new number of items stored in the hash table
+*/
+HashTable.prototype.set = function(key, value) {
+  const hash = hashCode(key, this.SIZE);
+  if (this.storage[hash] === undefined) {
+    const addObj = {};
+    addObj[key] = value;
+    this.storage[hash] = addObj;
+  } else {
+    this.storage[hash][key] = value;
+  }
+  this.stored++;
+  if (this.stored / this.SIZE > 0.75) {
+    const newStorage = [];
+    const rehashArr = JSON.parse(JSON.stringify(this.storage));
+    for (let i = 0; i < this.SIZE * 2; i++) {
+      newStorage.push(undefined);
+    }
+    this.SIZE *= 2;
+    rehashArr.forEach(obj => {
+      if (typeof obj === 'object') {
+        if (obj) {
+          for (let key in obj) {
+            let rehash = hashCode(key, this.SIZE);
+            if (newStorage[rehash] === undefined) {
+              const addObj = {};
+              addObj[key] = obj[key];
+              newStorage[rehash] = addObj;
+            } else {
+              newStorage[rehash][key] = obj[key];
+            }
+          }
+        }
+      }
+    });
+    this.storage = newStorage;
+  }
+  return this.stored;
+};
 
+/**
+* get - Retrieves a value stored in the hash table with a specified key
+*
+* - If more than one value is stored at the key's hashed address, then you must retrieve
+*   the correct value that was originally stored with the provided key
+*
+* @param {string} key - key to lookup in hash table
+* @return {string|number|boolean} The value stored with the specifed key in the
+* hash table
+*/
+HashTable.prototype.get = function(key) {
+  const hash = hashCode(key, this.SIZE);
+  return this.storage[hash][key];
+};
+
+/**
+* remove - delete a key/value pair from the hash table
+*
+* - If the key does not exist in the hash table, return undefined
+*
+* @param {string} key - key to be found and deleted in hash table
+* @return {string|number|boolean} The value deleted from the hash table
+*/
+HashTable.prototype.remove = function(key) {
+  const hash = hashCode(key, this.SIZE);
+  const returnVal = this.storage[hash][key];
+  delete this.storage[hash][key];
+  if (Object.keys(this.storage[hash]).length === 0) this.stored--;
+  if (this.SIZE > 16 && this.stored / this.SIZE < 0.25) {
+    const newStorage = [];
+    const rehashArr = JSON.parse(JSON.stringify(this.storage));
+    for (let i = 0; i < this.SIZE / 2; i++) {
+      newStorage.push(undefined);
+    }
+    this.SIZE /= 2;
+    rehashArr.forEach(obj => {
+      if (typeof obj === 'object') {
+        if (obj) {
+          for (let key in obj) {
+            let rehash = hashCode(key, this.SIZE);
+            if (newStorage[rehash] === undefined) {
+              const addObj = {};
+              addObj[key] = obj[key];
+              newStorage[rehash] = addObj;
+            } else {
+              newStorage[rehash][key] = obj[key];
+            }
+          }
+        }
+      }
+    });
+    this.storage = newStorage;
+  }
+  return returnVal;
+};
 
 // YOUR CODE ABOVE
 
