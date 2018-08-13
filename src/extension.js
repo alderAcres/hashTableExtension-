c7f8b98eb19350956c639a04a1170a193596a747
Ryan Vazquez <ryanvazquez@Ryans-MacBook-Pro.local>
2018-08-13 10:14:01 -0400

    main complete, resize functionality incomplete

--- src/extension.js
+++ src/extension.js
@@ -15,6 +15,105 @@
 
 // PASTE AND MODIFY YOUR CODE BELOW
 
+function HashTable() {
+  this.SIZE = 16;
+  this.storage = new Array(this.SIZE);
+}
+
+HashTable.prototype.set = function(key, value) {
+  const hash = hashCode(key, this.SIZE);
+
+  //check if location exists
+  if(this.storage[hash]) {
+
+    //check if any objects at location matches the key
+    const i = this.storage[hash].findIndex(function(obj){
+      return obj.hasOwnProperty(key);
+    })
+    //if found, override the obj
+    if(i >= 0) {
+      this.storage[hash][i] = {[key]: value};
+    //if not found, push key/value to array
+    } else {
+      this.storage[hash].push({[key]: value})
+    }
+  } else {
+    //check the size of storage [do undefined values count toward size??]
+    const count = this.storage[hash].reduce(function(count, location){
+      return Array.isArray(location) ? count++ : count;
+    }, 0);
+
+    const percent = this.SIZE / count * 100;
+    if(percent > 75) {
+      //get all the stored objects
+      const objs = this.storage
+      //resize
+      const newArray = new Array(this.SIZE * 2);
+
+    } else {
+      this.storage[hash] = [ {[key]: value} ];
+    }
+  }
+};
+
+HashTable.prototype.get = function(key) {
+  const hash = hashCode(key, this.SIZE);
+  if (this.storage[hash]) {
+    const i = this.storage[hash].findIndex(function(obj){
+      return obj.hasOwnProperty(key);
+    })
+    if (i >= 0) {
+      return this.storage[hash][i][key];
+    }
+    return undefined
+  }
+  return undefined
+};
+
+HashTable.prototype.remove = function(key) {
+  const hash = hashCode(key, this.SIZE);
+  
+  // check if hash exists
+  if(this.storage[hash]) {
+    const i = this.storage[hash].findIndex(function(obj){
+      return obj.hasOwnProperty(key);
+    })
+    //check if obj exists at hash location
+    if(i >= 0) {
+
+      //remove obj. mutates the array
+      this.storage[hash].splice(i, 1);
+
+      //check if array is empty. If so, set it to undefined
+      if (this.storage[hash].length === 0) {
+        this.storage[hash] = undefined;
+      }
+    }
+    return undefined
+  }
+  return undefined
+};
+
+
+// Do not modify
+function hashCode(string, size) {
+  'use strict';
+  
+  let hash = 0;
+  if (string.length === 0) return hash;
+  
+  for (let i = 0; i < string.length; i++) {
+    const letter = string.charCodeAt(i);
+    hash = ((hash << 5) - hash) + letter;
+    hash = hash & hash; // Convert to 32bit integer
+  }
+  
+  return Math.abs(hash) % size;
+}
+
+// Do not remove!!
+module.exports = HashTable;
+
 
 
 // YOUR CODE ABOVE
