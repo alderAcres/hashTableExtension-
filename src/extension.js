abd9a14f40cad48ac275ff4f659e31300f8a7ef6
g-es <gesun@usc.edu>
2018-09-17 10:15:51 -0700

    week-1-assessment

--- src/extension.js
+++ src/extension.js
@@ -15,6 +15,11 @@
 
 // PASTE AND MODIFY YOUR CODE BELOW
 
+function HashTable() {
+  this.SIZE = 16;
+  this.storage = new Array(this.SIZE);
+  this.mySize = 0;
+}
 
 
 // YOUR CODE ABOVE
@@ -33,6 +38,97 @@ function hashCode(string, size) {
   
   return Math.abs(hash) % size;
 }
+
+
+HashTable.prototype.set = function set(key, value) {
+  const realKey = hashCode(key, this.SIZE);
+  if (this.storage[realKey] === undefined) {
+    this.storage[realKey] = {};
+    this.storage[realKey][key] = value;
+    this.mySize++;
+  } else {
+    this.storage[realKey][key] = value;
+    this.mySize++;
+    // console.log(this.mySize, this.SIZE)
+  }
+  if (this.mySize >= 0.75 * this.SIZE) {
+    this.rehash();
+  }
+};
+
+// something wrong with get? mySize???
+HashTable.prototype.get = function get(key) {
+  const realK = hashCode(key, this.SIZE);
+  let cache;
+  // console.log(this.mySize, this.SIZE)  
+  if (this.storage[realK] === undefined) return undefined;
+  if (this.storage[realK][key] !== undefined) {
+    this.mySize--;
+    cache = this.storage[realK][key];
+  }
+  if (this.mySize <= 0.25 * this.SIZE) {
+    this.rehash2();
+  }
+  return cache;
+};
+
+
+HashTable.prototype.rehash = function rehash() {
+  this.SIZE *= 2;
+  this.mySize = 0;
+  const originalStorage = this.storage;
+  this.storage = new Array(this.SIZE);
+  // put all the key-value pairs in original storage [{},{},{},{xx:1,yy:2}] into new empty storage, using new realHashKeys;
+  originalStorage.forEach((obj) => {
+    for (let key in obj) {
+      const newK = hashCode(key, this.SIZE);
+      if (this.storage[newK] === undefined) {
+        this.storage[newK] = {};
+        this.storage[newK][key] = obj[key];
+        this.mySize++;
+      } else {
+        this.storage[newK][key] = obj[key];
+        this.mySize++;
+      }
+    }
+  });
+}
+
+// not working
+HashTable.prototype.rehash2 = function rehash2() {
+  this.SIZE /= 2;
+  const originalStorage = this.storage;
+  this.storage = new Array(this.SIZE);
+  this.mySize = 0;
+  originalStorage.forEach((obj) => {
+    for (let key in obj) {
+      const newK = hashCode(key, this.SIZE);
+      if (this.storage[newK] === undefined) {
+        this.storage[newK] = {};
+        this.storage[newK][key] = obj[key];
+        this.mySize++;
+      } else {
+        this.storage[newK][key] = obj[key];
+        this.mySize++;
+      }
+    }
+  });
+}
+
+
+const hashTable = new HashTable();
+for (let i = 0; i < 13; i++) {
+  const key = 'key ' + i;
+  const value = 'value ' + i;
+  hashTable.set(key, value);
+}
+// push 12 el, size became 32, minus 4 , 8 
+for (let i = 0; i < 5; i++) {
+  const key = 'key ' + i;
+  hashTable.get(key);
+}
+console.log(hashTable)
+console.log(hashTable.SIZE)
 
 // Do not remove!!
 module.exports = HashTable;
