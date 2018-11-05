86db875f09682f751e13c9fc89c3d76fcf2494f5
chrisfranz <chrisfranz@mac.com>
2018-11-05 10:12:26 -0800

    completed

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ function HashTable() {
   this.SIZE = 16;
   
   this.storage = new Array(this.SIZE);
+  this.items = 0;
 }
 
 /**
@@ -24,7 +25,25 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
+  // if key or value aren't provided as arguments, return undefined
+  if (!key || !value) return undefined;
+  // if invalid arguments are passed return undefined
+  if (typeof key !== 'string' || (typeof value !== 'string' && typeof value !== 'number' && typeof value !== 'boolean')) return undefined;
+  // generate index from hashCode, store as constant index
+  const index = hashCode(key, this.SIZE);
+  // check if element exists at generated index
+  if (!this.storage[index]) {
+    // if no element exists, assign empty object to index
+    this.storage[index] = {};
+  }
+  // if assignment will not overwrite a previously stored value, increment items by one
+  if (!this.storage[index][key]) {
+    this.items += 1;
+  }
+  // populate object at generated index with key/value arguments
+  this.storage[index][key] = value;
 
+  return this.items
 };
 
 /**
@@ -38,7 +57,12 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  // if key not provided, or argument passed not a string, return undefined
+  if (!key || typeof key !== 'string') return undefined;
+  // get index from hash function, store on constant index
+  const index = hashCode(key, this.SIZE);
+  // check if key exists at generated index. if true, return value. if not, return undefined
+  return (this.storage[index]) ? this.storage[index][key] : undefined;
 };
 
 /**
@@ -50,7 +74,20 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
-
+  // if key not provided, or argument passed not a string, return undefined
+  if (!key || typeof key !== 'string') return undefined;
+  // get index from hash function
+  const index = hashCode(key, this.SIZE);
+  // store the value to be removed. if value doesn't exist, output will evaluate to undefined
+  const output = this.storage[index][key];
+  // if item exists, decrement items
+  if (output) {
+    this.items -= 1;
+  }
+  // remove value
+  delete this.storage[index][key];
+  // return value
+  return output;
 };
 
 
