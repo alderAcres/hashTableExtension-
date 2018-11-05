2a59cd824d5927bf95fc4d88f7f9a9cd614e4d80
Adrian <34041016+Sgshi@users.noreply.github.com>
2018-11-05 10:16:01 -0800

    working on finishing expanding hashtable

--- src/extension.js
+++ src/extension.js
@@ -14,10 +14,47 @@
 */
 
 // PASTE AND MODIFY YOUR CODE BELOW
+function HashTable() {
+  this.SIZE = 2;
+  
+  this.storage = new Array(this.SIZE);
+}
 
+HashTable.prototype.set = function(key, value) {
+  const tooBig = this.storage.length === 0 ? 0 : this.storage.reduce((a,b)=>{return b?a+=Object.keys(b).length:a;},0)
+  if(tooBig > Math.floor(this.SIZE *.75)){
+    this.SIZE += this.SIZE;
+    const newArr = [];
 
+    //for each element in array, if it is object, for each element of object, add to code through hash
+    this.storage.forEach((e)=>{
+      if(e){
+        Object.keys(e).forEach((el)=>{
+          console.log(el, e[el]);
+          newArr[hashCode(el,this.SIZE)] = { el : e[el]};
+        })
+      }
+    })
+    this.storage = newArr;
+  }
+  const position = hashCode(key, this.SIZE);
+  if(!this.storage[position]){this.storage[position] = {}};
+  this.storage[position][key] = value;
+};
 
-// YOUR CODE ABOVE
+HashTable.prototype.get = function(key) {
+  return this.storage[hashCode(key,this.size)][key];
+};
+
+HashTable.prototype.remove = function(key) {
+  if(this.storage.length === 0){
+    return undefined;
+  }
+  const position = hashCode(key, this.size);
+  const output = this.storage[position][key];
+  delete this.storage[position][key];
+  return output;
+};
 
 function hashCode(string, size) {
   'use strict';
@@ -36,3 +73,14 @@ function hashCode(string, size) {
 
 // Do not remove!!
 module.exports = HashTable;
+
+const HT = new HashTable();
+HT.set("butt",1);
+HT.set("batt",7);
+console.log(HT.storage);
+HT.set("boot",5);
+HT.set("bott",4);
+console.log(HT.storage);
+HT.set("bart",3);
+HT.set("bunt",2);
+console.log(HT.storage);
