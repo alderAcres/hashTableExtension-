358e21aae842dcb9fc36a969c81488277ea2aa42
GeneTheBean <benesitis@gmail.com>
2018-11-19 10:16:11 -0500

    finish

--- src/main.js
+++ src/main.js
@@ -1,74 +1,109 @@
-/**
-* HashTable costructor
-*
-* construct a new hash table
-*
-* - You may modify this constructor as you need to achieve the challenges below.
-*/
 function HashTable() {
   this.SIZE = 16;
-  
+  this.items = 0;
   this.storage = new Array(this.SIZE);
 }
 
-/**
-* set - Adds given value to the hash table with specified key.
-*
-* - If the provided key has already been used to store another value, simply overwrite
-*   the existing value with the new value.
-* - If the hashed address already contains another key/value pair, you must handle
-*   the collision appropriately.
-*
-* @param {string} key - key to be used to create hashed address
-* @param {string|number|boolean} value - value to be stored in hash table
-* @return {number} The new number of items stored in the hash table
-*/
 HashTable.prototype.set = function(key, value) {
+  const index = hashCode(key, this.SIZE);
 
+  if (this.storage[index] === undefined) {
+    this.storage[index] = [];
+  }
+  this.storage[index].push([key, value]);
+  this.items++;
+  this.checkCapacity();
 };
 
-/**
-* get - Retrieves a value stored in the hash table with a specified key
-*
-* - If more than one value is stored at the key's hashed address, then you must retrieve
-*   the correct value that was originally stored with the provided key
-*
-* @param {string} key - key to lookup in hash table
-* @return {string|number|boolean} The value stored with the specifed key in the
-* hash table
-*/
+HashTable.prototype.checkCapacity = function() {
+  if (this.items >= 0.75 * this.SIZE) {
+    const copy = this.storage;
+    this.SIZE *= 2;
+    this.storage = new Array(this.SIZE);
+
+    for (let i = 0; i < copy.length; i++) {
+      if (copy[i] !== undefined) {
+        for (let j = 0; j < copy[i].length; j++) {
+          this.rehash(copy[i][j][0], copy[i][j][1]);
+        }
+      }
+    }
+  } else if (this.SIZE > 16 && this.items <= 0.25 * this.SIZE) {
+    const copy = this.storage;
+    this.SIZE /= 2;
+    this.storage = new Array(this.SIZE);
+
+    for (let i = 0; i < copy.length; i++) {
+      if (copy[i] !== undefined) {
+        for (let j = 0; j < copy[i].length; j++) {
+          this.rehash(copy[i][j][0], copy[i][j][1]);
+        }
+      }
+    }
+  }
+};
+
+HashTable.prototype.rehash = function(key, value) {
+  const index = hashCode(key, this.SIZE);
+  if (this.storage[index] === undefined) {
+    this.storage[index] = [];
+  }
+  this.storage[index].push([key, value]);
+};
+
 HashTable.prototype.get = function(key) {
+  const index = hashCode(key, this.SIZE);
 
+  if (this.storage[index] !== undefined) {
+    for (let i = 0; i < this.storage[index].length; i++) {
+      if (this.storage[index][i][0] === key) {
+        return this.storage[index][i][1];
+      }
+    }
+  }
 };
 
-/**
-* remove - delete a key/value pair from the hash table
-*
-* - If the key does not exist in the hash table, return undefined
-*
-* @param {string} key - key to be found and deleted in hash table
-* @return {string|number|boolean} The value deleted from the hash table
-*/
 HashTable.prototype.remove = function(key) {
+  const index = hashCode(key, this.SIZE);
+  if (this.storage[index] !== undefined) {
+    for (let i = 0; i < this.storage[index].length; i++) {
+      if (this.storage[index][i][0] === key) {
+        const value = this.storage[index][i][1];
+        this.storage[index].splice(i, 1);
+        this.items--;
+        if (this.storage[index].length === 0) {
+          this.storage[index] = undefined;
+        }
+        this.checkCapacity();
 
+        return value;
+      }
+    }
+  }
 };
 
-
-// Do not modify
 function hashCode(string, size) {
   'use strict';
-  
+
   let hash = 0;
   if (string.length === 0) return hash;
-  
+
   for (let i = 0; i < string.length; i++) {
-    const letter = string.charCodeAt(i);
-    hash = ((hash << 5) - hash) + letter;
+    const letter = String.charCodeAt(i);
+    hash = (hash << 5) - hash + letter;
     hash = hash & hash; // Convert to 32bit integer
   }
-  
+
   return Math.abs(hash) % size;
 }
 
-// Do not remove!!
-module.exports = HashTable;
+var ht = new HashTable();
+
+for (let i = 0; i < 23; i++) {
+  ht.set(i,i);
+}
+
+for (let i = 0; i < 23; i++) {
+  ht.remove(i,i);
+}
+console.log(ht.storage);
