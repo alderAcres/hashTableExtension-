3160645612e6ba5fa05f1492bb446e224b0fa4b7
howiekick <howardna317@gmail.com>
2018-12-21 10:05:07 -0800

    attempted extension

--- src/extension.js
+++ src/extension.js
@@ -14,6 +14,99 @@
 */
 
 // PASTE AND MODIFY YOUR CODE BELOW
+/**
+* HashTable costructor
+*
+* construct a new hash table
+*
+* - You may modify this constructor as you need to achieve the challenges below.
+*/
+function HashTable() {
+  this.SIZE = 16;
+
+  this.storage = new Array(this.SIZE);
+
+  this.occupied = 0;
+}
+
+/**
+* set - Adds given value to the hash table with specified key.
+*
+* - If the provided key has already been used to store another value, simply overwrite
+*   the existing value with the new value.
+* - If the hashed address already contains another key/value pair, you must handle
+*   the collision appropriately.
+*
+* @param {string} key - key to be used to create hashed address
+* @param {string|number|boolean} value - value to be stored in hash table
+* @return {number} The new number of items stored in the hash table
+*/
+HashTable.prototype.set = function(key, value) {
+  let hash = hashCode(key, this.SIZE)
+  let obj = this.storage[hash] = {}
+
+  let over75 = this.SIZE * 0.75;
+  
+
+  // if stored items go over 75% of hash table size ... then run code below
+  if (this.occupied >= over75) {
+    // put all existing objs (which are key/value pairs) into an array
+    var tempStorage = [];
+
+    this.storage.forEach(el => {
+      if (el !== undefined) {
+        tempStorage.push(el);
+      }
+    })
+    //double hash table size
+    this.SIZE = this.SIZE * 2;
+    this.storage = new Array(this.SIZE);
+    
+    //rehash everything
+    tempStorage.forEach(el => {
+      this.set(el[key], el[value]);
+    })
+  }
+  else {
+    obj[key] = value;
+    this.occupied += 1;
+  }
+};
+
+
+/**
+* get - Retrieves a value stored in the hash table with a specified key
+*
+* - If more than one value is stored at the key's hashed address, then you must retrieve
+*   the correct value that was originally stored with the provided key
+*
+* @param {string} key - key to lookup in hash table
+* @return {string|number|boolean} The value stored with the specifed key in the
+* hash table
+*/
+HashTable.prototype.get = function(key) {
+  let hash = hashCode(key, this.SIZE);
+
+  return this.storage[hash][key];
+};
+/**
+* remove - delete a key/value pair from the hash table
+*
+* - If the key does not exist in the hash table, return undefined
+*
+* @param {string} key - key to be found and deleted in hash table
+* @return {string|number|boolean} The value deleted from the hash table
+*/
+HashTable.prototype.remove = function(key) {
+  let hash = hashCode(key, this.SIZE);
+
+  if (this.storage[hash] === undefined) {
+    return undefined;
+  }
+  else if (this.storage[hash][key]) {
+    delete this.storage[hash][key];
+  } 
+};
 
 
 
