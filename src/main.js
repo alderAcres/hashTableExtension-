94f26f20816b22d85098285bbbe6978e06f4ba46
joel burton <joeltburton@gmail.com>
2018-12-21 10:20:59 -0800

    implemented set, get, remove in main.js. Wasn't able to refactor set to extend the table. Caught up on conditional maybe?

--- src/main.js
+++ src/main.js
@@ -5,9 +5,10 @@
 *
 * - You may modify this constructor as you need to achieve the challenges below.
 */
+/* eslint-disable func-names, no-use-before-define */
 function HashTable() {
   this.SIZE = 16;
-  
+
   this.storage = new Array(this.SIZE);
 }
 
@@ -23,8 +24,17 @@ function HashTable() {
 * @param {string|number|boolean} value - value to be stored in hash table
 * @return {number} The new number of items stored in the hash table
 */
-HashTable.prototype.set = function(key, value) {
+HashTable.prototype.set = function (key, value) {
+  // generate a hash key
+  const hashKey = hashCode(key, this.SIZE);
 
+  // Set value in table
+  // Check storage at hashKey for bin
+  // if no bin, create an empty bin
+  if (!this.storage[hashKey]) this.storage[hashKey] = {};
+
+  // assign storage at hashKey, bin at key to value
+  this.storage[hashKey][key] = value;
 };
 
 /**
@@ -37,8 +47,13 @@ HashTable.prototype.set = function(key, value) {
 * @return {string|number|boolean} The value stored with the specifed key in the
 * hash table
 */
-HashTable.prototype.get = function(key) {
-
+HashTable.prototype.get = function (key) {
+  // generate the hashKey
+  const hashKey = hashCode(key, this.SIZE);
+  const bin = this.storage[hashKey];
+  if (bin) {
+    return bin[key];
+  }
 };
 
 /**
@@ -49,26 +64,50 @@ HashTable.prototype.get = function(key) {
 * @param {string} key - key to be found and deleted in hash table
 * @return {string|number|boolean} The value deleted from the hash table
 */
-HashTable.prototype.remove = function(key) {
+HashTable.prototype.remove = function (key) {
+  // generate the hashKey
+  const hashKey = hashCode(key, this.SIZE);
+  const bin = this.storage[hashKey];
+  let removed;
+  if (bin) {
+    removed = bin[key];
+    delete bin[key];
+  }
 
+  return removed;
 };
 
 
 // Do not modify
 function hashCode(string, size) {
-  'use strict';
-  
   let hash = 0;
   if (string.length === 0) return hash;
-  
+
   for (let i = 0; i < string.length; i++) {
     const letter = string.charCodeAt(i);
     hash = ((hash << 5) - hash) + letter;
-    hash = hash & hash; // Convert to 32bit integer
+    hash &= hash; // Convert to 32bit integer
   }
-  
+
   return Math.abs(hash) % size;
 }
+
+// const table = new HashTable();
+// console.log('\n||||||| set testing |||||||\n');
+// table.set(0, 'value at key0');
+// table.set('testKey', 'testValue');
+// console.log('table after set', table);
+
+// console.log('\n||||||| get testing |||||||\n');
+// console.log('get test:', table.get(0));
+// console.log('get key:', table.get('key'));
+
+
+// console.log('\n||||||| remove testing |||||||\n');
+// console.log('remove \'testKey\' from table:', table.remove(0));
+// console.log('table after remove', table);
+// console.log('try to remove a key that doesn\'t exist -> undefined', table.remove('i dont exist'));
+
 
 // Do not remove!!
 module.exports = HashTable;
