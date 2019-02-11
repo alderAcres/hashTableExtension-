4ff588bc4ef4da3fc58ebcf0ddf13b65cfc6c322
Chris Air <sdchrisn@gmail.com>
2019-02-11 09:51:59 -0800

    main source completed & tested  before trying extentsion

--- src/main.js
+++ src/main.js
@@ -24,8 +24,24 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
+  // simply add value
+  let hashkey = hashCode(key, this.SIZE);
+  let obj = this.storage[hashkey];
 
+  if(obj){
+    // console.log('collision insert', value, 'Key', key)
+    obj[key] = value;   // handle collision
+    // console.log(obj);
+  }
+  else{
+    // console.log('new insert', value, 'Key', key, 'Hashkey', hashkey);
+    let temp = {};
+    temp[key] = value;
+    this.storage[hashkey] = temp;  // empty bucket
+    // console.log('ADDED', this.storage[hashkey]);
+  }
 };
+
 
 /**
 * get - Retrieves a value stored in the hash table with a specified key
@@ -38,7 +54,14 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  let hashkey = hashCode(key, this.SIZE);
+  let bucket = this.storage[hashkey];
+  // console.log('GET bucket', bucket);
+  for(let prop in bucket){
+      if(key === prop){
+        return bucket[key];
+      }
+  }
 };
 
 /**
@@ -50,9 +73,31 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
-
+  let hashkey = hashCode(key, this.SIZE);
+  let bucket = this.storage[hashkey];
+  for(let prop in bucket){
+      if(key === prop){
+        delete bucket[prop];
+        break;
+      }
+  }
+  return undefined;
 };
 
+// ---------------- TESTING ---------------
+// let h = new HashTable();
+// // h.set('1', 'hi');
+// // h.set('2', 'he');
+// h.set('3', 'hel');
+// h.set('3x', 'hell3333');
+// h.set('5', 'hello');
+
+// console.log(h.get('3'));
+// h.remove('2');
+// console.log(h.get('3x')); // hashkey = 5
+// console.log(h.get('5'));  // hashkey = 5
+
+// ---------------- ------- ---------------
 
 // Do not modify
 function hashCode(string, size) {
