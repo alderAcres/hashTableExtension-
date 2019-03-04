9a5f4b58b7fb713d7dcb3feb614ee7342124037b
Bo Peng <pb0928@gmail.com>
2019-03-04 10:17:18 -0500

    assessment 1 done

--- src/extension.js
+++ src/extension.js
@@ -15,7 +15,132 @@
 
 // PASTE AND MODIFY YOUR CODE BELOW
 
+/**
+* HashTable costructor
+*
+* construct a new hash table
+*
+* - You may modify this constructor as you need to achieve the challenges below.
+*/
+function HashTable() {
+  this.SIZE = 16;
+  this.items = 0;
+  this.storage = new Array(this.SIZE);
+}
 
+/**
+* set - Adds given value to the hash table with specified key.
+*
+* - If the provided key has already been used to store another value, simply overwrite
+*   the existing value with the new value.
+* - If the hashed address already contains another key/value pair, you must handle
+*   the collision appropriately.
+*
+* @param {string} key - key to be used to create hashed address
+* @param {string|number|boolean} value - value to be stored in hash table
+* @return {number} The new number of items stored in the hash table
+*/
+HashTable.prototype.set = function(key, value) {
+	const hash = hashCode(key, this.SIZE);
+ 	this.items++;
+	if(!this.storage[hash]) { this.storage[hash] = {}; }
+	this.storage[hash][key] = value;
+	if(this.items > .75*this.SIZE) { this.resize(2); }
+	return this.items;
+};
+
+HashTable.prototype.resize = function(num) {
+	this.SIZE = this.SIZE*num;
+	const arr = new Array(this.SIZE);
+	for(let i = 0; i < this.storage.length; i++) {
+		if(this.storage[i] !== undefined) {
+			for(let key in this.storage[i]) {
+				const hash = hashCode(key, this.SIZE);
+				if(!arr[hash]) arr[hash] = {};
+				arr[hash][key] = this.storage[i][key];
+			}
+		}
+	}
+	this.storage = arr;
+}
+
+/**
+* get - Retrieves a value stored in the hash table with a specified key
+*
+* - If more than one value is stored at the key's hashed address, then you must retrieve
+*   the correct value that was originally stored with the provided key
+*
+* @param {string} key - key to lookup in hash table
+* @return {string|number|boolean} The value stored with the specifed key in the
+* hash table
+*/
+HashTable.prototype.get = function(key) {
+	const hash = hashCode(key, this.SIZE);
+	if(!this.storage[hash]) return undefined;
+	else return this.storage[hash][key];
+};
+
+/**
+* remove - delete a key/value pair from the hash table
+*
+* - If the key does not exist in the hash table, return undefined
+*
+* @param {string} key - key to be found and deleted in hash table
+* @return {string|number|boolean} The value deleted from the hash table
+*/
+HashTable.prototype.remove = function(key) {
+	const hash = hashCode(key, this.SIZE);
+	if(!this.storage[hash]) return undefined;
+	const val = this.storage[hash][key];
+	if(val === undefined) return undefined;
+	else {
+		delete this.storage[hash][key];
+		this.items--;
+		if(this.SIZE > 16 && this.items < .25*this.SIZE) { this.resize(.5); }
+		return val;
+	}
+};
+
+// const ht = new HashTable();
+// ht.set('hello', 'there');
+// console.log('13',ht.SIZE, ht.storage.length, 'num of items:', ht.items);
+// ht.set('food', 'apple');
+// console.log('13',ht.SIZE, ht.storage.length, 'num of items:', ht.items);
+// ht.set('person', 'bo');
+// console.log('13',ht.SIZE, ht.storage.length, 'num of items:', ht.items);
+// ht.set('school', 'codesmith');
+// console.log('13',ht.SIZE, ht.storage.length, 'num of items:', ht.items);
+// ht.set('schools', 'nyu');
+// console.log('13',ht.SIZE, ht.storage.length, 'num of items:', ht.items);
+// ht.set('facility', 'test');
+// console.log('13',ht.SIZE, ht.storage.length, 'num of items:', ht.items);
+// ht.set('play', 'beer');
+// console.log('13',ht.SIZE, ht.storage.length, 'num of items:', ht.items);
+// ht.set('ite', 'test');
+// console.log('13',ht.SIZE, ht.storage.length, 'num of items:', ht.items);
+// ht.set('sdsd', 'test');
+// console.log('13',ht.SIZE, ht.storage.length, 'num of items:', ht.items);
+// ht.set('heeee', 'test');
+// console.log('10',ht.SIZE, ht.storage.length, 'num of items:', ht.items);
+// ht.set('seee', 'test');
+// console.log('11',ht.SIZE, ht.storage.length, 'num of items:', ht.items);
+// ht.set('zeee', 'test');
+// console.log('12',ht.SIZE, ht.storage.length, 'num of items:', ht.items);
+// ht.set('play', 'beer');
+// console.log('13',ht.SIZE, ht.storage.length, 'num of items:', ht.items, ht);
+// ht.remove('hello', 'there');
+// console.log('13',ht.SIZE, ht.storage.length, 'num of items:', ht.items, 'started to remove 1');
+// ht.remove('food', 'apple');
+// console.log('13',ht.SIZE, ht.storage.length, 'num of items:', ht.items);
+// ht.remove('person', 'bo');
+// console.log('13',ht.SIZE, ht.storage.length, 'num of items:', ht.items);
+// ht.remove('school', 'codesmith');
+// console.log('13',ht.SIZE, ht.storage.length, 'num of items:', ht.items, ht);
+// ht.remove('schools', 'nyu');
+// console.log('13',ht.SIZE, ht.storage.length, 'num of items:', ht.items, ht);
+// ht.remove('facility', 'test');
+// console.log('13',ht.SIZE, ht.storage.length, 'num of items:', ht.items, ht);
+// console.log('13',ht.SIZE, ht.storage.length, 'num of items:', ht.items);
 
 // YOUR CODE ABOVE
 
