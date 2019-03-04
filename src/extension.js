70f35f50e975bdcdb27ab71317fb81672ad9d5eb
luijocroussett <ljcroussett@gmail.com>
2019-03-04 10:18:37 -0500

    Week 1 assessment

--- src/extension.js
+++ src/extension.js
@@ -15,6 +15,65 @@
 
 // PASTE AND MODIFY YOUR CODE BELOW
 
+function HashTable() {
+  this.SIZE = 16;
+  
+  this.storage = new Array(this.SIZE);
+}
+
+HashTable.prototype.set = function(key, value) {
+  if (this.storage[hash]) this.storage[hash][key] = value; 
+  else {
+    this.storage[hash] = {};
+    this.storage[hash][key] = value;
+  }
+  // checking for new size and increasing SIZE property if value count is more than 75%
+  let counter = 0;
+  this.storage.forEach((element) => {
+    if (element) {
+      counter++
+    }
+  });
+  if (1.0*counter/this.SIZE > this.SIZE*0.75) {
+    this.SIZE = this.SIZE * 2;
+    //rehashing all data for new size
+    this.storage.forEach((value, key) => {
+      if (element){
+        this.set(key, value);
+        this.remove(key);
+      }
+    });
+  }
+};
+
+  HashTable.prototype.get = function(key) {
+    const hash = hashCode(key, this.SIZE);
+    return this.storage[hash][key];
+  };
+
+  HashTable.prototype.remove = function(key) {
+    const hash = hashCode(key, this.SIZE);
+    let removedValue = this.storage[hash][key];
+    delete this.storage[hash][key];
+    let counter = 0;
+    // checking for new size and reducing SIZE property if value count is less than 25%
+    this.storage.forEach((element) => {
+      if (element) {
+        counter++
+      }
+    });
+    if (1.0*counter/this.SIZE < this.SIZE*0.25 && this.SIZE > 32){
+      this.SIZE = this.SIZE / 2;
+      //rehashing all data for new size
+      this.storage.forEach((value, key) => {
+        if (element){
+          this.set(key, value);
+          this.remove(key);
+        }
+      });
+    }
+    return removedValue; 
+  };
 
 
 // YOUR CODE ABOVE
--- src/main.js
+++ src/main.js
@@ -1,60 +1,83 @@
-/**
-* HashTable costructor
-*
-* construct a new hash table
-*
-* - You may modify this constructor as you need to achieve the challenges below.
+/*
+  Complete this extension only AFTER getting the functionality in main.js working!
+  Copy-paste your working code from main.js below (being sure to have 1 module.exports line).
+  Modify the code to reflect to following:
+
+  1. set:
+      - If adding the new item will push the number of stored items to over 75% of
+        the hash table's SIZE, then double the hash table's SIZE and rehash everything
+
+  2. remove:
+      - If the hash table's SIZE is greater than 16 and the result of removing the
+        item drops the number of stored items to be less than 25% of the hash table's SIZE
+        (rounding down), then reduce the hash table's SIZE by 1/2 and rehash everything.
 */
+
+// PASTE AND MODIFY YOUR CODE BELOW
+
 function HashTable() {
   this.SIZE = 16;
   
   this.storage = new Array(this.SIZE);
 }
 
-/**
-* set - Adds given value to the hash table with specified key.
-*
-* - If the provided key has already been used to store another value, simply overwrite
-*   the existing value with the new value.
-* - If the hashed address already contains another key/value pair, you must handle
-*   the collision appropriately.
-*
-* @param {string} key - key to be used to create hashed address
-* @param {string|number|boolean} value - value to be stored in hash table
-* @return {number} The new number of items stored in the hash table
-*/
 HashTable.prototype.set = function(key, value) {
-
+  if (this.storage[hash]) this.storage[hash][key] = value; 
+  else {
+    this.storage[hash] = {};
+    this.storage[hash][key] = value;
+  }
+  // checking for new size and increasing SIZE property if value count is more than 75%
+  let counter = 0;
+  this.storage.forEach((element) => {
+    if (element) {
+      counter++
+    }
+  });
+  if (1.0*counter/this.SIZE > this.SIZE*0.75) {
+    this.SIZE *= 2;
+    //rehashing all data for new size
+    this.storage.forEach((value, key) => {
+      if (element){
+        this.set(key, value);
+        this.remove(key);
+      }
+    });
+  }
 };
 
-/**
-* get - Retrieves a value stored in the hash table with a specified key
-*
-* - If more than one value is stored at the key's hashed address, then you must retrieve
-*   the correct value that was originally stored with the provided key
-*
-* @param {string} key - key to lookup in hash table
-* @return {string|number|boolean} The value stored with the specifed key in the
-* hash table
-*/
-HashTable.prototype.get = function(key) {
+  HashTable.prototype.get = function(key) {
+    const hash = hashCode(key, this.SIZE);
+    return this.storage[hash][key];
+  };
 
-};
+  HashTable.prototype.remove = function(key) {
+    const hash = hashCode(key, this.SIZE);
+    let removedValue = this.storage[hash][key];
+    delete this.storage[hash][key];
+    let counter = 0;
+    // checking for new size and reducing SIZE property if value count is less than 25%
+    this.storage.forEach((element) => {
+      if (element) {
+        counter++
+      }
+    });
+    if (1.0*counter/this.SIZE < this.SIZE*0.25){
+      this.SIZE = this.SIZE / 2;
+      //rehashing all data for new size
+      this.storage.forEach((value, key) => {
+        if (element){
+          this.set(key, value);
+          this.remove(key);
+        }
+      });
+    }
+    return removedValue; 
+  };
 
-/**
-* remove - delete a key/value pair from the hash table
-*
-* - If the key does not exist in the hash table, return undefined
-*
-* @param {string} key - key to be found and deleted in hash table
-* @return {string|number|boolean} The value deleted from the hash table
-*/
-HashTable.prototype.remove = function(key) {
 
-};
+// YOUR CODE ABOVE
 
-
-// Do not modify
 function hashCode(string, size) {
   'use strict';
   
