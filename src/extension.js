8d54b693cc6cd626ab6d4c6a1c4b1ed2cd81e10a
Johnny Lim <bluejayl@ucla.edu>
2019-04-01 10:01:24 -0700

    currently modifying hashtable set function

--- src/extension.js
+++ src/extension.js
@@ -15,6 +15,89 @@
 
 // PASTE AND MODIFY YOUR CODE BELOW
 
+function HashTable() {
+  this.SIZE = 16;
+  
+  this.storage = new Array(this.SIZE);
+  this.count = this.SIZE;
+}
+
+HashTable.prototype.set = function (key, value) {
+  const hashKey = hashCode(key, this.SIZE);
+  const obj = {};
+  obj[key] = value
+
+  if (this.count === this.SIZE - (this.SIZE * 0.75)) {
+    this.SIZE = this.SIZE * 2;
+    }
+
+  if (this.storage[hashKey] === undefined) {
+    this.storage[hashKey] = obj;
+    this.count--;
+  } else {
+    Object.assign(this.storage[hashKey], obj);
+  }
+}
+
+const hash = new HashTable();
+console.log(hash);
+hash.set('a', 1);
+console.log(hash);
+hash.set('b',2)
+hash.set('c',3)
+hash.set('d',4)
+hash.set('e',5)
+hash.set('f',6)
+hash.set('g',7)
+hash.set('h',8)
+hash.set('i',9)
+hash.set('j',10)
+hash.set('k',11)
+
+console.log(hash);
+console.log(hash.SIZE)
+
+hash.set('l',12) 
+console.log(hash);
+console.log(hash.SIZE)
+hash.set('m',13)
+
+console.log(hash);
+console.log(hash.SIZE);
+// hash.set('n',14)
+// hash.set('o',15)
+// hash.set('p',16)
+
+
+
+
+
+
+
+
+
+
+HashTable.prototype.get = function(key) {
+  const hashKey = hashCode (key, this.SIZE);
+  if(this.storage[hashKey]) {
+    return this.storage[hashKey][key];
+  } else {
+    return 'The value for this key is not stored in this HashTable.'
+  }
+};
+
+
+HashTable.prototype.remove = function(key) {
+  const hashKey = hashCode (key, this.SIZE);
+  const removedVal = this.storage[hashKey][key];
+  if (this.storage[hashKey][key]) {
+    delete this.storage[hashKey][key];
+    return removedVal;
+  } else {
+  return undefined;
+  }
+};
+
 
 
 // YOUR CODE ABOVE
