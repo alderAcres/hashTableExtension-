5b5c207163462ba360942f251ac998562ce3922a
David Levien <davidlevien@Davids-MacBook-Pro.local>
2019-04-01 09:51:30 -0700

    completed main and extension week 1 assessment challenges

--- src/extension.js
+++ src/extension.js
@@ -15,6 +15,72 @@
 
 // PASTE AND MODIFY YOUR CODE BELOW
 
+function HashTable() {
+  this.SIZE = 16;
+  
+  this.storage = new Array(this.SIZE);
+}
+
+
+HashTable.prototype.set = function(key, value) {
+  // check hash table's capacity
+  let hashSlotsFilled = 0;
+  for (let i = 0; i < this.SIZE; i += 1) {
+    if (this.storage[i]) {
+      hashSlotsFilled += 1;
+    };
+  };
+  if (hashSlotsFilled + 1 > Math.floor(0.75 * this.SIZE)) {
+    this.SIZE = this.SIZE * 2;
+    return this.set(key, value);
+  };
+
+  // if re-hash not tripped because of capacity, hash like normal
+  let insertionIndex = hashCode(key, this.SIZE);
+  if (!this.storage[insertionIndex]) {
+    this.storage[insertionIndex] = {};
+  };
+  this.storage[insertionIndex][key] = value;
+};
+
+
+HashTable.prototype.get = function(key) {
+  let checkIndex = hashCode(key, this.SIZE);
+  return this.storage[checkIndex][key];
+};
+
+// 2. remove:
+//       - If the hash table's SIZE is greater than 16 and the result of removing the
+//         item drops the number of stored items to be less than 25% of the hash table's SIZE
+//         (rounding down), then reduce the hash table's SIZE by 1/2 and rehash everything.
+
+HashTable.prototype.remove = function(key) {
+  // check hash table's capacity
+  if (this.SIZE > 16) {
+    let hashSlotsFilled = 0;
+    // count hash slots occupied, if size is greater than 16
+    for (let i = 0; i < this.SIZE; i += 1) {
+      if (this.storage[i]) {
+        hashSlotsFilled += 1;
+      };
+    };
+    // if size was greater than 16 AND slots filled - 1 (to be removed) will be less than 25% capacity, half size and re-run
+    if (hashSlotsFilled - 1 < Math.floor(0.25 * this.SIZE)) {
+      this.SIZE = this.SIZE / 2;
+      return this.remove(key, value);
+    };
+  };
+
+  // if re-hash not tripped because of capacity, hash like normal
+  let checkIndex = hashCode(key, this.SIZE);
+  if (!this.storage[checkIndex][key]) {
+    return undefined;
+  } else {
+    const searchedKeyVal = this.storage[checkIndex][key];
+    delete this.storage[checkIndex][key];
+    return searchedKeyVal;
+  };
+};
 
 
 // YOUR CODE ABOVE
