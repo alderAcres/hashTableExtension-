ebc5be20c908e3d9172089070b47fea4d8883be7
Jaime Gonnzalez <2016jgonzalez139@gmail.com>
2019-04-01 09:58:00 -0700

    finished main but not extension

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@
 */
 function HashTable() {
   this.SIZE = 16;
-  
+
   this.storage = new Array(this.SIZE);
 }
 
@@ -24,7 +24,8 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
+  const code = hashCode(key, this.SIZE);
+  this.storage[code] = value;
 };
 
 /**
@@ -38,7 +39,8 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  const code = hashCode(key, this.SIZE);
+  return this.storage[code];
 };
 
 /**
@@ -50,25 +52,37 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
-
+  const code = hashCode(key,this.SIZE);
+  if(this.storage[code]){
+    const value = this.storage[code];
+    delete this.storage[code];
+    return value;
+  }
+  return undefined;
 };
 
 
 // Do not modify
 function hashCode(string, size) {
   'use strict';
-  
+
   let hash = 0;
   if (string.length === 0) return hash;
-  
+
   for (let i = 0; i < string.length; i++) {
     const letter = string.charCodeAt(i);
     hash = ((hash << 5) - hash) + letter;
     hash = hash & hash; // Convert to 32bit integer
   }
-  
+
   return Math.abs(hash) % size;
 }
+
+
+const table = new HashTable()
+
+table.set("password", "jaime");
+console.log(table.get("password"));
 
 // Do not remove!!
 module.exports = HashTable;
