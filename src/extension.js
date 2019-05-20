2989cb8628d581cd891898a032034684ba8429d1
gp5yk <nahuel.arjona.93@gmail.com>
2019-05-20 10:10:05 -0700

    finish resize functionality in set method of extensions.js

--- src/extension.js
+++ src/extension.js
@@ -15,6 +15,120 @@
 
 // PASTE AND MODIFY YOUR CODE BELOW
 
+/**
+* HashTable costructor
+*
+* construct a new hash table
+*
+* - You may modify this constructor as you need to achieve the challenges below.
+*/
+function HashTable() {
+  this.SIZE = 16;
+  this.stored = 0;
+  
+  this.storage = new Array(this.SIZE);
+}
+
+/**
+* set - Adds given value to the hash table with specified key.
+*
+* - If the provided key has already been used to store another value, simply overwrite
+*   the existing value with the new value.
+* - If the hashed address already contains another key/value pair, you must handle
+*   the collision appropriately.
+*
+* @param {string} key - key to be used to create hashed address
+* @param {string|number|boolean} value - value to be stored in hash table
+* @return {number} The new number of items stored in the hash table
+*/
+HashTable.prototype.set = function(key, value) {
+  // hash the key
+  const hash = hashCode(key, this.SIZE);
+  // console.log('key',key, 'hash', hash);
+  // look for hashed pos. if key didn't exist before, create it
+  if (!this.storage[hash]) {
+    this.storage[hash] = {};
+    this.stored += 1;
+  }
+  else {
+    // if key didn't existed add one to this.stored
+    if (!this.storage[hash].hasOwnProperty(key)) this.stored += 1;
+  }
+  if (this.SIZE * 0.75 < this.stored) {
+    this.resize();
+    this.set(key, value)
+  } 
+  else {
+    // add the value at specified key
+    this.storage[hash][key] = value;
+  }
+  // output: undefined
+};
+
+/**
+* get - Retrieves a value stored in the hash table with a specified key
+*
+* - If more than one value is stored at the key's hashed address, then you must retrieve
+*   the correct value that was originally stored with the provided key
+*
+* @param {string} key - key to lookup in hash table
+* @return {string|number|boolean} The value stored with the specifed key in the
+* hash table
+*/
+HashTable.prototype.get = function(key) {
+  // hash
+  const hash = hashCode(key, this.SIZE);
+  // look for object, if not return undefined
+  if (!this.storage[hash]) return;
+  // if object, look for key. If not, return undefined
+  if (!this.storage[hash].hasOwnProperty(key)) return;
+  return this.storage[hash][key]
+};
+
+/**
+* remove - delete a key/value pair from the hash table
+*
+* - If the key does not exist in the hash table, return undefined
+*
+* @param {string} key - key to be found and deleted in hash table
+* @return {string|number|boolean} The value deleted from the hash table
+*/
+HashTable.prototype.remove = function(key) {
+  const hash = hashCode(key, this.SIZE);
+  // check if obj in hash. if not -> undefined
+  // check if key in hash. if not -> undefined
+  if (!this.storage[hash] || !this.storage[hash].hasOwnProperty(key)) return;
+  // if key, delete entry and store value in temp variable
+  const value = this.storage[hash][key];
+  delete this.storage[hash][key];
+  // if obj now empty, reset position to null
+  if (Object.keys(this.storage[hash]).length === 0) this.storage[hash] = null;
+  // return value in temp
+  this.stored -= 1;
+  return value;
+};
+
+
+HashTable.prototype.resize = function() {
+  const entries = this.getAllEntries();
+  this.stored = 0;
+  this.SIZE = this.SIZE * 2;
+  this.storage = new Array(this.SIZE);
+  for (let o = 0; o < this.SIZE; o++) {
+    this.storage[o] = null;
+  }
+  entries.forEach(([key, value]) => {
+    this.set(key, value);
+  });
+}
+
+HashTable.prototype.getAllEntries = function() {
+  return this.storage.filter(obj => obj !== null)
+    .reduce((acc, obj) => {
+      return acc.concat(Object.entries(obj));
+    }, []);
+}
+
 
 
 // YOUR CODE ABOVE
@@ -36,3 +150,28 @@ function hashCode(string, size) {
 
 // Do not remove!!
 module.exports = HashTable;
+
+
+
+// TESTING
+const ht = new HashTable();
+ht.set('y', 5)
+ht.set('y', 8);
+ht.set('i', 4);
+// ht.set('aj', 10);
+ht.set('n', 7);
+console.log('all entries:', ht.getAllEntries());
+ht.resize();
+console.log(ht);
+console.log(ht.get('n'), 'this.stored:', ht.stored); // 7, // 3
+console.log(ht.get('y') === 8); // 8
+console.log(ht.get('i') === 4); // 4
+console.log(ht.get('aj') === undefined); // undefined
+console.log(ht.get('a') === undefined); // undefined
+console.log(JSON.stringify(ht.storage));
+console.log(ht.remove('y') === 8) // 8
+console.log(JSON.stringify(ht)); // storage still contains obj with 'i' key at hash 10. this.stored === 2
+console.log(ht.remove('y') === undefined) // undefined
+console.log(JSON.stringify(ht.storage)); //storage still contains obj with 'i' key at hash 10
+console.log(ht.remove('i') === 4) // 4
+console.log(JSON.stringify(ht)); // storage now has null at hash 10      this.stored === 1
