de19e1c2dc57848f6fd888fa67de7338d9ecf449
Amanda Flink <amanda@Amandas-MacBook-Pro-2.local>
2019-06-10 10:16:08 -0400

    main complete, half of extension complete

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ function HashTable() {
   this.SIZE = 16;
   
   this.storage = new Array(this.SIZE);
+  this.items = 0;
 }
 
 /**
@@ -23,8 +24,63 @@ function HashTable() {
 * @param {string|number|boolean} value - value to be stored in hash table
 * @return {number} The new number of items stored in the hash table
 */
-HashTable.prototype.set = function(key, value) {
 
+function HashLL() {
+  this.head = null ;
+  this.tail = null ;
+  this.length = 0 ;
+}
+
+function HashLLNode(k, val) {
+  this.key = k ;
+  this.value = val ;
+  this.next = null ;
+}
+
+HashLL.prototype.addNode = function(key, value) {
+  if (!this.head) { //if the bucket is empty, add to head (and tail)
+    const newNode = new HashLLNode(key, value)
+    this.head = newNode ;
+    this.tail = newNode ;
+    this.length++ ;
+  }
+  else { //if the bucket is not empty, add to tail
+    //but must overwrite a value if the same key is given
+     //keeps track of if we added a new Node or overwrote an old one
+    if (this.head.key === key) {this.head.value = value ; return } //overwrites head value if head key is same and no other links in list
+    if (this.tail.key === key) {this.tail.value = value ; return } //overwrites tail value if tail key is same
+    let curr = this.head ;
+    while (curr.next) {
+      if (curr.key === key) { //if found key in middle of list, reassign value and return
+        curr.value = value ;
+        return ;
+      }
+      curr = curr.next ;
+    }
+    const newNode = HashLLNode(key, value)
+    curr.next = newNode ;
+    this.tail = newNode ;
+    this.length++ ;
+  }
+}
+
+HashTable.prototype.set = function(key, value) { 
+  //find which bucket to dump key,value pair in
+  //create LL if bucket is empty
+  //add key, value pair to bucket's LL
+  //increment # of items of this
+  //return this number
+  let bucket = hashCode(key, this.SIZE) ;
+  if (!this.storage[bucket]) { //if the slot in the storage is empty
+    this.storage[bucket] = new HashLL() ; //create LL
+  }
+  // console.log(this.storage[bucket])
+  let originalLength = this.storage[bucket].length ;
+  this.storage[bucket].addNode(key, value) ; //add key, value pair as node to storage bucket's LL
+  if (this.storage[bucket].length > originalLength) { //if the length of the bucket's LL changes, we know we added a node
+    this.items++
+  }
+  return this.items ;
 };
 
 /**
@@ -38,7 +94,25 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  //use hashCode to find bucket
+  //look for key
+  //return value
+  let bucket = hashCode(key, this.SIZE) ;
+  if (this.storage[bucket] == undefined) {
+    return `Sorry, but ${key} is not in this hash table`
+  }
+  //finds value if key is in head //potentially unneccessary 
+  // if (this.storage[bucket].head.key === key) {
+  //   return this.storage[bucket].head.value ;
+  // }
+  else {
+    let curr = this.storage[bucket].head ;
+    while (curr) {
+      if (curr.key === key) {return curr.value} //if our current link has the key, return value
+      if (curr.next === null ) {return `Sorry, but ${key} is not in this hash table`} //if we hit the tail and never found the key, it's not there
+      curr = curr.next ; //if the curr link doesn't have the key, keep moving
+    }
+  }
 };
 
 /**
@@ -50,6 +124,39 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
+  //use hashCode to find bucket
+  //look for key
+  //delete key and reutrn value
+  let bucket = hashCode(key, this.SIZE) ;
+  if (this.storage[bucket] == undefined) {
+    return `Sorry, but ${key} is not in this hash table`
+  }
+  // finds value if key is in head //potentially unneccessary 
+  if (this.storage[bucket].head.key === key) { 
+    let removed = this.storage[bucket].head.value ;
+    if (this.storage[bucket].head.next) { //if the LL has more links
+      this.storage[bucket].head = this.storage[bucket].head.next ;
+    }
+    else { //if we just cut off the head and there's nothing to follow
+      this.storage[bucket] = undefined ; //obliterate LL
+    }
+    return removed;
+  }
+  else {
+    let curr = this.storage[bucket].head ;
+    if (curr.next === null ) {return `Sorry, but ${key} is not in this hash table`} //if we hit the tail and never found the key, it's not there
+    while (curr.next) {
+      if (curr.next === null ) {return `Sorry, but ${key} is not in this hash table`} //if we hit the tail and never found the key, it's not there
+      if (curr.next.key === key) { //if our current link has the key, delete and return value
+        let removed = curr.next.value ;
+        if (curr.next.next) {
+          curr.next = curr.next.next ;
+        }
+        return removed ;
+      }
+      curr = curr.next ; //if the curr link's next doesn't have the key, keep moving
+    }
+  }
 
 };
 
@@ -66,9 +173,38 @@ function hashCode(string, size) {
     hash = ((hash << 5) - hash) + letter;
     hash = hash & hash; // Convert to 32bit integer
   }
-  
   return Math.abs(hash) % size;
 }
 
 // Do not remove!!
 module.exports = HashTable;
+
+
+let hash = new HashTable() ;
+console.log(hash.SIZE)
+console.log(hash.storage)
+console.log(hash.items)
+
+//set something
+hash.set("meow", "meowmeow") //tests setting
+console.log(hash.set("meow", "woof")) //tests return value = # of items
+hash.set("meow", "woof") //tests overwriting key values
+console.log(hash.SIZE)
+console.log(hash.storage)
+console.log(hash.items)
+
+console.log(hash.get('mdeow'))
+console.log(hash.get('meow'))
+
+console.log(hash.remove("jjjj"))
+console.log(hash.remove("meow"))
+
+
+let newHashTable = new HashTable() ;
+for (let i=0; i<18; i++) {
+  k = "meow" + i ;
+  v = "meow" + i ;
+  newHashTable.set(k, v) ;
+}
+
+console.log(newHashTable)
