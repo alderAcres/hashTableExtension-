29be42f0ce58ef1f34e8f058d399f8d140ad73e2
Rocky <rliao3613@gmail.com>
2019-06-10 09:59:47 -0400

    This is what happens when tables collide

--- src/extension.js
+++ src/extension.js
@@ -15,6 +15,92 @@
 
 // PASTE AND MODIFY YOUR CODE BELOW
 
+function HashTable() {
+  this.SIZE = 16;
+  this.currentSize = 0;
+  this.storage = new Array(this.SIZE);
+}
+HashTable.prototype.rehashEverythang = function(isIncreasing){
+  let newSize = isIncreasing ? this.SIZE*2 : this.SIZE;
+  if(!isIncreasing && this.SIZE > 16){
+    newSize = this.SIZE/2;
+  }
+
+  let newStorage = new Array(newSize);
+  for(const obj of Object.keys(this.storage)){
+    for(const key of Object.keys(obj)){
+      newStorage[hashCode(key,newSize)][key]= obj[key];
+    }
+  }
+  this.SIZE = newSize;
+  this.storage = newStorage;
+}
+HashTable.prototype.set = function(key, value) {
+  //get hashed index of our key
+  let hashedIndex = hashCode(key,this.SIZE);
+  //check if theres anything there, if not then make new obj and throw it in there
+  if(this.storage[hashedIndex]===undefined){
+    let newObj = {};
+    newObj[key]=value;
+    this.storage[hashedIndex] = newObj;
+    this.currentSize+=1
+  }
+  else{
+    //simple overwrite existing,dont have to check cuz assigning automatically overwrites,
+    //nah dude we actually gotta check it so we know whether or not to add to current size
+    if(this.storage[hashedIndex][key]===undefined){
+      this.currentSize+=1;
+    }
+    this.storage[hashedIndex][key] = value;
+  }
+  if(this.currentSize >= this.SIZE*.75){
+    this.rehashEverythang(true);
+  }
+  //return number of items added which is one
+  //should overwriting it make it zero? but you are kinda adding a new item into it.
+  //wait actually it asks for the new number of items in it so we gotta check for current size;
+  
+  return this.currentSize;
+}
+/** 
+* get - Retrieves a value stored in the hash table with a specified key
+*
+* - If more than one value is stored at the key's hashed address, then you must retrieve
+*   the correct value that was originally stored with the provided key
+*
+* @param {string} key - key to lookup in hash table
+* @return {string|number|boolean} The value stored with the specifed key in the
+* hash table
+*/
+HashTable.prototype.get = function(key) {
+  //const value = this.storage[hashCode(key,this.SIZE)][key];
+  //return value===undefined ? undefined : value;
+  return this.storage[hashCode(key,this.SIZE)][key]
+};
+
+/**
+* remove - delete a key/value pair from the hash table
+*
+* - If the key does not exist in the hash table, return undefined
+*
+* @param {string} key - key to be found and deleted in hash table
+* @return {string|number|boolean} The value deleted from the hash table
+*/
+HashTable.prototype.remove = function(key) {
+  //we wanna return value at key
+  const popped = this.get(key);
+  //check if it exists
+  if(popped!==undefined){
+    //if yes then delete it then decrease current size
+    delete this.storage[hashCode(key,this.SIZE)][key];
+    this.currentSize-=1;
+  }
+  if(this.currentSize<=Math.floor(.25*this.SIZE)){
+    this.rehashEverythang(false);
+  }
+  return popped;
+};
+
 
 
 // YOUR CODE ABOVE
--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@
 */
 function HashTable() {
   this.SIZE = 16;
-  
+  this.currentSize = 0;
   this.storage = new Array(this.SIZE);
 }
 
@@ -24,10 +24,30 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
-};
-
-/**
+  //get hashed index of our key
+  let hashedIndex = hashCode(key,this.SIZE);
+  //check if theres anything there, if not then make new obj and throw it in there
+  if(this.storage[hashedIndex]===undefined){
+    let newObj = {};
+    newObj[key]=value;
+    this.storage[hashedIndex] = newObj;
+    this.currentSize+=1
+  }
+  else{
+    //simple overwrite existing,dont have to check cuz assigning automatically overwrites,
+    //nah dude we actually gotta check it so we know whether or not to add to current size
+    if(this.storage[hashedIndex][key]===undefined){
+      this.currentSize+=1;
+    }
+    this.storage[hashedIndex][key] = value;
+  }
+  //return number of items added which is one
+  //should overwriting it make it zero? but you are kinda adding a new item into it.
+  //wait actually it asks for the new number of items in it so we gotta check for current size;
+  
+  return this.currentSize;
+}
+/** 
 * get - Retrieves a value stored in the hash table with a specified key
 *
 * - If more than one value is stored at the key's hashed address, then you must retrieve
@@ -38,7 +58,9 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  //const value = this.storage[hashCode(key,this.SIZE)][key];
+  //return value===undefined ? undefined : value;
+  return this.storage[hashCode(key,this.SIZE)][key]
 };
 
 /**
@@ -50,9 +72,17 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
+  //we wanna return value at key
+  const popped = this.get(key);
+  //check if it exists
+  if(popped!==undefined){
+    //if yes then delete it then decrease current size
+    delete this.storage[hashCode(key,this.SIZE)][key];
+    this.currentSize-=1;
+  }
 
+  return popped;
 };
-
 
 // Do not modify
 function hashCode(string, size) {
@@ -69,6 +99,27 @@ function hashCode(string, size) {
   
   return Math.abs(hash) % size;
 }
+
+// let hashTable = new HashTable;
+// for(let i =0;i<25;i++){
+//   const key = 'key '+i;
+//   const value = i;
+//   //console.log(hashCode(key,hashTable.SIZE))
+//   hashTable.set(key,i);
+// }
+
+// console.log(hashTable,hashTable.currentSize)
+// hashTable.set('key 0',25);
+// console.log(hashTable.currentSize,hashTable)
+// console.log(hashTable.get('key 5'));
+// console.log(hashTable.get('25'));
+// // console.log(hashTable.storage[hashCode('25',hashTable.SIZE)]['25'])
+// // console.log(hashCode('key 1',hashTable.SIZE))
+// // console.log(hashCode('25',hashTable.SIZE))
+// console.log(hashTable.currentSize,hashTable)
+
+// console.log(hashTable.remove('key 3'))
+// console.log(hashTable.currentSize,hashTable)
 
 // Do not remove!!
 module.exports = HashTable;
