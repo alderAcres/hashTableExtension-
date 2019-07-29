1290482fc840ec2b9e95194792bacbdb8dfa27d0
Gareth Leake <gfleake@gmail.com>
2019-07-29 10:13:52 -0400

    challenges completed

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
+// ## How do I get started?
+// 1. Fork this repo
+// 1. Clone your fork
+// 1. Complete the challenge in located in `src/main.js`.
+//   - You must implement the `get`, `set`, and `remove` functions on the HashTable prototype. 
+//   - Each function is commented with its specifications. Implement the functions exactly as their documentation prescribes. You may assume the functions will be called with the types listed in their documentation.
+//   - **important** Be sure to return the specified return value from each function's documentation
+// 1. Extension: Copy-paste your working code from `src/main.js` into `src/extension.js`.
+//   - In `extension.js` implement resizing in the `set` and `remove` methods.
+
 /**
 * HashTable costructor
 *
@@ -7,7 +17,7 @@
 */
 function HashTable() {
   this.SIZE = 16;
-  
+  this.numberItemsStored = 0;
   this.storage = new Array(this.SIZE);
 }
 
@@ -24,6 +34,36 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
+  // 1. handle collisions (use an array);
+  // 2. create a variable, set it equal to the bucket location (run the key through the hash function)
+  // 3. create an antiCollision array
+  // 4. check if anything exists at bucket location
+  // 5. if nothing exists, add the antiCollision array
+  // 6. add [key, value] as a subArray in the antiCollision array
+  // 7. if something DOES exist inside the antiCollision array, iterate through and check all keys.
+      // if key matches, overwrite the value 
+      //    (note; could probably just overwrite that subArray... with the new subArray, since key is the same...)
+  // 8. adjust constructor function to have a key "numberValuesStored"
+  // 9. increase the value of numberItemsStored by 1
+    
+  const bucketLocation = hashCode(key, this.SIZE);
+  const antiCollision = [];
+
+  if (this.storage[bucketLocation] === undefined){
+    this.storage[bucketLocation] = antiCollision;
+    this.storage[bucketLocation].push([key, value]);
+    this.numberItemsStored += 1;
+    return this.numberItemsStored;
+  }
+  if (this.storage[bucketLocation] !== undefined){
+    for (let i = 0; i < this.storage[bucketLocation].length; i += 1){
+      if (this.storage[bucketLocation][i][0] === key) this.storage[bucketLocation][i][1] = value;
+      this.numberItemsStored += 1
+      return this.numberItemsStored;
+    }
+    this.storage[bucketLocation].push([key, value]);
+    return;
+  }
 
 };
 
@@ -39,6 +79,22 @@ HashTable.prototype.set = function(key, value) {
 */
 HashTable.prototype.get = function(key) {
 
+  /*
+  1. create a variable, intiialize it to hashTable location (returned value from hash function);
+  2. once at specific location, loop through elements in antiCollision array
+    - check if subArray[0] === key. If so, return subArray[1]
+  3. if loop through whole array, and nothing is present, return "Key is not stored in HashTable."
+  */
+
+  const bucketLocation = hashCode(key, this.SIZE);
+
+  for (let i = 0; i < this.storage[bucketLocation].length; i += 1){
+    if (this.storage[bucketLocation][i][0] === key) return this.storage[bucketLocation][i][1];
+  }
+
+  return "Requested key is not stored in HashTable.";
+
+
 };
 
 /**
@@ -50,6 +106,31 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
+
+  /*
+  1. create a variable, initialize it to bucketLocation (returned value from hashFunction);
+  2. iterate through all subArrays present at bucketLocation;
+  3. if requested key is found:
+    - overwrite the subArray to undefined;
+    - i.e. alternatively, use the .splice() array method to actually delete the information at that location
+      - (this will also reindex the remaining elements, so you don't have a hole in that spot, thereby managing storage space better);
+  4. if element is removed from hashTable, decrease numberItemsStored by 1
+  5. if key is not found, return undefined
+  */
+
+  const bucketLocation = hashCode(key, this.SIZE);
+  let removedValue;
+
+  for (let i = 0; i < this.storage[bucketLocation].length; i += 1){
+    if (this.storage[bucketLocation][i][0] === key){
+      removedValue = this.storage[bucketLocation][i][1];
+      this.storage[bucketLocation].splice(i, 1);
+      this.numberItemsStored -= 1;
+      return removedValue;
+    }
+  }
+
+  return undefined;
 
 };
 
