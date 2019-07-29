651d4277d8039e4ac28a94387b6fba9cd8ab06e0
William Magee <Will@Williams-MacBook-Pro.local>
2019-07-29 10:15:39 -0400

    completed main.js, unable to complete extension

--- src/main.js
+++ src/main.js
@@ -9,6 +9,20 @@ function HashTable() {
   this.SIZE = 16;
   
   this.storage = new Array(this.SIZE);
+  this.keysUsed = {};
+
+  this.numItems = 0;
+}
+
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
+
+function Node(key, value) {
+  this.key = key
+  this.value = value;
+  this.next = null;
 }
 
 /**
@@ -24,7 +38,42 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
+  let hashVal = hashCode(key, this.SIZE);
+  let newNode = new Node(key, value);
 
+  if (this.storage[hashVal] === undefined) {
+
+    this.storage[hashVal] = new LinkedList();
+    this.storage[hashVal].head = newNode;
+    this.storage[hashVal].tail = newNode
+    this.keysUsed[key] = 1;
+
+    this.numItems++;
+
+  } 
+  else if (this.storage[hashVal] !== undefined && this.keysUsed[key] !== undefined) {
+    
+    if (this.storage[hashVal].head.next === null) {
+      this.storage[hashVal].head.value = value;
+    } else {
+      let temp = this.storage[hashVal].head;
+      while (temp.next.key !== key) {
+        temp.next;
+      }
+      temp.next.value = value;
+    }
+    
+  } 
+  else {
+    this.keysUsed[key] = 1;
+    this.storage[hashVal].tail.next = newNode;
+    this.storage[hashVal].tail = newNode;
+
+    this.numItems++;
+    
+  }
+
+  return this.numItems;
 };
 
 /**
@@ -38,6 +87,18 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
+  let hashVal = hashCode(key, this.SIZE);
+
+  if (this.keysUsed[key] === undefined) {
+    return undefined;
+  }
+  else {
+    let temp = this.storage[hashVal].head
+    while (temp.key !== key) {
+      temp = temp.next
+    }
+    return temp.value;
+  }
 
 };
 
@@ -50,6 +111,38 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
+  
+  let hashVal = hashCode(key, this.SIZE);
+
+  if (this.keysUsed[key] === undefined) {
+    return undefined;
+  }
+  else {
+    this.numItems--;
+    delete this.keysUsed[key];
+    if (this.storage[hashVal].head.next === null) {
+      let nodeVal = this.storage[hashVal].head.value;
+      this.storage[hashVal] = undefined;
+      return nodeVal;
+    }
+
+    let temp = this.storage[hashVal].head;
+    while (temp.next.key !== key) {
+      temp = temp.next;
+    }
+    if (temp.next === this.storage[hashVal].tail) {
+      let nodeVal = this.storage[hashVal].tail.value;
+      this.storage[hashVal].tail = temp;
+
+    } 
+    else {
+      let nodeVal = temp.next.value;
+      temp.next = temp.next.next;
+      return nodeVal;
+    }
+
+
+  }
 
 };
 
@@ -72,3 +165,21 @@ function hashCode(string, size) {
 
 // Do not remove!!
 module.exports = HashTable;
+
+
+
+let table = new HashTable();
+table.set("a", 3);
+table.set('b', 5);
+table.set("c", 234);
+console.log(table.get("a"));
+table.set("a", 6);
+console.log(table.get("a"));
+table.remove("a");
+console.log(table.get("a"));
+table.set("abc", 6);
+table.set("bca", 8);
+console.log(table.get("abc"));
+console.log(table.get("bca"));
+
+
