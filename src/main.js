19e4652f228fc0a70541a7413b94628f8e4ecd61
Jackie Lin <jackie.lin128@gmail.com>
2019-07-29 09:31:54 -0400

    first check in

--- src/main.js
+++ src/main.js
@@ -11,6 +11,12 @@ function HashTable() {
   this.storage = new Array(this.SIZE);
 }
 
+function Node(value, key) {
+  this.key = key
+  this.value = value;
+  this.next = null;
+}
+
 /**
 * set - Adds given value to the hash table with specified key.
 *
@@ -24,7 +30,25 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
+  //use the hash function to get the location in the array
+  let location = hashCode(key, this.SIZE);
+  //create a new node with the key and value given
+  let newNode = new Node(value, key);
+  //if the location is currently empty just set the value of that location to the newly created node
+  if(!this.storage[location]) {
+    this.storage[location] = newNode;
+  }else {
+    //create a value to keep track of where you are in the list
+    let currentNode = this.storage[location];
+    //find the end point of the linked list and insert a new node
+      //while the next property is not null move on to the next node to move through the list by reassigning current to the next node
+    while (currentNode.next !== null) {
+      currentNode = currentNode.next;
+    }
+    //now you are at the end of the list
+    //set the next property of the last node to your new node
+    currentNode.next = newNode;
+  }
 };
 
 /**
@@ -38,7 +62,24 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  let location = hashCode(key, this.SIZE);
+  //first check if there is a value in that location. if not then just return
+  if (!this.storage[location]) {
+    return;
+  }
+  //if there is a value there, then start looking through the list until the next property is null
+  let currentNode = this.storage[location];
+  //at each step compare the value of the key to the key given
+  while (currentNode.next !== null) {
+    //when the key matches, return the value of that node.
+    if (currentNode.key === key) {
+      return currentNode.value;
+    }
+    //otherwise move on
+    currentNode = currentNode.next;
+  }
+  //if nothing matches then return undefined
+  return;
 };
 
 /**
