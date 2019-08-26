ab6059b5e258f466392787a02cd29db6a973e640
Darren Zhu <joodongri@gmail.com>
2019-08-26 09:44:40 -0700

    done with main task

--- src/main.js
+++ src/main.js
@@ -24,7 +24,19 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
+  const position = hashCode(key, this.SIZE);
+  // when certain position in hash table holds nothing
+  if(!this.storage[position]) {
+    // we add new obj with passed key and value to the position
+    let tempObj = {}
+    tempObj[key] = value;
+    this.storage[position] = tempObj;
+  }
+  // handles collision
+  else {
+    // we add new obj to the existing obj in the position of hash table
+    this.storage[position][key] = value;
+  }
 };
 
 /**
@@ -38,7 +50,13 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  const position = hashCode(key, this.SIZE);
+  if(!this.storage[position]) {
+    console.log('No value for the passed key. Please try again.');
+    return;
+  } else {
+    return this.storage[position][key];
+  }
 };
 
 /**
@@ -50,9 +68,23 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
+  const position = hashCode(key, this.SIZE);
+  // when the key does not exist in the hash table
+  if(!this.storage[position] || !this.storage[position][key]) {
+    return undefined;
 
+    // when there is only one key value pair in the objet
+  } else if(Object.keys(this.storage[position]).length === 1){
+    let returnObj = this.storage[position];
+    this.storage[position] = undefined;
+    return returnObj;
+  } else { // when there are multiple key value pairs in the object
+    let returnObj = {};
+    returnObj[key] = this.storage[position][key];
+    delete this.storage[position][key];
+    return returnObj;
+  }
 };
-
 
 // Do not modify
 function hashCode(string, size) {
@@ -69,6 +101,21 @@ function hashCode(string, size) {
   
   return Math.abs(hash) % size;
 }
+
+// Test Cases:
+// let hash = new HashTable;
+// hash.set('d', 'got');
+// hash.set('15', 20);
+// hash.set('hello', 'world');
+// console.log(hash.storage);
+// console.log(hash.get('d'));
+// console.log(hash.get('good'));
+// console.log(hash.remove('hello'));
+// console.log(hash.storage);
+// console.log(hash.remove('15'));
+// console.log(hash.storage);
+// console.log(hash.remove('15'));
+
 
 // Do not remove!!
 module.exports = HashTable;
