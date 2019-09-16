766c7ac0518958c2c07ae22497799de855d841d2
Chris Flannery <chriswillsflannery@gmail.com>
2019-09-16 10:13:04 -0400

    completed week-1-assessment

--- src/extension.js
+++ src/extension.js
@@ -15,22 +15,205 @@
 
 // PASTE AND MODIFY YOUR CODE BELOW
 
+/**
+* HashTable costructor
+*
+* construct a new hash table
+*
+* - You may modify this constructor as you need to achieve the challenges below.
+*/
+function HashTable() {
+  this.SIZE = 16;
+
+  this.storage = new Array(this.SIZE);
+}
+
+/**
+* set - Adds given value to the hash table with specified key.
+*
+* - If the provided key has already been used to store another value, simply overwrite
+*   the existing value with the new value.
+* - If the hashed address already contains another key/value pair, you must handle
+*   the collision appropriately.
+*
+* @param {string} key - key to be used to create hashed address
+* @param {string|number|boolean} value - value to be stored in hash table
+* @return {number} The new number of items stored in the hash table
+*/
+HashTable.prototype.set = function (key, value) {
+  const code = hashCode(key, this.SIZE);
+
+
+  if (!this.storage[code]) {
+    //if bucket is empty,
+    //create an object at that bucket in the hashTable
+    this.storage[code] = {};
+    // store key/value pair at that bucket
+    this.storage[code][key] = value;
+  } else {
+    //if bucket already contains an object,
+    //store or overwrite key/value pair at that bucket
+    this.storage[code][key] = value;
+  }
+
+  //check if buckets currently are filled more than 75% of SIZE
+  const fillsize = this.storage.reduce((total, curr) => {
+    if (curr) total += 1;
+    return total;
+  }, 0);
+  if (fillsize / this.SIZE >= .75) {
+    this.doubleHash(this.SIZE);
+  }
+};
+
+// const myTable = new HashTable;
+// console.log(myTable);
+// console.log(myTable.set("chris", "flannery"));
+// console.log(myTable);
+// console.log(myTable.set("shric", "wills"));
+// console.log(myTable);
+
+/**
+* get - Retrieves a value stored in the hash table with a specified key
+*
+* - If more than one value is stored at the key's hashed address, then you must retrieve
+*   the correct value that was originally stored with the provided key
+*
+* @param {string} key - key to lookup in hash table
+* @return {string|number|boolean} The value stored with the specifed key in the
+* hash table
+*/
+HashTable.prototype.get = function (key) {
+  const code = hashCode(key, this.SIZE);
+
+
+  if (!this.storage[code]) {
+    //if there is nothing stored in bucket at code
+    console.log("Hmm, it looks like there's nothing here to get!");
+  } else if (!this.storage[code][key]) {
+    //if there is an object stored in bucket at code
+    //...but the key you are looking for is not inside
+    console.log("There is something stored here, but it's not what you are looking for!")
+  } else {
+    //if there is an object stored in bucket at code
+    ///...and the key you are looking for is inside, return it
+    return this.storage[code][key];
+  }
+};
+
+// const myTable = new HashTable;
+// console.log(myTable);
+// console.log(myTable.set("chris", "flannery"));
+// console.log(myTable);
+// console.log(myTable.set("shric", "wills"));
+// console.log(myTable);
+// console.log(myTable.get("chris"));
+// console.log(myTable.get("hrsic"));
+// console.log(myTable.get("beans"));
+
+/**
+* remove - delete a key/value pair from the hash table
+*
+* - If the key does not exist in the hash table, return undefined
+*
+* @param {string} key - key to be found and deleted in hash table
+* @return {string|number|boolean} The value deleted from the hash table
+*/
+HashTable.prototype.remove = function (key) {
+  const code = hashCode(key, this.SIZE);
+
+  if (!this.storage[code]) {
+    //if there is nothing stored in bucket at code
+    console.log("Hmm... it looks like this bucket is already empty!");
+  } else if (!this.storage[code][key]) {
+    //if there is an object stored in bucket at code
+    //...but the key you are looking for is not inside
+    console.log("Hmmm... looks like the key/value pair you are trying to remove is not present here.")
+  } else {
+    //if there is an object stored in bucket at code
+    //...and the key you are looking for is inside, remove it
+    delete this.storage[code][key];
+  }
+
+  //check if buckets currently are filled less than 25% of SIZE
+  const fillsize = this.storage.reduce((total, curr) => {
+    if (curr) total += 1;
+    return total;
+  }, 0);
+  if (fillsize / this.SIZE <= .25) {
+    this.halveHash(this.SIZE);
+  }
+
+};
+
+// const myTable = new HashTable;
+// console.log(myTable);
+// console.log(myTable.set("chris", "flannery"));
+// console.log(myTable);
+// console.log(myTable.set("shric", "wills"));
+// console.log(myTable);
+// myTable.remove("chris");
+// console.log(myTable);
+// myTable.remove("chris");
+// myTable.remove("beans");
+
+HashTable.prototype.doubleHash = function (currentSize) {
+
+  //set new size to double what it was
+  this.SIZE = currentSize * 2;
+
+  //rehash all k/v pairs in new 
+  let newTable = new HashTable;
+
+  // I think maybe this is blowing the call stack because it's iterating over 
+  // all the k/v pairs in each obj's prototype chain, but I ran out of time here.
+  // would next try converting obj to array with Object.entries and iterating over those.
+  this.storage.forEach(obj => {
+    for (let k in obj) {
+      newTable.set(k, obj[k]);
+    }
+  })
+  //set this.storage = to the rehashed values in a hashTable which contains the new size of buckets.
+  this.storage = newTable.storage;
+}
+
+// ran out of time and didn't get here yet
+// HashTable.prototype.halveHash = function(currentSize) {
+
+//   //set new size to half what it was
+//   this.SIZE = currentSize / 2;
+// }
+
+const myTable = new HashTable;
+myTable.set("a");
+myTable.set("b");
+myTable.set("c");
+myTable.set("d");
+myTable.set("e");
+myTable.set("f");
+myTable.set("g");
+myTable.set("h");
+myTable.set("i");
+myTable.set("j");
+myTable.set("k");
+myTable.set("l");
+console.log(myTable);
 
 
 // YOUR CODE ABOVE
 
 function hashCode(string, size) {
   'use strict';
-  
+
   let hash = 0;
   if (string.length === 0) return hash;
-  
+
   for (let i = 0; i < string.length; i++) {
     const letter = string.charCodeAt(i);
     hash = ((hash << 5) - hash) + letter;
     hash = hash & hash; // Convert to 32bit integer
   }
-  
+
   return Math.abs(hash) % size;
 }
 
--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@
 */
 function HashTable() {
   this.SIZE = 16;
-  
+
   this.storage = new Array(this.SIZE);
 }
 
@@ -23,9 +23,30 @@ function HashTable() {
 * @param {string|number|boolean} value - value to be stored in hash table
 * @return {number} The new number of items stored in the hash table
 */
-HashTable.prototype.set = function(key, value) {
+HashTable.prototype.set = function (key, value) {
+  const code = hashCode(key, this.SIZE);
 
+
+  if (!this.storage[code]) {
+    //if bucket is empty,
+    //create an object at that bucket in the hashTable
+    this.storage[code] = {};
+    // store key/value pair at that bucket
+    this.storage[code][key] = value;
+  } else {
+    //if bucket already contains an object,
+    //store or overwrite key/value pair at that bucket
+    this.storage[code][key] = value;
+  }
+  //set should not return anything
 };
+
+// const myTable = new HashTable;
+// console.log(myTable);
+// console.log(myTable.set("chris", "flannery"));
+// console.log(myTable);
+// console.log(myTable.set("shric", "wills"));
+// console.log(myTable);
 
 /**
 * get - Retrieves a value stored in the hash table with a specified key
@@ -37,9 +58,33 @@ HashTable.prototype.set = function(key, value) {
 * @return {string|number|boolean} The value stored with the specifed key in the
 * hash table
 */
-HashTable.prototype.get = function(key) {
+HashTable.prototype.get = function (key) {
+  const code = hashCode(key, this.SIZE);
 
+
+  if (!this.storage[code]) {
+    //if there is nothing stored in bucket at code
+    console.log("Hmm, it looks like there's nothing here to get!");
+  } else if (!this.storage[code][key]) {
+    //if there is an object stored in bucket at code
+    //...but the key you are looking for is not inside
+    console.log("There is something stored here, but it's not what you are looking for!")
+  } else {
+    //if there is an object stored in bucket at code
+    ///...and the key you are looking for is inside, return it
+    return this.storage[code][key];
+  }
 };
+
+// const myTable = new HashTable;
+// console.log(myTable);
+// console.log(myTable.set("chris", "flannery"));
+// console.log(myTable);
+// console.log(myTable.set("shric", "wills"));
+// console.log(myTable);
+// console.log(myTable.get("chris"));
+// console.log(myTable.get("hrsic"));
+// console.log(myTable.get("beans"));
 
 /**
 * remove - delete a key/value pair from the hash table
@@ -49,24 +94,48 @@ HashTable.prototype.get = function(key) {
 * @param {string} key - key to be found and deleted in hash table
 * @return {string|number|boolean} The value deleted from the hash table
 */
-HashTable.prototype.remove = function(key) {
+HashTable.prototype.remove = function (key) {
+  const code = hashCode(key, this.SIZE);
 
+  if (!this.storage[code]) {
+    //if there is nothing stored in bucket at code
+    console.log("Hmm... it looks like this bucket is already empty!");
+  } else if (!this.storage[code][key]) {
+    //if there is an object stored in bucket at code
+    //...but the key you are looking for is not inside
+    console.log("Hmmm... looks like the key/value pair you are trying to remove is not present here.")
+  } else {
+    //if there is an object stored in bucket at code
+    //...and the key you are looking for is inside, remove it
+    delete this.storage[code][key];
+  }
 };
+
+// const myTable = new HashTable;
+// console.log(myTable);
+// console.log(myTable.set("chris", "flannery"));
+// console.log(myTable);
+// console.log(myTable.set("shric", "wills"));
+// console.log(myTable);
+// myTable.remove("chris");
+// console.log(myTable);
+// myTable.remove("chris");
+// myTable.remove("beans");
 
 
 // Do not modify
 function hashCode(string, size) {
   'use strict';
-  
+
   let hash = 0;
   if (string.length === 0) return hash;
-  
+
   for (let i = 0; i < string.length; i++) {
     const letter = string.charCodeAt(i);
     hash = ((hash << 5) - hash) + letter;
     hash = hash & hash; // Convert to 32bit integer
   }
-  
+
   return Math.abs(hash) % size;
 }
 
