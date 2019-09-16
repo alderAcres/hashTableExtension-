6840a1b2235b02a75e6dc0f55949b8832fcd495e
Brian Haller <brianhaller@Brians-MacBook-Pro.local>
2019-09-16 10:11:44 -0400

    finished main and happy with tests, did not get to test resizing, but hopefully implementation works

--- src/extension.js
+++ src/extension.js
@@ -15,7 +15,155 @@
 
 // PASTE AND MODIFY YOUR CODE BELOW
 
+function HashTable() {
+  this.SIZE = 16;
+  this.numElements = 0;
 
+  this.storage = new Array(this.SIZE);
+}
+
+/**
+* set - Adds given value to the hash table with specified key.
+*
+* - If the provided key has already been used to store another value, simply overwrite
+*   the existing value with the new value.
+* - If the hashed address already contains another key/value pair, you must handle
+*   the collision appropriately.
+*
+* @param {string} key - key to be used to create hashed address
+* @param {string|number|boolean} value - value to be stored in hash table
+* @return {number} The new number of items stored in the hash table
+*/
+HashTable.prototype.set = function(key, value) {
+  // hash a string of the key
+  const hashed = hashCode(key.toString(), this.SIZE)
+
+  // use hash to save an object of the k/v pair
+  var bucket = this.storage[hashed];
+
+  // if this.storage @ hash key is undefined, create a new bucket
+  if (bucket === undefined) {
+    var bucket = [];
+    this.storage[hashed] = bucket;
+  }
+
+  let conflicting = false;
+  // iterate through our bucket to see if there are any conflicting
+  // k/v pairs within our bucket. If there are any, override them.
+  for (let i = 0; i < bucket.length; i++) {
+    let tuple = bucket[i];
+    // if the bucket[i] contains the same key, over write the saved value
+    // associated with that key, otherwise, push new array in
+    if (tuple[0] === key) {
+      tuple[1] = value;
+      conflicting = true;
+    }
+  }
+
+  // if no conflicting k/v pairs, push array of k/v into the bucket
+  // if the push makes this.Size more than 75% full, double size
+  if (!conflicting) {
+    bucket.push([key, value]);
+    this.numElements += 1;
+    if (this.numElements > this.SIZE * 0.75) {
+      this.resize(this.SIZE * 2);
+    }
+  }
+  return this.numElements;
+};
+
+/**
+* get - Retrieves a value stored in the hash table with a specified key
+*
+* - If more than one value is stored at the key's hashed address, then you must retrieve
+*   the correct value that was originally stored with the provided key
+*
+* @param {string} key - key to lookup in hash table
+* @return {string|number|boolean} The value stored with the specifed key in the
+* hash table
+*/
+HashTable.prototype.get = function(key) {
+  const hashed = hashCode(key.toString(), this.SIZE);
+  const bucket = this.storage[hashed];
+
+  // if there is no bucket at the hashed key, return nothing stored at key
+  if (bucket === undefined) {
+    return "Nothing stored at this key.";
+  }
+
+  // otherwise, loop through the buckets and return the value when the
+  // key provided as an argument is found
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === key) {
+      return tuple[1];
+    }
+  }
+
+  // if the key doesn't match anything in hashed bucket, return error msg
+  return "Nothing stored at this key.";
+};
+
+/**
+* remove - delete a key/value pair from the hash table
+*
+* - If the key does not exist in the hash table, return undefined
+*
+* @param {string} key - key to be found and deleted in hash table
+* @return {string|number|boolean} The value deleted from the hash table
+*/
+HashTable.prototype.remove = function(key) {
+  let hashed = hashCode(key.toString(), this.SIZE);
+  var bucket = this.storage[hashed];
+
+  if (!bucket) {
+    return null;
+  }
+  // iterate over the bucket
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    // check to see if key is inside bucket
+    if (tuple[0] === key) {
+      // if it is, get rid of this tuple
+      bucket.splice(i, 1);
+      this.numElements -= 1;
+      // if this removal opens up more than 75% FREE space,
+      // reduce size of the hash table by half
+      if (this.numElements < this.SIZE * 0.25) {
+        // don't reduce table size if size is less than 16
+        if (this.SIZE >= 16) {
+          this.resize(this.SIZE / 2);
+        }
+      }
+      return tuple[1];
+    }
+  }
+};
+
+HashTable.prototype.resize = function(newLimit) {
+  // create a copy of data stored
+  let oldStorage = this.storage;
+
+  // reset data in hash table
+  this.SIZE = newLimit;
+  this.numElements = 0;
+  this.storage = [];
+
+  // iterate through saved hashtable and 
+  // reassign new hash keys to each saved k/v pair
+  oldStorage.forEach(function(bucket) {
+    if (!bucket) {
+      return;
+    }
+    else {
+      this.numElements += 1;
+    }
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      this.set(tuple[0], tuple[1]);
+    }
+  }.bind(this));
+};
 
 // YOUR CODE ABOVE
 
