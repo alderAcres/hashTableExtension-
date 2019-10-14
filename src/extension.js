23d5a47a3721ee086625b863ce5b4107122cf4b7
Mychal Estalilla <mychal.es@protonmail.com>
2019-10-14 10:16:30 -0700

    start on extension.js; doubling size works; issue with reshashing previously stored values

--- src/extension.js
+++ src/extension.js
@@ -14,7 +14,84 @@
 */
 
 // PASTE AND MODIFY YOUR CODE BELOW
+function HashTable() {
+  this.SIZE = 2;
 
+  this.storage = new Array(this.SIZE);
+}
+
+
+HashTable.prototype.set = function(key, value) {
+  const index = hashCode(key, this.SIZE);
+  let numItems = 0;
+
+  const counter = () => {
+	this.storage.forEach( bucket => {
+	  for (data in bucket) {
+		numItems++;
+	  }
+	});
+  };
+
+  counter();
+
+
+  // check if new bucket is needed
+  if (!this.storage[index] && numItems >=(this.SIZE * .75)) {
+
+	// if so, double storage size
+	console.log('increasing storage size');
+	this.SIZE = this.SIZE * 2;
+	console.log(this.SIZE);
+
+	const tempArr = this.storage;
+
+	// delete storage
+	this.storage = new Array(this.SIZE);
+
+	// then rehash every item
+	tempArr.forEach( bucket => {
+	  for (data in bucket) {
+		let oldIndex = hashCode(data, (this.SIZE / this.SIZE));
+		let newIndex = hashCode(data, this.SIZE);
+		let val = value;
+		if (tempArr[newIndex]) {
+		  val = tempArr[oldIndex][data];
+		  console.log(val);
+		}
+  		if (!this.storage[newIndex]) this.storage[newIndex] = {};
+		this.storage[newIndex][data] = val;
+	  }
+	});
+
+
+	// and finally set new item
+	const newItemIndex = hashCode(key, this.SIZE);
+	if (!this.storage[newItemIndex]) this.storage[newItemIndex] = {};
+	this.storage[newItemIndex][key] = value;
+
+  } else {
+
+	// otherwise set key-val as normal
+	if (!this.storage[index]) this.storage[index] = {};
+	this.storage[index][key] = value;
+
+  }
+
+
+
+  counter();
+
+  console.log(this.storage);
+  return numItems;
+};
+
+const ht = new HashTable();
+
+ht.set('hello', 'world');
+ht.set('mychal', 'estalilla');
+ht.set('code', 'smith');
+ht.set('k', 'one');
 
 
 // YOUR CODE ABOVE
