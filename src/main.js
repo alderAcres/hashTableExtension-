d50a01941023de8144d39cae4b573b2ce1029b54
Sejan Miah <sejan.miah@gmail.com>
2019-11-04 10:00:31 -0500

    Attempted bonus

--- src/main.js
+++ src/main.js
@@ -28,17 +28,24 @@ console.log(hash)
 * @return {number} The new number of items stored in the hash table
 */
 
-console.log( hashCode("hello")
 HashTable.prototype.set = function(key, value) {
-
-
-
   //to set a property first access whether or not a value exists for the particular index
   //if it goes add on the value into that particular index with your key, if there is a key overrwrite it
-
   //if there is no key/value pairing at the index you are accessing then you create a new object and add it onto that index
+  const index = hashCode(key, this.SIZE)
+  if (this.storage[index]) {
+    this.storage[index][key] = value
+  } else {
+    this.storage[index] = { [key] : value}
+  }
 
 };
+
+console.log(hash.set("hello", "goodbye"))
+hash.set("hello", "byebye")
+hash.set("dua", "lipa")
+hash.set("feelin", "myself")
+console.log(hash)
 
 /**
 * get - Retrieves a value stored in the hash table with a specified key
@@ -51,8 +58,20 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
+  //check to see if the key exists at the specific index that it was genereated for
+  //if it doesn't exist then you should return undefined
+  //if it does exist return the VALUE
+  const index = hashCode(key, this.SIZE)
+  console.log(index)
 
+  if(!this.storage[index]) {
+    return 
+  } else {
+    console.log(this.storage[index][key])
+    return this.storage[index][key]
+  }
 };
+
 
 /**
 * remove - delete a key/value pair from the hash table
@@ -63,8 +82,25 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
-
+  //as always first get the index asssociated with your key
+  const index = hashCode(key, this.SIZE) 
+  if(!this.storage[index]) {
+    return
+  } else {
+    const removedValue = this.storage[index][key]
+    delete this.storage[index][key]
+    return removedValue
+  }
 };
+
+console.log(hash.remove("hello"))
+console.log(hash.remove("hello"))
+console.log(hash)
+console.log(hash.remove("dua"))
+console.log(hash)
+
+console.log(hash.remove("dua"))
+
 
 
 // Do not modify
