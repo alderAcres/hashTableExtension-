3304d5e6edf2230ae41c212ed37fd33696f9f436
Stanford Dai <stanfordhdai@gmail.com>
2019-12-02 10:11:18 -0800

    week 1 assessment submission 1st try

--- src/extension.js
+++ src/extension.js
@@ -15,6 +15,106 @@
 
 // PASTE AND MODIFY YOUR CODE BELOW
 
+function HashTable() {
+  this.SIZE = 2;  
+  this.storage = new Array(this.SIZE);
+}
+
+/**
+* set - Adds given value to the hash table with specified key.
+*/
+HashTable.prototype.set = function(key, value) {
+  const hashVal = hashCode(key, this.SIZE);
+  if (!this.storage[hashVal]) {
+    this.storage[hashVal] = {[key]: value};
+  }
+  this.storage[hashVal][key] = value;
+
+  // create counter to track # of undefined properties
+  let count = 0;
+  for (let i = 0; i < this.storage.length; i++) {
+    if (this.storage[i] !== undefined) {
+      count += 1;
+    }
+  }
+  
+  // check undefined properties against size
+  let maxStorage = 0.75 * this.SIZE;
+  // if count is larger than max storage, double the size
+  if (count > maxStorage) {
+    this.SIZE = this.SIZE * 2;
+    // reset all key value pairs to undefined
+    for (let i = 0; i < this.storage.length; i++) {
+      if (this.storage[i] !== undefined) {
+        this.set(key, undefined);
+      }
+    }
+    // rerun hashcode function and store key
+    const hashVal2 = hashCode(key, this.SIZE);
+    if (!this.storage[hashVal2]) {
+      this.storage[hashVal2] = {[key]: value};
+    }
+    this.storage[hashVal2][key] = value;
+  }
+};
+
+// const newHash = new HashTable();
+// console.log(newHash);
+
+// newHash.set('mood', 'happy');
+// newHash.set('time', '8oclock');
+// newHash.set(3, 3);
+// console.log(newHash);
+// console.log('size ', newHash.SIZE);
+
+/**
+* get - Retrieves a value stored in the hash table with a specified key
+*/
+HashTable.prototype.get = function(key) {
+  const hashVal = hashCode(key, this.SIZE);
+  if (this.storage[hashVal]) {
+    return this.storage[hashVal][key];
+  };
+  return undefined;
+};
+
+// console.log(newHash.get('mood'));
+
+
+/**
+* remove - delete a key/value pair from the hash table
+*/
+HashTable.prototype.remove = function(key) {
+  const getKey = this.get(key);
+  this.set(key, undefined);  // if we assign the value of undefined to key, the key still exists in the object with the value undefined. How can we remove it?
+  
+  // create count to track # of properties after removal
+  let count = 0;
+  for (let i = 0; i < this.storage.length; i++) {
+    if (this.storage[i] !== undefined) {
+      count += 1;
+    }
+  }
+
+  // create variable for minimum storage
+  let minStorage = 0.25 * this.SIZE;
+  // check if size is greater than 16 and count is less than minStorage
+  if (this.SIZE > 16 && count < Math.floor(minStorage)) {
+    this.SIZE = this.SIZE / 2; // does this reset my table elements to undefined or simply remove everything above the size count?
+
+    // rehash everything
+    const hashVal2 = hashCode(key, this.SIZE);
+    if (!this.storage[hashVal2]) {
+      this.storage[hashVal2] = {[key]: value};
+    }
+    this.storage[hashVal2][key] = value;
+  }
+  // return removed item
+  return getKey;
+}
+
+// newHash.remove('time');
+// console.log('removed time ', newHash);
 
 
 // YOUR CODE ABOVE
--- src/main.js
+++ src/main.js
@@ -24,8 +24,19 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
+  const hashVal = hashCode(key, this.SIZE);
+  if (!this.storage[hashVal]) {
+    this.storage[hashVal] = {[key]: value};
+  }
+  this.storage[hashVal][key] = value;
 };
+
+const newHash = new HashTable();
+console.log(newHash);
+
+newHash.set('mood', 'happy');
+newHash.set('time', '8oclock');
+console.log(newHash);
 
 /**
 * get - Retrieves a value stored in the hash table with a specified key
@@ -38,8 +49,15 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  const hashVal = hashCode(key, this.SIZE);
+  if (this.storage[hashVal]) {
+    return this.storage[hashVal][key];
+  };
+  return undefined;
 };
+
+console.log(newHash.get('mood'));
+
 
 /**
 * remove - delete a key/value pair from the hash table
@@ -50,9 +68,13 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
-
+  const getKey = this.get(key);
+  this.set(key, undefined);  // if we assign the value of undefined to key, the key still exists in the object with the value undefined. How can we remove it?
+  return getKey;
 };
 
+newHash.remove('time');
+console.log('removed time ', newHash);
 
 // Do not modify
 function hashCode(string, size) {
