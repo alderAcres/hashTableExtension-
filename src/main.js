a79948912d3e08083751a08632779ccd4fcf4c59
Connor Bovino <connor.bovino@gmail.com>
2019-12-30 10:18:58 -0500

    assessment

--- src/main.js
+++ src/main.js
@@ -6,10 +6,20 @@
 * - You may modify this constructor as you need to achieve the challenges below.
 */
 function HashTable() {
+  // represents the spots in array or objects in storage
   this.SIZE = 16;
-  
   this.storage = new Array(this.SIZE);
+
+  //represents the key and value pairs within hashtable
+  this.number = 0;
+
 }
+
+
+//constructs a new Hashtable object and assigns it to ht
+const ht = new HashTable
+
+
 
 /**
 * set - Adds given value to the hash table with specified key.
@@ -24,8 +34,41 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
+  // pass the key and size into hashcode, assign it to const index. The index will then be the correct index in storage
+  const index = hashCode(key, this.SIZE)
+
+  //check if spot in index is undefined
+  //if undefined, at index create a new object and as key value pair.
+  if (this.storage[index] === undefined){
+    this.storage[index] = {}
+    this.storage[index][key] = value
+    this.number += 1
+  } else {
+    // if object exists at index, add key and value pair
+
+    let tempObj = {}
+    tempObj[key] = value
+    let oldObj = this.storage[index]
+    
+    this.storage[index][key] = value
+  
+
+  }
+
+  // return the number of key value pairs in  hashtable
+  return this.number
 
 };
+
+//Tests
+ht.set("test", "round 1")
+ht.set("test2", "round 2")
+ht.set("test", "round 3")
+
+// console.log(ht.storage)
+
+
+
 
 /**
 * get - Retrieves a value stored in the hash table with a specified key
@@ -38,8 +81,18 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
+  //correct index of item in storage
+  const index = hashCode(key, this.SIZE)
+  
+  // returns value of key at the correct index in storage
+  return this.storage[index][key]
 
 };
+
+//get
+
+// console.log(ht.get("test"))
+
 
 /**
 * remove - delete a key/value pair from the hash table
@@ -50,8 +103,36 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
+  //correct index of item in storage
+  const index = hashCode(key, this.SIZE)
+
+  // if the value of the key at the correct index in hashtable is undefined return undefined
+  if (this.storage[index][key] === undefined) return undefined
+
+  //store the value key and value that will be deleted
+  const storedObject = this.storage[index][key]
+
+
+  // decrease number if object empty
+  if(this.storage[index] === {} ){
+    this.storage[index] === undefined
+    this.number -= 1;
+  }
+
+
+
+  // delete the item
+  delete this.storage[index][key]
+
+  //return the deleted item
+  return storedObject
+
 
 };
+
+//tests
+console.log(ht.remove("test"))
+console.log(ht.storage)
 
 
 // Do not modify
