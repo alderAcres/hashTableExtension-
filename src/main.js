b9dad8f6ed4419d3d3fa79f259930f64216f0e66
Mitchel Severe <truth531@gmail.com>
2019-12-30 12:02:24 -0500

    finish main.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@
 */
 function HashTable() {
   this.SIZE = 16;
-  
   this.storage = new Array(this.SIZE);
 }
 
@@ -23,8 +22,12 @@ function HashTable() {
 * @param {string|number|boolean} value - value to be stored in hash table
 * @return {number} The new number of items stored in the hash table
 */
-HashTable.prototype.set = function(key, value) {
-
+HashTable.prototype.set = function (key, value) {
+  const hashed = hashCode(key, this.SIZE);
+  if (!this.storage[hashed]) {
+    this.storage[hashed] = {};
+  }
+  this.storage[hashed][key] = value;
 };
 
 /**
@@ -37,8 +40,9 @@ HashTable.prototype.set = function(key, value) {
 * @return {string|number|boolean} The value stored with the specifed key in the
 * hash table
 */
-HashTable.prototype.get = function(key) {
-
+HashTable.prototype.get = function (key) {
+  const hashed = hashCode(key, this.SIZE);
+  return this.storage[hashed][key];
 };
 
 /**
@@ -49,24 +53,28 @@ HashTable.prototype.get = function(key) {
 * @param {string} key - key to be found and deleted in hash table
 * @return {string|number|boolean} The value deleted from the hash table
 */
-HashTable.prototype.remove = function(key) {
-
+HashTable.prototype.remove = function (key) {
+  const hashed = hashCode(key, this.SIZE);
+  if (!this.storage[hashed] || !this.storage[hashed][key]) return undefined;
+  const popped = this.storage[hashed][key];
+  delete this.storage[hashed][key];
+  return popped;
 };
 
 
 // Do not modify
 function hashCode(string, size) {
   'use strict';
-  
+
   let hash = 0;
   if (string.length === 0) return hash;
-  
+
   for (let i = 0; i < string.length; i++) {
     const letter = string.charCodeAt(i);
     hash = ((hash << 5) - hash) + letter;
     hash = hash & hash; // Convert to 32bit integer
   }
-  
+
   return Math.abs(hash) % size;
 }
 
