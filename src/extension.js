c919625d81f14b5465226d01c43bb1c2828c34dc
rbargues <rbargues@gmail.com>
2020-02-18 09:48:21 -0500

    starting resizing

--- src/extension.js
+++ src/extension.js
@@ -14,9 +14,127 @@
 */
 
 // PASTE AND MODIFY YOUR CODE BELOW
+/**
+* HashTable costructor
+*
+* construct a new hash table
+*
+* - You may modify this constructor as you need to achieve the challenges below.
+*/
+function HashTable() {
+  this.SIZE = 16;
+  
+  this.storage = new Array(this.SIZE);
+}
+
+/**
+* set - Adds given value to the hash table with specified key.
+*
+* - If the provided key has already been used to store another value, simply overwrite
+*   the existing value with the new value.
+* - If the hashed address already contains another key/value pair, you must handle
+*   the collision appropriately.
+*
+* @param {string} key - key to be used to create hashed address
+* @param {string|number|boolean} value - value to be stored in hash table
+* @return {number} The new number of items stored in the hash table
+*/
+
+HashTable.prototype.set = function(key, value) {
+  const hashedKey = hashCode(key, this.SIZE);
+  if (this.storage[hashedKey]) {
+    // collision. we will use an object for sake of ease
+    const cellStorage = {};
+    let arrayedCellStorage = Object.keys(this.storage[hashedKey]);
+    for (let i = 0; i < arrayedCellStorage.length; i ++) {
+      let cellKey = arrayedCellStorage[i];
+      let cellVal = this.storage[hashedKey][cellKey];
+      cellStorage[cellKey] = cellVal;
+    }
+     cellStorage[key] = value;
+     this.storage[hashedKey] = cellStorage;
+  }
+  else {
+    // no collision
+    const cellStorage = {};
+    cellStorage[key] = value;
+    this.storage[hashedKey] = cellStorage;
+  }
+
+  // need to check if >= 0.75 usage
+  if ((this.storage.length/this.SIZE) >= 0.75) {
+    //resize required
+    let tempStorage = {};
+    tempStorage = Object.assign(tempStorage, this.storage);
+    // we probably need to deep copy this otherwise we are pointing to the same item
+
+    this.SIZE = (this.SIZE * 2);
+    this.storage = new Array(this.SIZE);
+    for (let i = 0; i < tempStorage.length; i += 1) {
+      let cellStorage = Object.keys(tempStorage[i]);
+      // this is an array of the keys in the object in that cell
+      for (let j = 0; j < cellStorage.length; j ++) {
+        this.set(cellStorage[j],tempStorage[cellStorage[j]]);
+      }
+    }
+  }
+
+};
+
+/**
+* get - Retrieves a value stored in the hash table with a specified key
+*
+* - If more than one value is stored at the key's hashed address, then you must retrieve
+*   the correct value that was originally stored with the provided key
+*
+* @param {string} key - key to lookup in hash table
+* @return {string|number|boolean} The value stored with the specifed key in the
+* hash table
+*/
+HashTable.prototype.get = function(key) {
+  const hashedKey = hashCode(key, this.SIZE);
+  const cellStorage = this.storage[hashedKey];
+  //given our previous implementation cellStorage should be an object
+  let arrayedCellStorage = Object.keys(this.storage[hashedKey]);
+  console.log(arrayedCellStorage)
+  for (let i = 0; i < arrayedCellStorage.length; i++) {
+    let cellKey = arrayedCellStorage[i];
+    if (String(key) === cellKey) {
+      return this.storage[hashedKey][cellKey];
+    }
+  }
+  // if we get to this point, the key is not in storage;
+  return null;
+};
+
+/**
+* remove - delete a key/value pair from the hash table
+*
+* - If the key does not exist in the hash table, return undefined
+*
+* @param {string} key - key to be found and deleted in hash table
+* @return {string|number|boolean} The value deleted from the hash table
+*/
+HashTable.prototype.remove = function(key) {
+  
+  if (!this.get(key)) return undefined;
+  // if we hit this point, we know the hashtable contains our key
+  const hashedKey = hashCode(key, this.SIZE);
+  let arrayedCellStorage = Object.keys(this.storage[hashedKey]);
+  const cellStorage = {};
+  for (let i = 0; i < arrayedCellStorage.length; i ++) {
+    let cellKey = arrayedCellStorage[i];
+    let cellVal = this.storage[hashedKey][cellKey];
+    if (cellKey === String(key)) continue;
+    cellStorage[cellKey] = cellVal;
+  }
+  this.storage[hashedKey] = cellStorage;
+  return true; // just returning something that is not undefined to confirm removal
+};
 
 
-
+// Do not remove!!
+module.exports = HashTable;
 // YOUR CODE ABOVE
 
 function hashCode(string, size) {
