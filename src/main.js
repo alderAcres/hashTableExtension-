e9040555c6d1c1715d38e1d90b2a39c05eee44f3
alex <allx02@hotmail.com>
2020-03-16 21:22:27 -0400

    first assessment

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@
 */
 function HashTable() {
   this.SIZE = 16;
-  
+  this.items = 0;
   this.storage = new Array(this.SIZE);
 }
 
@@ -24,8 +24,45 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
+  //address contains key/val pair?
+  const address = hashCode(key, this.SIZE)
+  let element = this.storage[address]
+  let current;
 
+  if (element) {
+    //key === key? override,
+    if (element[key]) {element[key] = value}
+
+      //add a 'next' key/val pair, pointing to next value in same address
+    else {
+      while (element.next) {
+        //find last element with .next
+        current = current.next
+      }
+      element.next = {key:value}
+    }
+
+  } else { //!element?
+    this.storage[address] = {}
+    this.storage[address][key] = value;
+  }
+  this.items++;
+  return this.items;
 };
+
+let testTable = new HashTable()
+console.log(testTable)
+
+testTable.set('First', '1st')
+console.log(testTable);
+
+testTable.set('First', 'NEW') //same key, new Value should be reassigned
+console.log(testTable)
+//Why are my keys always set to 'key' and not 'First'???//Improperly created an Object. - Hour lost.
+testTable.set('Firstt', '1st')
+console.log(testTable);
+testTable.set('Firsttt', '1st')
+console.log(testTable);
 
 /**
 * get - Retrieves a value stored in the hash table with a specified key
@@ -38,8 +75,22 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
+  let current;
+  const address = hashCode(key, this.SIZE)
+  if (this.storage[address][key]) {
+    return this.storage[address][key];
+  } //check if address has .next
+  else if (this.storage[address][next]) {
+    current = this.storage[address][next];
+    while (current[next]) { //check if we have a next, or if key is found
+      current = current[next]
+      if (current[key]) {return current[key]}
+    }
+    return 'key does not exist'
+  }
+}
 
-};
+console.log(testTable.get('Firstt'))
 
 /**
 * remove - delete a key/value pair from the hash table
@@ -57,16 +108,16 @@ HashTable.prototype.remove = function(key) {
 // Do not modify
 function hashCode(string, size) {
   'use strict';
-  
+
   let hash = 0;
   if (string.length === 0) return hash;
-  
+
   for (let i = 0; i < string.length; i++) {
     const letter = string.charCodeAt(i);
     hash = ((hash << 5) - hash) + letter;
     hash = hash & hash; // Convert to 32bit integer
   }
-  
+
   return Math.abs(hash) % size;
 }
 
