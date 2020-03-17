fb662134391dddde3b85ee4caa95b6ab19c3bf67
Jeffrey C. Lu <ceeloo@gmail.com>
2020-03-16 20:49:36 -0400

    finished main.js

--- src/main.js
+++ src/main.js
@@ -24,8 +24,28 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
+  //create the index using hash function
+  const index = hashCode(key, this.SIZE);
+  console.log(index)
+  //check if anything exists in bucket
+  if (this.storage[index]) {
+    //add key value pair to bucket
+    this.storage[index][key] = value;
+  }
+  //otherwise store a key/value pair there
+  else {
+    //initialize a new holding object
+    this.storage[index] = {};
+    //store input key/value at storage object
+    this.storage[index][key] = value;
+  }
+  console.log(this.storage[0])
 };
+
+const myHash = new HashTable();
+myHash.set('key 1', 'value 1')
+myHash.set('key 2', 'value 2')
+console.log(myHash)
 
 /**
 * get - Retrieves a value stored in the hash table with a specified key
@@ -38,8 +58,21 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  //create the index using the hash function
+  const index = hashCode(key, this.SIZE);
+  //check if anything exists at the bucket
+  console.log(this.storage[index]);
+  if (this.storage[index]) {
+    //return the value stored at the input key
+    return this.storage[index][key];
+  }
+  //otherwise do nothing
+  return;
 };
+
+console.log(myHash.get('key 1')) //->'value 1'
+console.log(myHash.get('key 2')) //->'value 2'
+console.log(myHash.get('jeffrey')) //->'undefined'
 
 /**
 * remove - delete a key/value pair from the hash table
@@ -50,8 +83,29 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
-
+  //create index using hash function
+  const index = hashCode(key, this.SIZE);
+  //check if there is anything at the bucket
+  console.log(this.storage[index])
+  if (this.storage[index]) {
+    //capture value to return before deleting it
+    const returnVal = this.storage[index][key]
+    //delete the value at the input key
+    delete this.storage[index][key];
+    //check length of storage object at current bucket
+      //set storage object to undefined if object size is 0
+    console.log(Object.keys(this.storage[index]).length);
+    if (Object.keys(this.storage[index]).length === 0) this.storage[index] = undefined;
+    return returnVal;
+  }
+  //otherwise do nothing
+  return;
 };
+
+console.log(myHash.remove('key 1')) //'value 1'
+console.log(myHash)
+console.log(myHash.set('key 1', 'value 1'))
+console.log(myHash)
 
 
 // Do not modify
