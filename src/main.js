ffbad1a37da57afded3522e863eb5f839f81a4ff
michael coker <mikey@michaels-MacBook-Pro.local>
2020-03-23 11:18:01 -0600

    finished

--- src/main.js
+++ src/main.js
@@ -24,7 +24,28 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
+  //hash creates the address/index for they key and value
+  const hash = hashCode(key, this.storage.length)
 
+  if(this.storage[hash] === undefined){
+    //if there is no values or keys in the hash address then add a key and value there
+    this.storage[hash] = [
+      [key, value]
+    ]
+  } else if(this.storage[hash]){
+    if(this.storage[hash][0] !== undefined){
+      for(let i = 0; i < this.storage[hash].length; i++){
+        if(key !== this.storage[hash][i][0]){
+          const bucket = [key, value]
+          this.storage[hash].push(bucket)
+        }
+      }
+    } else {
+      const bucket = [key, value]
+      this.storage[hash].push(bucket)
+    }
+    //handles collision by chaining on a new key/value in the same hash address, but does not allow duplicate key/values
+  }
 };
 
 /**
@@ -38,7 +59,21 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
+  const hash = hashCode(key, this.storage.length)
+  let value;
 
+  if(this.storage[hash]) {
+    //loops through the storage at the hash address to check for multiple values
+    for(let i = 0; i < this.storage[hash].length; i++){
+      //makes sure that we are returning the correct value by checking to make sure the keys are equal
+      if(key === this.storage[hash][i][0]) {
+        value = this.storage[hash][i][1]
+        break
+      }
+    }
+    return value
+  }
+  return 'The value does not exist'
 };
 
 /**
@@ -50,7 +85,20 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
-
+  const hash = hashCode(key, this.storage.length)
+  
+  //checks to see it the hash exists in our storage
+  if(this.storage[hash]) {
+    //loops through the storage at the hash address to check for multiple values
+    for(let i = 0; i < this.storage[hash].length; i++){
+      //if the key is found at the hash address, then remove the key and value pair
+      if(key === this.storage[hash][i][0]){
+        this.storage[hash][i] = undefined
+      }
+    }
+  } else {
+    return undefined
+  }
 };
 
 
@@ -70,5 +118,17 @@ function hashCode(string, size) {
   return Math.abs(hash) % size;
 }
 
+const table = new HashTable()
+
+console.log(table.set('Mike', 'name'))
+console.log(table.set('Mike', 'name'))
+console.log(table.storage)
+console.log(table.get('Mike'))
+console.log(table.remove('Mike'))
+console.log(table.storage)
+console.log(table.set('Mike', 'name'))
+console.log(table.storage)
+
 // Do not remove!!
 module.exports = HashTable;
+
