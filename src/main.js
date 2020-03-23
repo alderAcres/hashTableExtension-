e6e6e780c66482e26c3ee212e6fe7bced04033cc
Marcus Valverde <marcusvalverde18@gmail.com>
2020-03-23 10:13:48 -0700

    completed assesemnt

--- src/main.js
+++ src/main.js
@@ -24,7 +24,32 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
+  // create a variabe to hold the index of the bucket ,the evaluated reslt 
+  // of invoking hashcode function below 
+  const idx  = hashCode(key,this.size)
+// if the current key exists in the hashtable  bucket overwirte the existing value 
+  if(this.storage[idx] === key){
+    this.storage[idx] = value; 
+  }else if (this.storage[idx].value){ 
+    // else if the hastable objects storage array at the current index already has 
+    // an object then access the current object using bracket and dot notation 
+    //and add a new key value pair to exisiting obj
+    this.storage[idx].key = {[key]: value}
+  }
+//iterate over the storage array usuin reduce 
+const itemCount = this.storage.reduce((acc,curr)=>{
+  if(typeof curr === null){
+    // if the storage array at curr idx doesnt contain an object 
+    // acc stays the same 
+    acc += 0 ; 
+  } else if (typeof curr === 'object'){ // if the curr el contains an object 
+    // use Obj,keys/length to find how many key value pairs are in the object.
+  let keyVal = Object.keys(curr); 
+  acc += keyVal.length; // add to result variable 
+  }
+  return acc
+}, 0) // acc =  starts at 0 to add all items in each current elemt object 
+return itemCount
 };
 
 /**
@@ -38,7 +63,18 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  // iterate through the storage array on the hastable obj 
+  this.storage.forEach((el)=>{
+    if(typeof el === 'object'){ // if the curr el contains an object 
+      // nested if conditional to check if the value we are looking for is there 
+      if(el.key){
+        // if its there return the value 
+        return el.key; 
+      }
+    }
+    return "the requested item is not in the hashtable" // if we iterate through the whole storage 
+    // array and do see the value then return an error message 
+  })
 };
 
 /**
@@ -50,6 +86,23 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
+  // iterate through the storage array on the hastable obj 
+  this.storage.forEach((el)=>{
+    if(typeof el === 'object'){ // if the curr el contains an object 
+      // nested if conditional to check if the value we are looking for is there 
+      if(el.key){// if ithe key is located in our element Obj 
+        // save the value in a variable 
+        const retKey = el.key; 
+        // delete that key value pair 
+        delete el.key; 
+        // rturn the deleted item 
+        return retKey
+
+      }
+    }
+    return "the requested item is not in the hashtable" // if we iterate through the whole storage 
+    // array and do see the value then return an error message 
+  })
 
 };
 
@@ -72,3 +125,14 @@ function hashCode(string, size) {
 
 // Do not remove!!
 module.exports = HashTable;
+
+// testing it out 
+// dec variable to equal new instance of hastable constructor 
+const testington  = new HashTable(); 
+testington.set('name','Juan');// should return curr count 1
+testington.set('age','34'); // should return curr count 2 
+testington.set('job','accountant'); // should return curr count 3 
+console.log(testington) // should show array of objects
+testington.get('age') // should return 34 
+testington.remove('job') // should return accountant 
+testington.remove('height') // should return "the requested item is not in the hashtable"
