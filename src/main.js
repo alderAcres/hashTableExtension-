47278aaf22de3c0c5821266bff5fa96687fe015f
Tony Shen <tshen815@gmail.com>
2020-03-23 10:14:53 -0700

    attempted set and get

--- src/main.js
+++ src/main.js
@@ -7,11 +7,11 @@
 */
 function HashTable() {
   this.SIZE = 16;
-  
+
   this.storage = new Array(this.SIZE);
 }
 
-/**
+/**`
 * set - Adds given value to the hash table with specified key.
 *
 * - If the provided key has already been used to store another value, simply overwrite
@@ -23,8 +23,14 @@ function HashTable() {
 * @param {string|number|boolean} value - value to be stored in hash table
 * @return {number} The new number of items stored in the hash table
 */
-HashTable.prototype.set = function(key, value) {
-
+HashTable.prototype.set = function (key, value) {
+  // create key for value
+  const index = hashCode(key, this.SIZE);
+  if (!this.storage[index]) {
+    this.storage[index] = new LinkedList();
+  } else {
+    this.storage[index].push(key, value);
+  }
 };
 
 /**
@@ -37,7 +43,7 @@ HashTable.prototype.set = function(key, value) {
 * @return {string|number|boolean} The value stored with the specifed key in the
 * hash table
 */
-HashTable.prototype.get = function(key) {
+HashTable.prototype.get = function (key) {
 
 };
 
@@ -49,7 +55,7 @@ HashTable.prototype.get = function(key) {
 * @param {string} key - key to be found and deleted in hash table
 * @return {string|number|boolean} The value deleted from the hash table
 */
-HashTable.prototype.remove = function(key) {
+HashTable.prototype.remove = function (key) {
 
 };
 
@@ -57,16 +63,16 @@ HashTable.prototype.remove = function(key) {
 // Do not modify
 function hashCode(string, size) {
   'use strict';
-  
+
   let hash = 0;
   if (string.length === 0) return hash;
-  
+
   for (let i = 0; i < string.length; i++) {
     const letter = string.charCodeAt(i);
     hash = ((hash << 5) - hash) + letter;
     hash = hash & hash; // Convert to 32bit integer
   }
-  
+
   return Math.abs(hash) % size;
 }
 
