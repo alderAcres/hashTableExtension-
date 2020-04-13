40721ea7829e5d2a758937a76b3a777f893a0710
Henry Black <blackhaj@gmail.com>
2020-04-13 09:11:41 -0500

    completed main, started extensions

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@
 */
 function HashTable() {
   this.SIZE = 16;
-  
+
+  // To count the number of items added to storage, including clashes
+  this.itemCount = 0
   this.storage = new Array(this.SIZE);
 }
 
@@ -24,8 +26,21 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
+  // Hash the key
+  let hashedKey = hashCode(key, this.SIZE);
+  // If object exists, add key value pair
+  if (this.storage[hashedKey]){
+    this.storage[hashedKey][key] = value
+  } else { // else, create object and add
+    this.storage[hashedKey] = {
+      [key]: value
+    }
+  }
+  // return the number of stored items after incrementing (inc. clashed)
+  return ++this.itemCount
 };
+
+
 
 /**
 * get - Retrieves a value stored in the hash table with a specified key
@@ -38,7 +53,14 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  // Hash the key
+  let hashedKey = hashCode(key, this.SIZE);
+  // Check if key value pair exists and return if so
+  if (this.storage[hashedKey]){
+    return this.storage[hashedKey][key]
+  } else { // else, return item not found
+    return "item not found"  
+  }
 };
 
 /**
@@ -50,8 +72,21 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
-
+  // Hash the key
+  let hashedKey = hashCode(key, this.SIZE);
+  // Check for key value pair at location
+  let returned = this.storage[hashedKey][key]
+  // If key value pair exist, delete them but return the stored value and decrement item count
+  if (returned !== undefined){
+    delete this.storage[hashedKey][key]
+    this.itemCount--
+    return returned
+  } else { // else, return key not found
+    return "Key not found in storage"
+  }
 };
+
+
 
 
 // Do not modify
@@ -72,3 +107,16 @@ function hashCode(string, size) {
 
 // Do not remove!!
 module.exports = HashTable;
+
+let myHash = new HashTable()
+// console.log(myHash.set("henry", "black") + "SET")
+// console.log(myHash.set("address", "Bogota") + "SET")
+// console.log(myHash.storage)
+// console.log(myHash.get("henry") + "GET")
+// console.log(myHash.get("address") + "GET")
+// console.log("COUNT: " + myHash.itemCount)
+// console.log(myHash.remove("henry") + "REMOVE")
+// console.log("COUNT: " + myHash.itemCount)
+// console.log(myHash.remove("address") + "REMOVE")
+// console.log("COUNT: " + myHash.itemCount)
+console.log(myHash.storage[1])
