850cb73e20c47d46a0e7ac343d7d7591eedc63ad
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-04-13 10:06:57 -0400

    yevgeniy's assessment

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@
 * - You may modify this constructor as you need to achieve the challenges below.
 */
 function HashTable() {
-  this.SIZE = 16;
-  
-  this.storage = new Array(this.SIZE);
+  this.SIZE = 16; // size of hash
+  this.items = 0; // items currently the hash table holds
+  this.storage = new Array(this.SIZE); // array that holds the items
 }
 
 /**
@@ -24,7 +24,17 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
+  const hash = hashCode(key); // generate hash
+  if(this.storage[hash] === undefined){ // check if this hash holds any keys
+    this.storage[hash] = {}; // if not create an empty object that we will later mutate
+  }
+  if(this.storage[hash][key] === undefined){ // check if the key we are setting is new 
+                                            // to the table so we then increment
+                                            // this.items by one
+    this.items++;
+  }
+  this.storage[hash][key] = value; // set the value of the key stored at the storage's hash
+  return this.items;
 };
 
 /**
@@ -38,7 +48,8 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  const hash = hashCode(key); // generate hash
+  return this.storage[hash][key]; // grab they value stored at the key that the hash's object hold
 };
 
 /**
@@ -50,10 +61,37 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
+  const hash = hashCode(key); // generate code
+  if(this.storage[hash][key] === undefined) return undefined; // check if the key doesn't exist
 
+  const cachedVal = this.storage[hash][key]; // store the value the key holds
+  delete this.storage[hash][key]; // delete the key the hash holds
+  this.items--; // decrease the number of items the hash table holds
+  return cachedVal;
 };
 
-
+/*
+const table = new HashTable();
+table.set("foo", "dur");
+console.log(table.storage);
+console.log(table.items);
+table.set("foo", "dur");
+console.log(table.storage);
+console.log(table.items);
+table.set("foo", "bar");
+table.set("bar", "foo");
+console.log(table.items);
+console.log(table.storage);
+console.log(table.get("foo"));
+console.log(table.items);
+console.log(table.remove("foo"));
+console.log(table.items);
+console.log(table.remove("foo"));
+console.log(table.items);
+console.log(table.remove("bar"));
+console.log(table.storage);
+console.log(table.remove("dfdfd"));
+console.log(table.items);*/
 // Do not modify
 function hashCode(string, size) {
   'use strict';
@@ -69,6 +107,7 @@ function hashCode(string, size) {
   
   return Math.abs(hash) % size;
 }
+
 
 // Do not remove!!
 module.exports = HashTable;
