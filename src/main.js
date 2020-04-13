e08916cf8d24c770f9268eb1d2b00edc44bb2055
AP <ap@Allisons-MBP.cable.rcn.com>
2020-04-13 10:11:00 -0400

    hash table

--- src/main.js
+++ src/main.js
@@ -7,8 +7,18 @@
 */
 function HashTable() {
   this.SIZE = 16;
-  
   this.storage = new Array(this.SIZE);
+}
+
+function LinkedList() {
+  this.tail = null;
+  this.head = null;
+}
+
+function Node(key, value) {
+  this.key = key;
+  this.value = value;
+  this.next = null;
 }
 
 /**
@@ -24,8 +34,27 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
+  console.log(this.storage[(hashCode(key, this.SIZE))])
+  let index = (this.storage[hashCode(key, this.SIZE)]);
+  let node = new Node(key, value);
+  // if no collisions
+  if (index === undefined) {
+    index = new LinkedList();
+    index.head = index.tail = node;
+  }
+  // collisions
+  else {
+    index.tail.next = node;
+    index.tail = node;
+    console.log(index)
+  }
 };
+
+const hashTable = new HashTable();
+hashTable.set(0, 12);
+hashTable.set(1, 2);
+hashTable.set(1, 13);
+console.log(hashTable.storage[hashCode(1, 16)]);
 
 /**
 * get - Retrieves a value stored in the hash table with a specified key
@@ -38,7 +67,13 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  let index = (this.storage[hashCode(key, this.SIZE)]);
+  if (!index.head) return undefined;
+  let pointer = index.head;
+  while (pointer.key !== key || pointer.next !== null) {
+    pointer = pointer.next;
+  }
+  return pointer.key === key ? pointer.value : undefined;
 };
 
 /**
@@ -50,7 +85,39 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
-
+  let index = (this.storage[hashCode(key, this.SIZE)]);
+  if (!index.head) return undefined;
+  let pointer = index.head;
+  // if no collisions
+  if (pointer.key === key && !pointer.next) {
+    // get value of node
+    let val = pointer.value;
+    // reset value of index to undefined
+    index = undefined;
+    // return value of node
+    return val;
+    // if key matches head node in list
+  } else if (pointer.key === key && pointer.next) {
+    // get value of node
+      let val = pointer.value;
+      // reset head to next node in list
+      index.head = pointer.next;
+      pointer = undefined;
+      return val;
+  } else if (pointer.key !== key && pointer.next) {
+    while (pointer.next) {
+      // check if next node contains key
+      if (pointer.next.key === key) {
+        // get value of node w/ matching key
+        let val = pointer.next.value;
+        // reset current node's 'next' to reference the node after the one we are deleting
+        pointer.next = pointer.next.next;
+        return val;
+      }
+    }
+    // if we reach tail, return value if it matches key and undefined otherwise
+    return pointer.key === key ? pointer.value : undefined;
+  }
 };
 
 
