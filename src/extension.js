366f4cacb22e367b6e97ab25d8e24adecd43ff12
Madison Brown <mbrown3391@gmail.com>
2020-04-13 10:15:29 -0400

    completed

--- src/extension.js
+++ src/extension.js
@@ -13,11 +13,145 @@
         (rounding down), then reduce the hash table's SIZE by 1/2 and rehash everything.
 */
 
-// PASTE AND MODIFY YOUR CODE BELOW
+/**
+* HashTable costructor
+*
+* construct a new hash table
+*
+* - You may modify this constructor as you need to achieve the challenges below.
+*/
+function HashTable() {
+  this.SIZE = 16;
+  this.length = 0;
+  this.storage = new Array(this.SIZE);
+}
 
+function HashNode(key, value) {
+  this.key = key;
+  this.value = value;
+  this.next = null;
+}
+HashNode.prototype.last = function() {
+  let result = this;
+  while (result.next) {
+    result = result.next;
+  }
+  return result;
+}
 
+/**
+* set - Adds given value to the hash table with specified key.
+*
+* - If the provided key has already been used to store another value, simply overwrite
+*   the existing value with the new value.
+* - If the hashed address already contains another key/value pair, you must handle
+*   the collision appropriately.
+*
+* @param {string} key - key to be used to create hashed address
+* @param {string|number|boolean} value - value to be stored in hash table
+* @return {number} The new number of items stored in the hash table
+*/
+HashTable.prototype.set = function(key, value) {
+  if ((this.length + 1) / this.size > .75){
+    this.resize(this.size * 2);
+  }
 
-// YOUR CODE ABOVE
+  let hash = hashCode(key, this.SIZE);
+  let node = new HashNode(key, value);
+  if (this.storage[hash]){
+    this.storage[hash].last().next = node;
+  } else {
+    this.storage[hash] = node;
+  }
+  return ++this.length;
+};
+
+/**
+* get - Retrieves a value stored in the hash table with a specified key
+*
+* - If more than one value is stored at the key's hashed address, then you must retrieve
+*   the correct value that was originally stored with the provided key
+*
+* @param {string} key - key to lookup in hash table
+* @return {string|number|boolean} The value stored with the specifed key in the
+* hash table
+*/
+HashTable.prototype.get = function(key) {
+  let hash = hashCode(key, this.SIZE);
+
+  if (this.storage[hash] === undefined){
+    //bucket is empty, key cannot exist
+    return undefined;
+  }
+
+  //crawl linked list to find key
+  let result = this.storage[hash];
+  while (result.key !== key) {
+    if (result.next === null) {
+      //no more nodes in the list, key does not exist
+      return undefined;
+    }
+
+    result = result.next;
+  }
+
+  return result.value;
+};
+
+/**
+* remove - delete a key/value pair from the hash table
+*
+* - If the key does not exist in the hash table, return undefined
+*
+* @param {string} key - key to be found and deleted in hash table
+* @return {string|number|boolean} The value deleted from the hash table
+*/
+HashTable.prototype.remove = function(key) {
+  if (this.size > 16 && (this.length - 1) / this.size < .25){
+    this.resize(this.size / 2);
+  }
+
+  let hash = hashCode(key, this.SIZE);
+
+  if (this.storage[hash] === undefined) {
+    //bucket is empty, key cannot exist
+    return undefined;
+  }
+
+  //crawl linked list to find key
+  let parent = null;
+  let target = this.storage[hash]; 
+  while (target.key !== key) {
+    if (target.next === null) {
+      //no more nodes in the list, key does not exist
+      return undefined;
+    }
+
+    parent = target;
+    target = target.next;
+  }
+
+  //key exists
+  this.length -= 1;
+
+  let result;
+  if (parent === null) { 
+    //node to remove is head
+    if (target.next) {
+      this.storage[hash] = target.next;
+    } else { 
+      //node to remove is last value in bucket
+      delete this.storage[hash];
+    }
+  } else if (target.next === null) { 
+    //node to remove is tail
+    parent.next = null;
+  } else { 
+    //node to remove has parent and child which must be linked
+    parent.next = target.next;
+  }
+  return target.value;
+};
 
 function hashCode(string, size) {
   'use strict';
@@ -32,6 +166,22 @@ function hashCode(string, size) {
   }
   
   return Math.abs(hash) % size;
+}
+
+//change SIZE property and rehash all elements
+HashTable.prototype.resize = function(size) {
+  let temp = new HashTable();
+  temp.size = size;
+  //add all current elements to temp hashtable
+  for (let head of this.storage) {
+    while (head) {
+      temp.set(head.key, head.value);
+      head = head.next;
+    }
+  }
+
+  this.storage = temp.storage;
+  this.size = temp.size;
 }
 
 // Do not remove!!
--- src/main.js
+++ src/main.js
@@ -7,8 +7,21 @@
 */
 function HashTable() {
   this.SIZE = 16;
-  
+  this.length = 0;
   this.storage = new Array(this.SIZE);
+}
+
+function HashNode(key, value) {
+  this.key = key;
+  this.value = value;
+  this.next = null;
+}
+HashNode.prototype.last = function() {
+  let result = this;
+  while (result.next) {
+    result = result.next;
+  }
+  return result;
 }
 
 /**
@@ -24,7 +37,14 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
+  let hash = hashCode(key, this.SIZE);
+  let node = new HashNode(key, value);
+  if (this.storage[hash]){
+    this.storage[hash].last().next = node;
+  } else {
+    this.storage[hash] = node;
+  }
+  return ++this.length;
 };
 
 /**
@@ -38,7 +58,25 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
+  let hash = hashCode(key, this.SIZE);
 
+  if (this.storage[hash] === undefined){
+    //bucket is empty, key cannot exist
+    return undefined;
+  }
+
+  //crawl linked list to find key
+  let result = this.storage[hash];
+  while (result.key !== key) {
+    if (result.next === null) {
+      //no more nodes in the list, key does not exist
+      return undefined;
+    }
+
+    result = result.next;
+  }
+
+  return result.value;
 };
 
 /**
@@ -50,7 +88,46 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
+  let hash = hashCode(key, this.SIZE);
 
+  if (this.storage[hash] === undefined) {
+    //bucket is empty, key cannot exist
+    return undefined;
+  }
+
+  //crawl linked list to find key
+  let parent = null;
+  let target = this.storage[hash]; 
+  while (target.key !== key) {
+    if (target.next === null) {
+      //no more nodes in the list, key does not exist
+      return undefined;
+    }
+
+    parent = target;
+    target = target.next;
+  }
+
+  //key exists
+  this.length -= 1;
+
+  let result;
+  if (parent === null) { 
+    //node to remove is head
+    if (target.next) {
+      this.storage[hash] = target.next;
+    } else { 
+      //node to remove is last value in bucket
+      delete this.storage[hash];
+    }
+  } else if (target.next === null) { 
+    //node to remove is tail
+    parent.next = null;
+  } else { 
+    //node to remove has parent and child which must be linked
+    parent.next = target.next;
+  }
+  return target.value;
 };
 
 
@@ -72,3 +149,18 @@ function hashCode(string, size) {
 
 // Do not remove!!
 module.exports = HashTable;
+
+//tests
+let hashtable = new HashTable();
+hashtable.set(1, "one");
+hashtable.set(2, "two");
+hashtable.set(3, "three");
+console.log(hashtable.set(4, "four"), "(expect '4')");
+console.log(hashtable.get(1), "(expect 'one')");
+console.log(hashtable.get(2), "(expect 'two')");
+console.log(hashtable.get(4), "(expect 'four')");
+console.log(hashtable.get("five"), "(expect 'undefined')");
+console.log(hashtable.remove(4), "(expect 'four')");
+console.log(hashtable.remove(2), "(expect 'two')");
+console.log(hashtable.remove(1), "(expect 'one')");
+console.log(hashtable.set("five", 5), "(expect '2')")
