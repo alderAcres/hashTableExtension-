51c4d4f6f65d28b4e08a013836f188c315d5ac8b
Matt Gin <mgin1013@gmail.com>
2020-05-11 10:17:15 -0700

    set and get working. experiencing problems with remove

--- src/main.js
+++ src/main.js
@@ -24,8 +24,20 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
+  // get the index of the key value pair by passing in the key to the hash function
+  // if array[hashed index is empty] create an empty object
+  // set the value of the array at hashed index to be equal to the passed key and value
+  // if the a key value pair already exists at hashed index, add the current key value pair to the existing object
+  // if the key is the same as a previously passed key, overwrite that key with the new value
 
+  let index = hashCode(key, this.SIZE);
+  if (!this.storage[index]){
+    this.storage[index] = {};
+  } 
+  this.storage[index][key] = value;
 };
+
+
 
 /**
 * get - Retrieves a value stored in the hash table with a specified key
@@ -38,7 +50,11 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  let index = hashCode(key, this.SIZE);
+  if (this.storage[index] === undefined || this.storage[index][key] === undefined){
+    return `value for ${key} does not exist in storage`;
+  }
+  return this.storage[index][key];
 };
 
 /**
@@ -50,8 +66,38 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
-
+  let index = hashCode(key, this.size);
+  if (this.storage[index] === undefined || this.storage[index][key] === undefined){
+    return undefined;
+  }
+  let deletedValue = this.storage[index][key];
+  delete this.storage[index][key];
+  return deletedValue;
 };
+
+
+let test = new HashTable();
+console.log(hashCode('bat', 16));
+console.log(hashCode('rat', 16));
+test.set('bat', 21);
+test.set('rat', 22);
+console.log(test.get('bat'));
+console.log(test.get('rat'));
+console.log(test.get('matt'));
+test.set('bat', 'my name jeff');
+console.log(test.get('bat'));
+test.set('bat', 'y u no delete');
+console.log(test.remove('cat'));
+console.log(test.remove('bat')) // value not deleting, possibly because i'm trying to delete a key value pair that's stored in an array?
+console.log(test.get('bat'));
+
+// const testObject = {name: 'matt', age: 29};
+// delete testObject.name;
+// console.log(testObject);
+
+
+
+
 
 
 // Do not modify
