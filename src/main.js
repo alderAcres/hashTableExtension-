40431935b39a6943c77c122c70074179e92944a3
Louis Xavier Sheid III <louisxsheid@gmail.com>
2020-05-11 10:12:53 -0700

    complete assessment

--- src/main.js
+++ src/main.js
@@ -23,9 +23,22 @@ function HashTable() {
 * @param {string|number|boolean} value - value to be stored in hash table
 * @return {number} The new number of items stored in the hash table
 */
-HashTable.prototype.set = function(key, value) {
 
+HashTable.prototype.set = function(key, value) {
+    //make a new hashTable
+  const hash = hashCode(key, this.SIZE);
+        //check if storage has hash value
+  if(this.storage[hash]){
+    //set storage with hash/key to value (overwrite)
+    this.storage[hash][key] = value;
+  }else { //avoid collision
+        //set storage w/ hash to empty obj
+    this.storage[hash] = {};
+        //set storage with hash/key to value
+    this.storage[hash][key] = value;
+  }
 };
+
 
 /**
 * get - Retrieves a value stored in the hash table with a specified key
@@ -37,9 +50,19 @@ HashTable.prototype.set = function(key, value) {
 * @return {string|number|boolean} The value stored with the specifed key in the
 * hash table
 */
-HashTable.prototype.get = function(key) {
 
+//return a previously stored value
+HashTable.prototype.get = function(key) {
+    //loop through this.storage array
+  for(let i in this.storage){
+        //check if key in hash table is equal to passed in key
+    if(Object.keys(this.storage[i])[0] === key){
+            //return that key
+      return Object.values(this.storage[i])[0];
+    }//else return false/null/'key does not exist in hash table'
+  } return false;
 };
+
 
 /**
 * remove - delete a key/value pair from the hash table
@@ -50,7 +73,19 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
-
+  //loop through storage
+  for(let i in this.storage){
+    //check if key in storage === key given
+    if(Object.keys(this.storage[i])[0] === key){
+      //delete item
+      const deleted = this.storage[i]
+      console.log(deleted)
+      delete (this.storage[i]);
+      //return hashtable
+      return deleted;
+    }
+  }
+  return undefined;
 };
 
 
@@ -70,5 +105,16 @@ function hashCode(string, size) {
   return Math.abs(hash) % size;
 }
 
+
+let newHash = new HashTable();
+newHash.set('one', 1)
+newHash.set('two', 2)
+newHash.set('three', 3)
+newHash.set('four', 4)
+newHash.set('one', 11)
+console.log(newHash)
+newHash.remove('two')
+console.log(newHash)
+console.log(newHash.get('one'))
 // Do not remove!!
 module.exports = HashTable;
