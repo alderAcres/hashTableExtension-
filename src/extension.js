384215a6d46a92ae01a546fc82edaba92fef7c7c
Edward Chow <edkchow@gmail.com>
2020-06-01 10:10:47 -0400

    init commit

--- src/extension.js
+++ src/extension.js
@@ -15,7 +15,110 @@
 
 // PASTE AND MODIFY YOUR CODE BELOW
 
+/**
+* HashTable costructor
+*
+* construct a new hash table
+*
+* - You may modify this constructor as you need to achieve the challenges below.
+*/
+function HashTable() {
+  this.SIZE = 16;
+  this.items = 0;
+  this.storage = new Array(this.SIZE);
+}
 
+/**
+* set - Adds given value to the hash table with specified key.
+*
+* - If the provided key has already been used to store another value, simply overwrite
+*   the existing value with the new value.
+* - If the hashed address already contains another key/value pair, you must handle
+*   the collision appropriately.
+*
+* @param {string} key - key to be used to create hashed address
+* @param {string|number|boolean} value - value to be stored in hash table
+* @return {number} The new number of items stored in the hash table
+*/
+HashTable.prototype.set = function(key, value) {
+  const storageIndex = hashCode(key, this.SIZE);
+  // const storedArr = [];
+  // storedArr.push(storageIndex);
+  if (!this.storage[storageIndex]) {
+    const obj = {};
+    obj[key] = value;
+    this.storage[storageIndex] = obj;
+    this.items += 1;
+    if (this.items > (0.75 * this.SIZE)) {
+      this.SIZE *= 2;
+      this.storage = new Array(this.SIZE);
+      const storageIndex = hashCode(key, this.SIZE);
+      
+      // if (!this.storage[storageIndex]) {
+      //   const obj = {};
+      //   obj[key] = value;
+      //   this.storage[storageIndex] = obj;
+      //   this.items += 1;
+      // } else {
+      //   this.storage[storageIndex][key] = value;
+      //   this.items += 1;
+      // }
+    }
+  } else {
+    this.storage[storageIndex][key] = value;
+    this.items += 1;
+    if (this.items > (0.75 * this.SIZE)) {
+      this.SIZE *= 2;
+      this.storage = new Array(this.SIZE);
+      const storageIndex = hashCode(key, this.SIZE);
+      // if (!this.storage[storageIndex]) {
+      //   const obj = {};
+      //   obj[key] = value;
+      //   this.storage[storageIndex] = obj;
+      //   this.items += 1;
+      // } else {
+      //   this.storage[storageIndex][key] = value;
+      //   this.items += 1;
+      // }
+    }
+  }
+};
+
+/**
+* get - Retrieves a value stored in the hash table with a specified key
+*
+* - If more than one value is stored at the key's hashed address, then you must retrieve
+*   the correct value that was originally stored with the provided key
+*
+* @param {string} key - key to lookup in hash table
+* @return {string|number|boolean} The value stored with the specifed key in the
+* hash table
+*/
+HashTable.prototype.get = function(key) {
+  const storageIndex = hashCode(key, this.SIZE);
+  return this.storage[storageIndex][key];
+};
+
+/**
+* remove - delete a key/value pair from the hash table
+*
+* - If the key does not exist in the hash table, return undefined
+*
+* @param {string} key - key to be found and deleted in hash table
+* @return {string|number|boolean} The value deleted from the hash table
+*/
+HashTable.prototype.remove = function(key) {
+  const storageIndex = hashCode(key, this.SIZE);
+  const deleted = this.storage[storageIndex][key];
+  delete this.storage[storageIndex][key];
+  return deleted;
+  // if (this.items > this.SIZE && this.items < Math.floor(0.25 * this.SIZE)) {
+  //   this.SIZE /= 2;
+  //   const storageIndex = hashCode(key, this.SIZE);
+    
+  // }
+  return deleted;
+};
 
 // YOUR CODE ABOVE
 
@@ -36,3 +139,26 @@ function hashCode(string, size) {
 
 // Do not remove!!
 module.exports = HashTable;
+
+const hash = new HashTable();
+console.log(hash);
+hash.set('1', true);
+hash.set('2', false);
+hash.set('3', true);
+hash.set('4', false);
+hash.set('5', true);
+hash.set('6', false);
+hash.set('7', true);
+hash.set('8', false);
+hash.set('9', true);
+hash.set('10', false);
+hash.set('11', true);
+hash.set('12', false);
+// hash.set('13', false);
+console.log(hash);
+// hash.remove('1');
+// hash.remove('2');
+// hash.remove('3');
+// hash.remove('4');
+// console.log(hash.remove('4'));
+// console.log(hash);
