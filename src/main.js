0c5708ada5fb04dd925cd2b35f0a6930deab535c
cfranq <56765803+cfranq@users.noreply.github.com>
2020-06-01 10:02:11 -0400

    submitting final answers

--- src/main.js
+++ src/main.js
@@ -24,8 +24,26 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
+  //use hashCode to obtain a number where key:value pair will be stored
+  let num = hashCode(string, this.Size);
+    //assign number to key
+    key = num;
+    //store value at that assigned number which is assoicated with with key
+    this.storage[key] = value;
+    //not sure how to handle collision
 };
+ 
+//I'm not sure how to write my tests for constructors functions:
+//I create a new instance of a HashTable by assigning it to a variable;
+//I invoke it and try to add values
+//I know that my newHash has access to the methods created because they are stored in the prototype section of this object 
+//I apply the method to the value of interest to see the result
+// used mdn as a resource but did not quite understand: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/constructor
+
+let newHash = new HashTable();
+newHash() = {a: "Cyn"};
+newHash()= {b:"NYC"};
+console.log(newHash.get("Cyn"))
 
 /**
 * get - Retrieves a value stored in the hash table with a specified key
@@ -38,7 +56,13 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  //use hashCode to obtain a number that was assigned to the key and where it's value was stored;
+  let num = hashCode(string, this.Size);
+    //assign number to that key;
+    key = num;
+    //store value at that assigned number which is assoicated with with key
+    return this.storage[key];
+    
 };
 
 /**
@@ -50,7 +74,16 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
-
+  //use hashCode to obtain a number that was assigned to the key and where it's value was stored;
+  let num = hashCode(string, this.Size);
+    //assign number to that key;
+    key = num;
+    //if key does not exist return undefined;
+    if(!this.storage[key] === undefined) {
+      return undefined;
+    }
+    //delete that key at that number inorder to remove from hash table storage
+    delete this.storage[key];
 };
 
 
