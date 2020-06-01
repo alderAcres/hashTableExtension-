e87f546f7c92746df4e274dc70c7a89bf63baf71
Nak Young Kim <nydkim@gmail.com>
2020-06-01 10:10:21 -0400

    Finished main.js and attempted extension.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@
 */
 function HashTable() {
   this.SIZE = 16;
-  
+  this.items = 0;
   this.storage = new Array(this.SIZE);
 }
 
@@ -23,8 +23,33 @@ function HashTable() {
 * @param {string|number|boolean} value - value to be stored in hash table
 * @return {number} The new number of items stored in the hash table
 */
-HashTable.prototype.set = function(key, value) {
 
+HashTable.prototype.set = function(key, value) {
+  const node = {
+    [key]:value,
+    next: null
+  }
+  const bucket = hashCode(key,this.SIZE);
+    // console.log(bucket);
+  if (!this.storage[bucket]) {
+    this.storage[bucket] = node;
+    this.items++;
+  } else {
+    let current = this.storage[bucket];
+    while (current) {
+      if (current[key]) {
+        current[key] = value;
+        return this.items
+      }
+      if (!current.next){
+        current.next = node;
+        this.items++;
+      }
+      current = current.next;
+    }
+
+  }
+  return this.items;
 };
 
 /**
@@ -38,7 +63,14 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  const bucket = hashCode(key,this.SIZE);
+  let current = this.storage[bucket];
+  while (current) {
+    if (current[key]) {
+      return current[key];
+    }
+    current = current.next;
+  }
 };
 
 /**
@@ -50,8 +82,43 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
-
+  const bucket = hashCode(key,this.SIZE);
+  let current = this.storage[bucket];
+  let removed;
+  //if it's at head
+  if (current[key]) {
+    removed = current[key];
+    this.items--;
+    this.storage[bucket] = current.next;
+  } else {
+    while (current.next) {
+      if (current.next[key]) {
+        removed = current.next[key];
+        this.items--;
+        return removed;
+      }
+      current.next = current.next.next;
+    }
+  }
+  return removed;
 };
+
+
+let hashTable = new HashTable();
+console.log(hashTable.set('hi', 'value'));
+console.log(hashTable.set('hi', 'whatever'));
+console.log(hashTable.set('hh', 'something else'));
+console.log(hashTable.set('hj', 'something else'));
+console.log(hashTable.set('hk', 'hks'));
+console.log(hashTable.set('hp', 'hps'));
+console.log(hashTable.set('diff', 'whatev'));
+
+console.log(hashTable.get('doesnt'));
+console.log(hashTable.remove('hp'));
+console.log(hashTable.remove('hk'));
+
+console.log(hashTable.set('hk', 'hks'));
+
 
 
 // Do not modify
@@ -69,6 +136,8 @@ function hashCode(string, size) {
   
   return Math.abs(hash) % size;
 }
+
+
 
 // Do not remove!!
 module.exports = HashTable;
