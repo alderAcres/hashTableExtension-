d2c329a168f06a65438fa869e592a8a1dbd2b064
Midori Yang <myang5@wellesley.edu>
2020-06-01 10:15:37 -0400

    'commit finished exercises'

--- src/extension.js
+++ src/extension.js
@@ -14,25 +14,86 @@
 */
 
 // PASTE AND MODIFY YOUR CODE BELOW
+function HashTable() {
+  this.SIZE = 4;
 
+  this.storage = new Array(this.SIZE);
+}
 
+HashTable.prototype.set = function (key, value) {
+  //hashCode(key) will throw error of key is an object
+  //key should only be primitive data type
+  if (typeof key !== 'string' && typeof key !== 'number' && typeof key !== 'boolean'
+    || value === undefined) {
+    return;
+  }
+
+  const currSize = this.storage.reduce((acc, obj) => {
+    if (obj) return acc + Object.keys(obj).length;
+    else return acc;
+  }, 0);
+
+  if (currSize / this.SIZE > 0.75) {
+    this.SIZE = this.SIZE * 2;
+    this.storage.forEach(obj => {
+      if(obj) {
+        Object.keys(obj).forEach(key => {
+          this.set(key, obj[key]);
+        })
+      }
+    })
+  }
+
+  const hash = hashCode(key, this.SIZE);
+  if (this.storage[hash]) this.storage[hash][key] = value;
+  else this.storage[hash] = { [key]: value }
+
+  return newSize = this.storage.reduce((acc, obj) => {
+    if (obj) return acc + Object.keys(obj).length;
+    else return acc;
+  }, 0);
+};
 
 // YOUR CODE ABOVE
 
 function hashCode(string, size) {
   'use strict';
-  
+
   let hash = 0;
   if (string.length === 0) return hash;
-  
+
   for (let i = 0; i < string.length; i++) {
     const letter = string.charCodeAt(i);
     hash = ((hash << 5) - hash) + letter;
     hash = hash & hash; // Convert to 32bit integer
   }
-  
+
   return Math.abs(hash) % size;
 }
 
 // Do not remove!!
 module.exports = HashTable;
+
+const hashTable = new HashTable();
+console.log('test this.set()')
+console.log(hashTable.set(0, 'test0'));
+console.log(hashTable.set('a', 'testA'));
+console.log(hashTable.set('b', 'testB'));
+console.log(hashTable.set('3', 'test3'));
+console.log(hashTable.set('4', 'test4'));
+console.log(hashTable.set('5', 'test5'));
+console.log(hashTable.set('6', 'test6'));
+
+
+
+console.log(hashTable.set([1], 'array'))
+console.log(hashTable.set([1], 'array'))
+console.log(hashTable.set(3))
+console.log(hashTable.set(undefined, 'testUndefined'))
+
+console.log(hashTable.storage);
+
+//console.log('test this.get()')
+//console.log(hashTable.get('a'));
+//console.log(hashTable.get('1'));
+//console.log(hashTable.get('na'));
