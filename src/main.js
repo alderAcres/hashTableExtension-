85c5549481164790c279496da4c9d00a86a3149b
geewailee <30604942+geewailee@users.noreply.github.com>
2020-06-05 08:43:55 -0400

    assessment refactored

--- src/main.js
+++ src/main.js
@@ -7,32 +7,9 @@
 */
 function HashTable() {
   this.SIZE = 16;
-  
   this.storage = new Array(this.SIZE);
+  this.length = 0;
 }
-
-function LinkedList() {
-  this.head = null;
-  this.tail = null;
-}
-
-function Node(val) {
-  this.value = val; 
-  this.next = null;
-}
-
-LinkedList.prototype.push = function(value) {
-  let newNode = new Node(value);
-
-  if (this.head == null) {
-    this.head = newNode;
-    this.tail = newNode;
-  } else {
-    this.tail.next = newNode;
-    this.tail = newNode;
-  }
-}
-
 
 /**
 * set - Adds given value to the hash table with specified key.
@@ -47,62 +24,65 @@ LinkedList.prototype.push = function(value) {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-<<<<<<< Updated upstream
-
-=======
   // use hashcode to get hashed address
-  let address = hashCode(key, this.SIZE)
+  const location = hashCode(key, this.SIZE)
   // console.log(`address ${address}`);
-  let linkedList = new LinkedList()
-
-  // if there is a collision, add to linked list
-  this.storage[address] = linkedList.push(value);
-
-  console.log(linkedList);
-
-  // get count of keys in hash table
-  let hashCount = Object.keys(this.storage).length;
-  // get count of linked list values within has table
-  for (const key in this.storage) {
-    for (const keyL in this.storage[key]) {
-        
-      }
-    }
+  if (!this.storage[location]) {
+    this.storage[location] = {[key]: value}
+  } else {
+    this.storage[location][key] = value;
   }
-
-  
-  
-  // return how many keys in hash table
-  return Object.keys(this.storage).length;
->>>>>>> Stashed changes
+  return this.length++;
 };
+let ht = new HashTable()
+ht.set('key', 'value')
+ht.set('key0', 'value0')
+ht.set('key1', 'value1')
+console.log(ht);
+
 
 /**
-* get - Retrieves a value stored in the hash table with a specified key
-*
-* - If more than one value is stored at the key's hashed address, then you must retrieve
-*   the correct value that was originally stored with the provided key
-*
-* @param {string} key - key to lookup in hash table
-* @return {string|number|boolean} The value stored with the specifed key in the
-* hash table
-*/
+ * get - Retrieves a value stored in the hash table with a specified key
+ *
+ * - If more than one value is stored at the key's hashed address, then you must retrieve
+ *   the correct value that was originally stored with the provided key
+ *
+ * @param {string} key - key to lookup in hash table
+ * @return {string|number|boolean} The value stored with the specifed key in the
+ * hash table
+ */
 HashTable.prototype.get = function(key) {
-
+  const location = hashCode(key, this.SIZE)
+  if (this.storage[location] && this.storage[location][key]) {
+    return this.storage[location][key];
+  }
+  return 'Key does not exist'
 };
+console.log('get');
+console.log(ht.get('key'));
+console.log(ht);
+
 
 /**
-* remove - delete a key/value pair from the hash table
-*
-* - If the key does not exist in the hash table, return undefined
-*
-* @param {string} key - key to be found and deleted in hash table
-* @return {string|number|boolean} The value deleted from the hash table
-*/
+ * remove - delete a key/value pair from the hash table
+ *
+ * - If the key does not exist in the hash table, return undefined
+ *
+ * @param {string} key - key to be found and deleted in hash table
+ * @return {string|number|boolean} The value deleted from the hash table
+ */
 HashTable.prototype.remove = function(key) {
-
+  const location = hashCode(key, this.SIZE);
+  if (this.storage[location] && this.storage[location][key]) {
+    const value = this.storage[location][key]
+    delete this.storage[location][key];
+    this.length--;
+    return value;
+  }  
+  return undefined; 
 };
-
+console.log(ht.remove('key'));
+console.log(ht);
 
 // Do not modify
 function hashCode(string, size) {
