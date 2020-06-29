22ef3816de5ce4f66c1f5925efea8fba75bc70ab
Sey Kim <asykim@tutanota.com>
2020-06-29 16:14:00 -0400

    retry....

--- src/main.js
+++ src/main.js
@@ -1,74 +1,52 @@
-/**
-* HashTable costructor
-*
-* construct a new hash table
-*
-* - You may modify this constructor as you need to achieve the challenges below.
-*/
 function HashTable() {
   this.SIZE = 16;
-  
   this.storage = new Array(this.SIZE);
 }
 
-/**
-* set - Adds given value to the hash table with specified key.
-*
-* - If the provided key has already been used to store another value, simply overwrite
-*   the existing value with the new value.
-* - If the hashed address already contains another key/value pair, you must handle
-*   the collision appropriately.
-*
-* @param {string} key - key to be used to create hashed address
-* @param {string|number|boolean} value - value to be stored in hash table
-* @return {number} The new number of items stored in the hash table
-*/
-HashTable.prototype.set = function(key, value) {
-
+HashTable.prototype.address = function (key) {
+  return hashCode(key, this.SIZE);
 };
 
-/**
-* get - Retrieves a value stored in the hash table with a specified key
-*
-* - If more than one value is stored at the key's hashed address, then you must retrieve
-*   the correct value that was originally stored with the provided key
-*
-* @param {string} key - key to lookup in hash table
-* @return {string|number|boolean} The value stored with the specifed key in the
-* hash table
-*/
-HashTable.prototype.get = function(key) {
-
+HashTable.prototype.set = function (key, value) {
+  const a = this.address(key);
+  if (!this.storage[a]) this.storage[a] = {};
+  this.storage[a][key] = value;
 };
 
-/**
-* remove - delete a key/value pair from the hash table
-*
-* - If the key does not exist in the hash table, return undefined
-*
-* @param {string} key - key to be found and deleted in hash table
-* @return {string|number|boolean} The value deleted from the hash table
-*/
-HashTable.prototype.remove = function(key) {
-
+HashTable.prototype.get = function (key) {
+  const a = this.address(key);
+  return this.storage[a][key];
 };
 
+HashTable.prototype.remove = function (key) {
+  const a = this.address(key);
+  delete this.storage[a][key];
+};
 
 // Do not modify
 function hashCode(string, size) {
-  'use strict';
-  
+  "use strict";
+
   let hash = 0;
   if (string.length === 0) return hash;
-  
+
   for (let i = 0; i < string.length; i++) {
     const letter = string.charCodeAt(i);
-    hash = ((hash << 5) - hash) + letter;
+    hash = (hash << 5) - hash + letter;
     hash = hash & hash; // Convert to 32bit integer
   }
-  
+
   return Math.abs(hash) % size;
 }
 
 // Do not remove!!
 module.exports = HashTable;
+
+// // test case
+// let hash = new HashTable();
+
+// for (let i = 50; i < 150; i++) {
+//   hash.set(String(i), Math.floor(Math.random() * 100));
+// }
+
+// console.log(hash.storage);
