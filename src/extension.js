218c92a50ebbb7e498bc3f9dc7c019aa8e290182
Michelle Holland <michelle@MacBook-Pro.local>
2020-06-29 10:12:56 -0700

    Complete main?

--- src/extension.js
+++ src/extension.js
@@ -14,6 +14,32 @@
 */
 
 // PASTE AND MODIFY YOUR CODE BELOW
+HashTable.prototype.set = function (key, value) {
+  const index = hashCode(key);
+  const limit = 0.75 * this.SIZE;
+  if (this.storage.length + 1 > limit) {
+    this.SIZE *= 2;
+    // re-hash: 1. iterate over storage
+    for (let i = 0; i < this.storage.length; i++) {
+      const newCode = hashCode(this.storage[i][0]);
+      this.storage[newCode] = this.storage[i];
+    }
+  }
+  //-------------haven't modified code below
+  // check if index already exists in storage
+  if (this.storage[index]) {
+    // if it does, store inputs as key-val pair on index object
+    this.storage[index][key] = value;
+    // if index doesn't exist, first initialize index val as an empty object
+  } else {
+    const container = {};
+    container[key] = value;
+    this.storage[index] = container;
+  }
+  return this.storage.length;
+};
+
+
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,72 +1,89 @@
 /**
-* HashTable costructor
-*
-* construct a new hash table
-*
-* - You may modify this constructor as you need to achieve the challenges below.
-*/
+ * HashTable costructor
+ *
+ * construct a new hash table
+ *
+ * - You may modify this constructor as you need to achieve the challenges below.
+ */
 function HashTable() {
   this.SIZE = 16;
-  
+
   this.storage = new Array(this.SIZE);
 }
 
 /**
-* set - Adds given value to the hash table with specified key.
-*
-* - If the provided key has already been used to store another value, simply overwrite
-*   the existing value with the new value.
-* - If the hashed address already contains another key/value pair, you must handle
-*   the collision appropriately.
-*
-* @param {string} key - key to be used to create hashed address
-* @param {string|number|boolean} value - value to be stored in hash table
-* @return {number} The new number of items stored in the hash table
-*/
-HashTable.prototype.set = function(key, value) {
-
+ * set - Adds given value to the hash table with specified key.
+ *
+ * - If the provided key has already been used to store another value, simply overwrite
+ *   the existing value with the new value.
+ * - If the hashed address already contains another key/value pair, you must handle
+ *   the collision appropriately.
+ *
+ * @param {string} key - key to be used to create hashed address
+ * @param {string|number|boolean} value - value to be stored in hash table
+ * @return {number} The new number of items stored in the hash table
+ */
+HashTable.prototype.set = function (key, value) {
+  const index = hashCode(key);
+  // check if index already exists in storage
+  if (this.storage[index]) {
+    // if it does, store inputs as key-val pair on index object
+    this.storage[index][key] = value;
+    // if index doesn't exist, first initialize index val as an empty object
+  } else {
+    const container = {};
+    container[key] = value;
+    this.storage[index] = container;
+  }
+  return this.storage.length;
 };
 
 /**
-* get - Retrieves a value stored in the hash table with a specified key
-*
-* - If more than one value is stored at the key's hashed address, then you must retrieve
-*   the correct value that was originally stored with the provided key
-*
-* @param {string} key - key to lookup in hash table
-* @return {string|number|boolean} The value stored with the specifed key in the
-* hash table
-*/
-HashTable.prototype.get = function(key) {
-
+ * get - Retrieves a value stored in the hash table with a specified key
+ *
+ * - If more than one value is stored at the key's hashed address, then you must retrieve
+ *   the correct value that was originally stored with the provided key
+ *
+ * @param {string} key - key to lookup in hash table
+ * @return {string|number|boolean} The value stored with the specifed key in the
+ * hash table
+ */
+HashTable.prototype.get = function (key) {
+  const index = hashCode(key);
+  return this.storage[index][key];
 };
 
 /**
-* remove - delete a key/value pair from the hash table
-*
-* - If the key does not exist in the hash table, return undefined
-*
-* @param {string} key - key to be found and deleted in hash table
-* @return {string|number|boolean} The value deleted from the hash table
-*/
-HashTable.prototype.remove = function(key) {
-
+ * remove - delete a key/value pair from the hash table
+ *
+ * - If the key does not exist in the hash table, return undefined
+ *
+ * @param {string} key - key to be found and deleted in hash table
+ * @return {string|number|boolean} The value deleted from the hash table
+ */
+HashTable.prototype.remove = function (key) {
+  // compute index
+  const index = hashCode(key);
+  // store value of item to be removed - if key doesn't exist, this value will be undefined
+  const removed = this.storage[index][key];
+  delete this.storage[index][key];
+  return removed;
 };
+
+
 
 
 // Do not modify
 function hashCode(string, size) {
-  'use strict';
-  
   let hash = 0;
   if (string.length === 0) return hash;
-  
+
   for (let i = 0; i < string.length; i++) {
     const letter = string.charCodeAt(i);
-    hash = ((hash << 5) - hash) + letter;
-    hash = hash & hash; // Convert to 32bit integer
+    hash = (hash << 5) - hash + letter;
+    hash &= hash; // Convert to 32bit integer
   }
-  
+
   return Math.abs(hash) % size;
 }
 
