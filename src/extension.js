6d2074fc5c5459cc648044ec21eee078b0fb8400
justinjaeger <jjustinjaeger@gmail.com>
2020-07-20 09:35:11 -0400

    completed has table

--- src/extension.js
+++ src/extension.js
@@ -13,11 +13,73 @@
         (rounding down), then reduce the hash table's SIZE by 1/2 and rehash everything.
 */
 
-// PASTE AND MODIFY YOUR CODE BELOW
+//////////////////////////// PASTE AND MODIFY YOUR CODE BELOW
+
+// construct a new hash table
+function HashTable() {
+  this.SIZE = 10;
+  
+  this.storage = new Array(this.SIZE);
+  for (let i=0; i<this.SIZE; i++){
+    this.storage[i] = {}
+  }
+}
+const testHash = new HashTable()
+// console.log(testHash)
 
 
+// adds a value to the hash table
+HashTable.prototype.set = function(key, value) {
+  const hash = hashCode(key, this.SIZE); // get the hash code
 
-// YOUR CODE ABOVE
+  this.storage[hash][key] = value
+};
+
+testHash.set('asdf',3)
+testHash.set('sdfa',5)
+// console.log(testHash)
+
+// retrieve a value from hash table
+HashTable.prototype.get = function(key) {
+  const hash = hashCode(key, this.SIZE);
+
+  return this.storage[hash][key]
+};
+
+// console.log(testHash.get('sdfa'))
+
+
+// Delete a key/value pair from the hash table
+HashTable.prototype.remove = function(key) {
+  let hash = hashCode(key, this.SIZE);
+
+  delete this.storage[hash][key]
+};
+
+testHash.remove('asdf')
+// console.log(testHash)
+
+
+// Do not modify
+function hashCode(string, size) {
+  'use strict';
+  
+  let hash = 0;
+  if (string.length === 0) return hash;
+  
+  for (let i = 0; i < string.length; i++) {
+    const letter = string.charCodeAt(i);
+    hash = ((hash << 5) - hash) + letter;
+    hash = hash & hash; // Convert to 32bit integer
+  }
+  
+  return Math.abs(hash) % size;
+}
+
+// Do not remove!!
+module.exports = HashTable;
+
+//////////////////////////////// YOUR CODE ABOVE
 
 function hashCode(string, size) {
   'use strict';
--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 /**
 * HashTable costructor
-*
-* construct a new hash table
-* 
-* - You may modify this constructor as you need to achieve the challenges below.
 */
+
+// construct a new hash table
 function HashTable() {
   this.SIZE = 10;
   
@@ -16,18 +14,6 @@ function HashTable() {
 const testHash = new HashTable()
 console.log(testHash)
 
-/**
-* set - Adds given value to the hash table with specified key.
-*
-* - If the provided key has already been used to store another value, simply overwrite
-*   the existing value with the new value.
-* - If the hashed address already contains another key/value pair, you must handle
-*   the collision appropriately.
-*
-* @param {string} key - key to be used to create hashed address
-* @param {string|number|boolean} value - value to be stored in hash table
-* @return {number} The new number of items stored in the hash table
-*/
 
 // adds a value to the hash table
 HashTable.prototype.set = function(key, value) {
@@ -40,17 +26,7 @@ testHash.set('asdf',3)
 testHash.set('sdfa',5)
 console.log(testHash)
 
-/**
-* get - Retrieves a value stored in the hash table with a specified key
-*
-* - If more than one value is stored at the key's hashed address, then you must retrieve
-*   the correct value that was originally stored with the provided key
-*
-* @param {string} key - key to lookup in hash table
-* @return {string|number|boolean} The value stored with the specifed key in the
-* hash table
-*/
-
+// retrieve a value from hash table
 HashTable.prototype.get = function(key) {
   const hash = hashCode(key, this.SIZE);
 
@@ -59,14 +35,8 @@ HashTable.prototype.get = function(key) {
 
 console.log(testHash.get('sdfa'))
 
-/**
-* remove - delete a key/value pair from the hash table
-*
-* - If the key does not exist in the hash table, return undefined
-*
-* @param {string} key - key to be found and deleted in hash table
-* @return {string|number|boolean} The value deleted from the hash table
-*/
+
+// Delete a key/value pair from the hash table
 HashTable.prototype.remove = function(key) {
   let hash = hashCode(key, this.SIZE);
 
