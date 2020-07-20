f823f51ab9988df0c9e94acd15f2c3ad34ee43e9
Nicole <nicoleduu@gmail.com>
2020-07-20 10:11:57 -0400

    1stassessment

--- src/main.js
+++ src/main.js
@@ -24,7 +24,14 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
+	//to get HASH number to store the key value pair in, run the key in hashCode function
+	let bucket = hashCode(String(key), this.SIZE);
+	//to prevent collision, if the bucket is empty, set it equal to a new obj	
+		if(!this.storage[bucket]){
+			this.storage[bucket] = {};
+		}
+		//else, set the key value pair of that bucket to the key & values passed in
+		this.storage[bucket][key] = value
 };
 
 /**
@@ -38,7 +45,10 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+	//use hashCode function to figure out the hash number
+	let bucket = hashCode (String(key, this.SIZE); 
+	// with the hashCode, find out the value at that bucket, with the key passed
+	return this.storage[bucket][key];
 };
 
 /**
@@ -50,7 +60,16 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
+	//use hashCode function to figure out the hash number
+	let bucket = hashCode (String(key, this.SIZE) 
+	// with the hashCode, find out the value at that bucket, with the key passed to delete
 
+	//if there is a key value pair at the bucket with the key passed in, delete it
+	if (this.storage[bucket][key]) {
+		delete this.storage[bucket][key];
+	} else {
+		return undefined;
+	}
 };
 
 
