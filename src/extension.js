0d30af275a4c8c3cf0cf44a6d3826c6bb7f35cf9
aaronyang24 <aaronyang024@gmail.com>
2020-08-17 10:09:33 -0700

    main.js complete

--- src/extension.js
+++ src/extension.js
@@ -14,8 +14,54 @@
 */
 
 // PASTE AND MODIFY YOUR CODE BELOW
+function HashTable() {
+  this.SIZE = 16;
+  
+  this.storage = new Array(this.SIZE);
+}
+HashTable.prototype.set = function(key, value) {
+  //create var hash to store hashcode in
+  const hash = hashCode(key,this.SIZE);
+  //create count to store item count;
+  let count = 0;
+  //if size is over 75% of the tables size, double size and rehash everything
+  if(count > this.SIZE * 0.75){
+    //create new hash table
+    let newTable = new HashTable();
+    //double the size
+    newTable.SIZE = this.SIZE * 2;
+    //somehow rehash the old hash table;
+    for(let element of this.storage){
+      //store the old elements into the new table
+        newTable.storage[element];
+      }
+      return newTable;
+    }
+  //if hash index is undefined
+  if(this.storage[hash] === undefined){
+    count += 1;
+    //set that index to an empty object
+    this.storage[hash] = {};
+    //store item in object
+    this.storage[hash][key] = value;
+  } else{
+    count += 1;
+    console.log(count)
+    this.storage[hash][key] = value;
+  }
+}
+HashTable.prototype.get = function(key) {
+  const hash = hashCode(key,this.SIZE);
+  return this.storage[hash][key];
+};
 
-
+HashTable.prototype.remove = function(key) {
+  const hash = hashCode(key,this.SIZE);
+  //return undefined if key does not exist in table
+  if(!this.storage[hash]) return undefined;
+  //delete item from the hash table
+  delete this.storage[hash];
+};
 
 // YOUR CODE ABOVE
 
@@ -36,3 +82,18 @@ function hashCode(string, size) {
 
 // Do not remove!!
 module.exports = HashTable;
+
+
+let table = new HashTable();
+table.set('Aaron', 1)
+table.set('Aon', 2)
+table.set('Aarn', 3)
+table.set('Aaro', 4)
+table.set('Aa', 5)
+table.set('aron', 6)
+table.set('ron', 7)
+table.set('on', 8)
+table.set('n', 9)
+table.set('sdaf', 10)
+table.set('Adsads', 11)
+console.log(table);
