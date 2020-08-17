1a6de551133ebdd4d6c0252ef18f15016af77b67
Jay <36286598+Theqwertypusher@users.noreply.github.com>
2020-08-17 13:15:31 -0400

    completed all - set, get, remove

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@
 */
 function HashTable() {
   this.SIZE = 16;
-  
   this.storage = new Array(this.SIZE);
 }
 
@@ -24,8 +23,29 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
+  // declare hash and store the result of hash passing in key
+  let hash = hash(key, this.SIZE);
+  // if the key at hash of this storage exists
+  if (this.storage[hash]){
+    // overwrite with new key value pair
+    this.storage[hash][key] = value;
+  } else {
+    // else create new object and add key value pair 
+    this.storage[hash] = {};
+    this.storage[hash][key] = value;
+  }
 };
+
+// testcases
+
+const test = new HashTable();
+test.set('firstName', 'jason')
+test.set('middleName', 'alexander')
+test.set('lastName', 'victor')
+test.set('favColor', 'red')
+
+console.log(test)
+
 
 /**
 * get - Retrieves a value stored in the hash table with a specified key
@@ -38,8 +58,13 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  // declare variable and store result of hashCode passing in key
+  const hash = hashCode(key, this.SIZE);
+  // return value stored at key of hash in this.storage
+  return this.storage[hash][key]
 };
+
+console.log(test.get('favColor'))
 
 /**
 * remove - delete a key/value pair from the hash table
@@ -50,9 +75,25 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
-
+  // declare variable and store result of hashCode passing in key
+  const hash = hashCode(key, this.SIZE);
+  // if key/value pair exists - then delete key/value pair
+  if (this.storage[hash]) {
+    delete this.storage[hash][key];
+    // if stored obj has no key/value pairs, delete stored obj
+    if (this.storage[hash].keys[0] === null){
+      delete this.storage[hash]
+    }
+  }
+  // else key/value pair does not exists - return undefined
+  else {
+    return undefined
+  }
 };
 
+console.log(test.remove('something'))
+console.log(test.remove('favColor'))
+console.log(test)
 
 // Do not modify
 function hashCode(string, size) {
