6fa1ced2dda3fb7d1a1134c565d750487127fe42
Tommy Song <TommySong@Tommys-MacBook-Air.local>
2020-08-17 10:11:13 -0700

    week 1 assessment

--- src/main.js
+++ src/main.js
@@ -24,7 +24,17 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
+  // we need to get the value of the hash function of key
+  let hashInd = hashCode(key, this.SIZE);
+  // check to see if there is anything at the index provided by the hash function
+  if (!this.storage[hashInd]) {
+    // if empty we would create an empty object with the key value pair
+    this.storage[hashInd] = {};
+    this.storage[hashInd].key = value;
+  } else {
+    // we would add the key value pair into the hash table
+    this.storage[hashInd].key = value;
+  }
 };
 
 /**
@@ -38,7 +48,10 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  // need to find the object at the hash index
+  let hashInd = hashCode(key, this.SIZE);
+  // return the key at that has index
+  return this.storage[hashInd].key;
 };
 
 /**
@@ -50,8 +63,25 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
+  // find the object at the hash index
+  let hashInd = hashCode(key, this.SIZE);
+  // hold the value at the key
+  let hold = this.storage[hashInd].key;
+  // remove the key value pair with key as key
+  delete this.storage[hashInd].key;
+  // return the value
+  return hold;
 
 };
+
+let hashTable = new HashTable();
+hashTable.set('first key', 'first value');
+hashTable.set('second key', 'second value');
+console.log(hashTable.get('first key')); //.to.be('first value');
+console.log(hashTable.get('second key')); //.to.be('second value');
+console.log(hashTable.storage.length); //.to.eql(16);
+console.log(hashTable.remove('first key')); // .to.eql('first value');
+console.log(hashTable.get('first key')); // .to.eql(undefined);
 
 
 // Do not modify
