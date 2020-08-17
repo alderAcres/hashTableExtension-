3b56d215d4fad3c007a06381612b4961ef42bf7d
Jinseon Shin <jinseonshin@Jinseons-MacBook-Pro.local>
2020-08-17 13:03:29 -0400

    Finished Fundamentals Assessment Jinseon Shin

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+const { delete } = require("request");
+
 /**
 * HashTable costructor
 *
@@ -10,48 +12,6 @@ function HashTable() {
   
   this.storage = new Array(this.SIZE);
 }
-
-/**
-* set - Adds given value to the hash table with specified key.
-*
-* - If the provided key has already been used to store another value, simply overwrite
-*   the existing value with the new value.
-* - If the hashed address already contains another key/value pair, you must handle
-*   the collision appropriately.
-*
-* @param {string} key - key to be used to create hashed address
-* @param {string|number|boolean} value - value to be stored in hash table
-* @return {number} The new number of items stored in the hash table
-*/
-HashTable.prototype.set = function(key, value) {
-
-};
-
-/**
-* get - Retrieves a value stored in the hash table with a specified key
-*
-* - If more than one value is stored at the key's hashed address, then you must retrieve
-*   the correct value that was originally stored with the provided key
-*
-* @param {string} key - key to lookup in hash table
-* @return {string|number|boolean} The value stored with the specifed key in the
-* hash table
-*/
-HashTable.prototype.get = function(key) {
-
-};
-
-/**
-* remove - delete a key/value pair from the hash table
-*
-* - If the key does not exist in the hash table, return undefined
-*
-* @param {string} key - key to be found and deleted in hash table
-* @return {string|number|boolean} The value deleted from the hash table
-*/
-HashTable.prototype.remove = function(key) {
-
-};
 
 
 // Do not modify
@@ -69,6 +29,76 @@ function hashCode(string, size) {
   
   return Math.abs(hash) % size;
 }
+
+
+
+/**
+* set - Adds given value to the hash table with specified key.
+*
+* - If the provided key has already been used to store another value, simply overwrite
+*   the existing value with the new value.
+* - If the hashed address already contains another key/value pair, you must handle
+*   the collision appropriately.
+*
+* @param {string} key - key to be used to create hashed address
+* @param {string|number|boolean} value - value to be stored in hash table
+* @return {number} The new number of items stored in the hash table
+*/
+HashTable.prototype.set = function (key, value) {
+  const index = hashCode(key, this.SIZE); // index position in table
+
+  if (this.storage[index]) { // if something exists at index
+    this.storage[index][key] = value; // store/rewrite key value pair
+  } else { // if nothing exists at index
+    const newObj = {};
+    newObj[key] = value;
+    this.storage[index] = newObj; // add new key value pair to table as object 
+  }
+};
+
+
+
+/**
+* get - Retrieves a value stored in the hash table with a specified key
+*
+* - If more than one value is stored at the key's hashed address, then you must retrieve
+*   the correct value that was originally stored with the provided key
+*
+* @param {string} key - key to lookup in hash table
+* @return {string|number|boolean} The value stored with the specifed key in the
+* hash table
+*/
+HashTable.prototype.get = function (key) {
+  const index = hashCode(key, this.SIZE); // index position in table
+
+  // return value at index position if key name === key
+  return this.storage[index][key];
+};
+
+
+
+
+/**
+* remove - delete a key/value pair from the hash table
+*
+* - If the key does not exist in the hash table, return undefined
+*
+* @param {string} key - key to be found and deleted in hash table
+* @return {string|number|boolean} The value deleted from the hash table
+*/
+HashTable.prototype.remove = function(key) {
+  let removedPair;
+  const index = hashCode(key, this.SIZE); // index position in table 
+
+  if(this.storage[index][key]) { // if key value pair w/ key exists 
+    removedPair = this.storage[index][key]; // store key value pair
+    delete this.storage[index][key]; // delete key value pair
+  }
+  return removedPair;
+};
+
+
+
 
 // Do not remove!!
 module.exports = HashTable;
