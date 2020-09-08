14edd83705d47a924ff0f91d8a71ce22d1ddec85
Jin Soo Lim <jinsoolim1@gmail.com>
2020-09-08 07:14:35 -0700

    jinsoo finished cs-fundamentals-assessment

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@
 */
 function HashTable() {
   this.SIZE = 16;
-  
+
   this.storage = new Array(this.SIZE);
 }
 
@@ -24,7 +24,27 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
+  let addressIndex = hashCode(key, this.SIZE);
+  const newBucket = {};
+  if (this.storage[addressIndex]) {
+    this.storage[addressIndex][key] = value;
+  } else {
+    newBucket[key] = value;
+    this.storage[addressIndex] = newBucket;
+  }
+  let numberOfItems = 0;
+  for (let i = 0; i < this.storage.length; i+=1) {
+    // Directions for "number" of items unclear - are items every key/value pair? or elements of the storage array?
+    // 1. Below is the method if "items" are every key/value pair
+    if (this.storage[i]) {
+      numberOfItems += 1;
+    }
+    //2. Below is the method if "items" are elements of the storage array
+    // if (this.storage[i]) {
+    //   Object.keys(this.storage[i]).forEach(() => numberOfItems += 1);
+    // }
+  }
+  return numberOfItems;
 };
 
 /**
@@ -38,8 +58,20 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  let addressIndex = hashCode(key, this.SIZE);
+  if (this.storage[addressIndex]) {
+    return this.storage[addressIndex][key];
+  } else {
+    return 'Does Not Exist';
+    // or False *** Didn't put return false in case value that is stored could be false *** See below example
+  }
 };
+
+const newTable = new HashTable();
+newTable.set('dan', true);
+newTable.set('dkx', false);
+console.log(newTable.storage);
+console.log(newTable.get('dkd'));
 
 /**
 * remove - delete a key/value pair from the hash table
@@ -50,23 +82,31 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
-
+  let addressIndex = hashCode(key, this.SIZE);
+  let deleted;
+  if (this.storage[addressIndex]) {
+    deleted = this.storage[addressIndex][key];
+    delete this.storage[addressIndex][key];
+    return deleted;
+  } else {
+    return undefined;
+  }
 };
 
 
 // Do not modify
 function hashCode(string, size) {
   'use strict';
-  
+
   let hash = 0;
   if (string.length === 0) return hash;
-  
+
   for (let i = 0; i < string.length; i++) {
     const letter = string.charCodeAt(i);
     hash = ((hash << 5) - hash) + letter;
     hash = hash & hash; // Convert to 32bit integer
   }
-  
+
   return Math.abs(hash) % size;
 }
 
