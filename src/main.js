c1b25a837f728ffa6b18fd066c2abe75a07b3ca2
Chris Lung <chrislung@Chriss-MBP.verizon.net>
2020-09-21 21:16:06 -0400

    assessment update

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@
 */
 function HashTable() {
   this.SIZE = 16;
-  
   this.storage = new Array(this.SIZE);
+  this.items = 0;
 }
 
 /**
@@ -24,7 +24,31 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
+  //Key must a string
+  if (typeof key !== 'string') return 'Please input a valid key';
+  //Convert key into hashcode
+  let ind = hashCode(key, this.SIZE);
+  //Check if bucket exists
+  if (this.storage[ind] === undefined) {
+    this.storage[ind] = [{key, value}];
+    this.items++;
+  } else {
+  //If collision, check if key is in bucket
+    let keyUpdate = false;
+    for (let i = 0; i < this.storage[ind].length; i++) {
+      //If key is in bucket, update value
+      if (this.storage[ind][i]['key'] === key) {
+        this.storage[ind][i]['value'] = value;
+        keyUpdate = true;
+      }
+    } 
+  //If key was not updated, then we have to add the new key to bucket to avoid collision
+    if (!keyUpdate) {
+      this.storage[ind][this.storage[ind].length] = {key, value};
+      this.items++;
+    }
+  }
+  return this.items;
 };
 
 /**
@@ -38,7 +62,20 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  //Key must be a string
+  if (typeof key === 'string') {
+    //Convert key into hashcode
+    let ind = hashCode(key, this.SIZE);
+    //If bucket does not exist, return key not found.
+    if (!this.storage[ind]) return 'Key not found';
+    //If bucket exists, iterate to check if key is in bucket
+    for (let i = 0; i < this.storage[ind].length; i++) {
+      //If found, retrieve value of specified key
+      if (this.storage[ind][i]['key'] === key) return this.storage[ind][i]['value']
+    }
+  }
+  //If key not found in bucket, return key not found.
+  return 'Key not found';
 };
 
 /**
@@ -50,9 +87,26 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
-
-};
-
+  //Key must be a string
+  if (typeof key === 'string') {
+    //Convert key into hashcode
+    let ind = hashCode(key, this.SIZE);
+    //If bucket does not exist
+    if (!this.storage[ind]) return undefined;
+    //If bucket exists, iterate to check if key is in bucket
+    for (let i = 0; i < this.storage[ind].length; i++) {
+    //If key is found, check if bucket length. Delete entire bucket if it only contains one key, otherwise delete specified key
+      if (this.storage[ind][i]['key'] === key) {
+        let removed = this.storage[ind][i]['value'];
+        if (this.storage[ind].length === 1) delete this.storage[ind];
+        else delete this.storage[ind][i];
+        return removed;
+      }
+    }
+  }
+  //If key not found in bucket
+  return undefined;
+}
 
 // Do not modify
 function hashCode(string, size) {
