8a82dff1a3b41e452eabb2980ba12df045dc1566
mlndz-la <erickmlndz@protonmail.com>
2020-10-05 10:09:51 -0700

    completed main.js, extension WIP

--- src/extension.js
+++ src/extension.js
@@ -15,7 +15,61 @@
 
 // PASTE AND MODIFY YOUR CODE BELOW
 
+function HashTable() {
+  this.SIZE = 16;
+  this.storage = new Array(this.SIZE);
+}
 
+HashTable.prototype.set = function(key, value) {
+  // declare a variable invoking key and current size
+  const index = hashCode(key, this.SIZE);
+  // check if index exits
+  if (!this.storage[index]) {
+    // if it doesn't
+    // add an object with a key pair value
+    // setting an object makes it easier to retrieve or delete later
+    this.storage[index] = { [key]: value };
+  } else {
+    // it does exit add another key pair value
+    this.storage[index][key] = value;
+  }
+};
+
+HashTable.prototype.get = function(key) {
+  // declare a hashed index
+  const index = hashCode(key, this.SIZE);
+  // at index of storage retrieve value from obj via key
+  // (all stores in storage are objects)
+  return this.storage[index][key];
+};
+
+HashTable.prototype.remove = function(key) {
+  // declare hashed value
+  const index = hashCode(key, this.SIZE);
+  // instantiate returned result
+  let removed;
+  // check if storage at index exits
+  if (!this.storage[index]) removed = undefined;
+  else if (Object.keys(this.storage[index]).length === 1) {
+    // save removed value
+    removed = this.storage[index][key];
+    // if storage at index has 1 key pair value
+    // set index to be empty
+    this.storage[index] = undefined;
+  } else if (Object.keys(this.storage[index]).length > 1) {
+    // else delete key from storage at index
+    // save removed value
+    removed = this.storage[index][key];
+    delete this.storage[index][key];
+  }  
+  // return removed element
+  return removed;
+};
+
+
+const resize = (array, size) => {
+  // if
+};
 
 // YOUR CODE ABOVE
 
