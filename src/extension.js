6bac006f1588f4f40a80fdab2326fa5e75dd45f4
jaimebaik <jaimebaik5@gmail.com>
2020-10-05 10:00:55 -0700

    finished main started extension

--- src/extension.js
+++ src/extension.js
@@ -14,7 +14,100 @@
 */
 
 // PASTE AND MODIFY YOUR CODE BELOW
+function HashTable() {
+  this.SIZE = 16;
+  this.items = 0;
+  this.storage = new Array(this.SIZE);
+}
 
+/**
+* set - Adds given value to the hash table with specified key.
+*
+* - If the provided key has already been used to store another value, simply overwrite
+*   the existing value with the new value.
+* - If the hashed address already contains another key/value pair, you must handle
+*   the collision appropriately.
+*
+* @param {string} key - key to be used to create hashed address
+* @param {string|number|boolean} value - value to be stored in hash table
+* @return {number} The new number of items stored in the hash table
+*/
+HashTable.prototype.set = function(key, value) {
+  //increment the number of items in the hash table
+  this.items++;
+  //check if the number of items in hash table is over 75% of its size
+  if(this.items > Math.floor(this.SIZE*1.75)){
+    this.SIZE = this.SIZE*2;
+    //rehash everything before adding the value
+    //create a new storage
+    let newStorage = new Array(this.SIZE);
+    //copy the values in this storage to new one
+    this.storage.forEach((el)=>{
+      if(el){
+        for(let key in el){
+          let newIndex = hashCode(key, this.SIZE);
+          if(!newStorage[newIndex]) newStorage[newIndex] = {};
+          newStorage[newIndex] = el[key];
+        }
+      }
+    })
+    this.storage = newStorage;
+
+  }
+  //get an index from the hash function provided using the key and size of the buckets
+  let index = hashCode(key, this.SIZE);
+  
+  //check if the buckets already has the key and create an obj to store the key-value pair
+  if(!this.storage[index]) this.storage[index] = {};
+
+  //store the key-value pair in the right index
+  this.storage[index][key] = value;
+  
+};
+
+/**
+* get - Retrieves a value stored in the hash table with a specified key
+*
+* - If more than one value is stored at the key's hashed address, then you must retrieve
+*   the correct value that was originally stored with the provided key
+*
+* @param {string} key - key to lookup in hash table
+* @return {string|number|boolean} The value stored with the specifed key in the
+* hash table
+*/
+HashTable.prototype.get = function(key) {
+  //get an index from the hash function provided using the key and size of the buckets
+  let index = hashCode(key, this.SIZE);
+  //return the value stored at index using key since the key-value pair is stored in obj form
+  return this.storage[index][key];
+};
+
+/**
+* remove - delete a key/value pair from the hash table
+*
+* - If the key does not exist in the hash table, return undefined
+*
+* @param {string} key - key to be found and deleted in hash table
+* @return {string|number|boolean} The value deleted from the hash table
+*/
+HashTable.prototype.remove = function(key) {
+  //get an index from the hash function provided using the key and size of the buckets
+  let index = hashCode(key, this.SIZE);
+  //check if the key exist, 
+  if(!this.storage[index][key])
+    //if not, return undefined
+    return undefined;
+    //otherwise save the value in a new var
+  else{
+    let removed = this.storage[index][key];
+    //remove the entry in the obj at the index
+    delete this.storage[index][key];
+    //decrement the number of items in the hash table
+    this.items--;
+    //return the value using the new store var
+    return removed;
+  }
+};
 
 
 // YOUR CODE ABOVE
@@ -36,3 +129,12 @@ function hashCode(string, size) {
 
 // Do not remove!!
 module.exports = HashTable;
+
+hashT.set("hi", "bye");
+hashT.set("hello", "goodbye");
+hashT.set("welcome", "thanks");
+hashT.set("nice", "you, too");
+console.log(hashT);
+hashT.set("nice", "good to know");
+hashT.set(0, 1000);
+console.log(hashT);
