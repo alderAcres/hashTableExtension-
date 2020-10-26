7cbba733311e4b933f2cdff1ee2fe099411839ed
Ai Mi Bui <aimibui.dev@gmail.com>
2020-10-26 09:56:33 -0400

    add packages and linter

--- src/extension.js
+++ src/extension.js
@@ -15,22 +15,18 @@
 
 // PASTE AND MODIFY YOUR CODE BELOW
 
-
-
 // YOUR CODE ABOVE
 
 function hashCode(string, size) {
-  'use strict';
-  
   let hash = 0;
   if (string.length === 0) return hash;
-  
+
   for (let i = 0; i < string.length; i++) {
     const letter = string.charCodeAt(i);
     hash = ((hash << 5) - hash) + letter;
-    hash = hash & hash; // Convert to 32bit integer
+    hash &= hash; // Convert to 32bit integer
   }
-  
+
   return Math.abs(hash) % size;
 }
 
