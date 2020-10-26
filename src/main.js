90ab70d5a77575d3d3b0b29f7894234ba9ffd6cb
Warren Tait <warrenhtait@gmail.com>
2020-10-26 09:57:22 -0400

    Finished most of assessment.

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@
 */
 function HashTable() {
   this.SIZE = 16;
-  
   this.storage = new Array(this.SIZE);
 }
 
@@ -24,7 +23,13 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
+  const index = hashCode(key, this.SIZE);
+  if (!this.storage[index]) {
+    this.storage[index] = {};
+    this.storage[index][key] = value;
+  } else {
+    this.storage[index][key] = value;
+  }
 };
 
 /**
@@ -38,7 +43,8 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  const index = hashCode(key, this.SIZE);
+  return this.storage[index][key];
 };
 
 /**
@@ -50,7 +56,11 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
-
+  const index = hashCode(key, this.SIZE);
+  if (!this.storage[index][key]) return;
+  const temp = this.storage[index][key];
+  delete this.storage[index];
+  return temp;
 };
 
 
