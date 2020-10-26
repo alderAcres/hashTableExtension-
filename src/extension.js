3e93d5a16f46218760816f66ce4ab8ec004788eb
Sanaya Mirpuri <sanayamirpuri@Sanayas-MacBook-Pro.local>
2020-10-26 09:57:07 -0400

    Main & Extension CS Fundamentals Assessment

--- src/extension.js
+++ src/extension.js
@@ -15,22 +15,73 @@
 
 // PASTE AND MODIFY YOUR CODE BELOW
 
+function HashTable() {
+  this.SIZE = 16;
 
+  this.storage = new Array(this.SIZE);
+}
+
+/**
+ * set - Adds given value to the hash table with specified key.
+ */
+HashTable.prototype.set = function (key, value) {
+  // calculate 75% of size
+  const sizeToPass = 0.75 * this.SIZE;
+  // check how many key/value pairs are in current hashTable, if less than size above, add everything normally, if more than size above double the hash table's size and rehash everything
+
+  const hashed = hashCode(key, this.SIZE);
+  if (this.storage[hashed] === undefined) {
+    this.storage[hashed] = {};
+    this.storage[hashed][key] = value;
+  }
+  this.storage[hashed][key] = value;
+};
+
+/**
+ * get - Retrieves a value stored in the hash table with a specified key
+
+ */
+HashTable.prototype.get = function (key) {
+  // first convert key to an integer using our hashCode function
+  const hashed = hashCode(key, this.SIZE);
+  // return the value at that new hashed key
+  return this.storage[hashed][key];
+};
+
+/**
+ * remove - delete a key/value pair from the hash table
+
+ */
+HashTable.prototype.remove = function (key) {
+  // convert key to an integer using hashCode function
+  const hashed = hashCode(key, this.SIZE);
+  // create a copy of value to delete
+  const copy = this.storage[hashed][key];
+  // delete that specific value
+  delete this.storage[hashed][key];
+  // return
+  return copy;
+};
+
+const test = new HashTable();
+test.set(654, 'apple');
+test.set(333, 'banana');
+console.log(test.get(333));
+test.remove(654);
+console.log(test);
 
 // YOUR CODE ABOVE
 
 function hashCode(string, size) {
-  'use strict';
-  
   let hash = 0;
   if (string.length === 0) return hash;
-  
+
   for (let i = 0; i < string.length; i++) {
     const letter = string.charCodeAt(i);
-    hash = ((hash << 5) - hash) + letter;
+    hash = (hash << 5) - hash + letter;
     hash = hash & hash; // Convert to 32bit integer
   }
-  
+
   return Math.abs(hash) % size;
 }
 
