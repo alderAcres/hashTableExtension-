a0dd2c5a9b622810572cc7b0f46673afca17a287
Edward Cho <edwardcho1231@gmail.com>
2020-10-26 10:13:16 -0400

    formatting

--- src/extension.js
+++ src/extension.js
@@ -20,39 +20,26 @@ function HashTable() {
   this.itemCount = 0;
 }
 
-/**
- * set - Adds given value to the hash table with specified key.
- *
- * - If the provided key has already been used to store another value, simply overwrite
- *   the existing value with the new value.
- * - If the hashed address already contains another key/value pair, you must handle
- *   the collision appropriately.
- *
- * @param {string} key - key to be used to create hashed address
- * @param {string|number|boolean} value - value to be stored in hash table
- * @return {number} The new number of items stored in the hash table
- */
 HashTable.prototype.set = function (key, value) {
-  let capacity = this.storage.reduce((acc,cur)=> {
-    if(Object.keys(cur).length === 0 || undefined){
+  const capacity = this.storage.reduce((acc, cur) => {
+    if (Object.keys(cur).length === 0 || undefined) {
       return acc;
     }
     return acc + 1;
   }, 0);
 
-  if(capacity > this.SIZE * 0.75) {
+  if (capacity > this.SIZE * 0.75) {
     const currentItemCount = this.itemCount;
-    const previousData = this.storage.reduce((acc,cur)=> {
-      return {...acc, ...cur};
+    const previousData = this.storage.reduce((acc, cur) => {
+      return { ...acc, ...cur };
     }, {});
-    this.SIZE = this.SIZE * 2;
+    this.SIZE *= 2;
     this.storage = new Array(this.SIZE);
-    Object.keys(previousData).forEach(e=> {
+    Object.keys(previousData).forEach((e) => {
       this.set(e, previousData[e]);
-    })
+    });
     this.itemCount = currentItemCount;
   }
-
 
   // get index by calling hashCode with key;
   const binNumber = hashCode(key, this.SIZE);
@@ -66,23 +53,12 @@ HashTable.prototype.set = function (key, value) {
     this.itemCount += 1;
     return this.itemCount;
     // if there is data already in storage at binNumber, combine current data with new obj and storage as one object to reduce collision
-  } else {
-    this.storage[binNumber][key] = value;
-    this.itemCount += 1;
-    return this.itemCount;
   }
+  this.storage[binNumber][key] = value;
+  this.itemCount += 1;
+  return this.itemCount;
 };
 
-/**
- * get - Retrieves a value stored in the hash table with a specified key
- *
- * - If more than one value is stored at the key's hashed address, then you must retrieve
- *   the correct value that was originally stored with the provided key
- *
- * @param {string} key - key to lookup in hash table
- * @return {string|number|boolean} The value stored with the specifed key in the
- * hash table
- */
 HashTable.prototype.get = function (key) {
   // run hashCode to find out where to look for key in hash table
   const binNumber = hashCode(key, this.SIZE);
@@ -92,14 +68,6 @@ HashTable.prototype.get = function (key) {
   }
 };
 
-/**
- * remove - delete a key/value pair from the hash table
- *
- * - If the key does not exist in the hash table, return undefined
- *
- * @param {string} key - key to be found and deleted in hash table
- * @return {string|number|boolean} The value deleted from the hash table
- */
 HashTable.prototype.remove = function (key) {
   // run hashCode to find out where to look for key in hash table
   const binNumber = hashCode(key, this.SIZE);
