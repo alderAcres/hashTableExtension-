34baa2315a0fc315a3d0c731a92c15d1d31f6dab
Chelsea Harris <chelseaharris@Chelseas-MacBook-Pro.local>
2020-10-26 09:56:16 -0400

    assessment 2 upload

--- src/extension.js
+++ src/extension.js
@@ -14,7 +14,57 @@
 */
 
 // PASTE AND MODIFY YOUR CODE BELOW
+// Table builder function
+function HashTable() {
+  this.SIZE = 16;
+  this.storage = new Array(this.SIZE);
+}
 
+//Function that retrieves a value from a bucket
+HashTable.prototype.get = function(key) {
+  const retrieveCode = hashCode(key, this.size);
+    if(!this.storage[hashCode]) {
+      return "no value exists";
+    }
+  return this.storage[hashCode][key];
+  };
+
+//Function that sets a value in a bucket
+HashTable.prototype.set = function(key, value) {
+// if 12 of the buckets are filled
+//if this.storage.length = 12?
+//this.SIZE = this.SIZE * 2
+const storageCheck = this.storage;
+  if(storageCheck.length === 12) {
+      this.SIZE = this.SIZE * 2;
+      const hashCodeIfFull = hashCode(key, this.size);
+      this.storage[hashCodeIfFull][key] = value;
+  } else {
+    const hashCode = hashCode(key, this.size);
+    if(!this.storage[hashCode]) {
+      this.storage[hashCode] = {};
+    }
+    this.storage[hashCode][key] = value;
+  }
+};
+
+//Function that removes a key/val pair from a bucket
+HashTable.prototype.remove = function(key) {
+  //if hash table size is greater than 16
+  const storageCheck2 = this.storage;
+  if((storageCheck2.length/this.SIZE) * 100 === 25) {
+    this.SIZE = this.SIZE/2;
+  }
+  //if current size is 25%
+  //reduce SIZE by 1/2
+  const removalCode = hashCode(key, this.size);
+    if(!this.storage[removalCode][key]) {
+      return undefined;
+    }
+  const deletedPair = this.storage[removalCode][key];
+  delete this.storage[removalCode][key];
+  return deletedPair;
+  };
 
 
 // YOUR CODE ABOVE
