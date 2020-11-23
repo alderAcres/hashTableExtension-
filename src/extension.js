393542f79b7fa7dc8add0a47971f55edbc0a7f7a
Akshay Suggula <akshaysuggula@akshays-mbp.lan>
2020-11-23 09:54:53 -0800

    submitting cs fundamentals assessment

--- src/extension.js
+++ src/extension.js
@@ -15,6 +15,91 @@
 
 // PASTE AND MODIFY YOUR CODE BELOW
 
+HashTable.prototype.set = function(key, value) {
+  //take in the key and convert it into a hash value 
+  let hashValue = hashCode(key); 
+  //declare storage size variable and set it equal to 0
+  let storageSize = 0; 
+  //declare new storage variable
+  let newStorage;  
+  
+  //NORMALLY ADD THE ELEMENT 
+  this.set(key, value); 
+
+  //check how much of the hashtable has been filled out 
+  for(let i = 0; i < this.storage.length; i++){
+    if(this.storage[i]){
+      storageSize++; 
+    }
+  }
+
+  //if > 75% has been filled out then double the size and rehash every item
+  if(storageSize / this.SIZE > 0.75){
+    //double size
+    this.SIZE = this.SIZE * 2; 
+    newStorage = new Array(this.SIZE); 
+    
+    //rehash every single item 
+    for(let i = 0; i < this.storage.length; i++){
+      for(let j = 0; j < this.storage[i].length; j++){
+        let hashValue = hashCode(key); 
+        if(!newStorage[hashValue]){
+          newStorage[hashValue] = []; 
+          newStorage[hashValue].push([key, value]); 
+        } else {
+          newStorage[hashValue].push([key, value]); 
+        }
+      }
+    }
+
+    this.storage = newStorage; 
+  }
+   
+   
+};
+
+
+HashTable.prototype.remove = function(key) {
+  //take in the key and convert it into a hash value 
+  let hashValue = hashCode(key); 
+  //declare storage size variable and set it equal to 0
+  let storageSize = 0; 
+  //declare new storage variable
+  let newStorage;  
+  
+  //check how much of the hashtable has been filled out 
+  for(let i = 0; i < this.storage.length; i++){
+    if(this.storage[i]){
+      storageSize++; 
+    }
+  }
+
+  this.remove(key); 
+
+  //after removing the value, if the population of the items is less than 25% of the SIZE variable, then resize and rehash the storage array
+  if(storageSize / this.SIZE < 0.25){
+    //double size
+    this.SIZE = this.SIZE / 2; 
+    newStorage = new Array(this.SIZE); 
+    
+    //rehash every single item 
+    for(let i = 0; i < this.storage.length; i++){
+      for(let j = 0; j < this.storage[i].length; j++){
+        let hashValue = hashCode(key); 
+        if(!newStorage[hashValue]){
+          newStorage[hashValue] = []; 
+          newStorage[hashValue].push([key, value]); 
+        } else {
+          newStorage[hashValue].push([key, value]); 
+        }
+      }
+    }
+
+    this.storage = newStorage; 
+  }  
+};
+
+
 
 
 // YOUR CODE ABOVE
