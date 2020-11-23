4576affeb4e202ff88e8b9dc293b5ccb28f14249
Jenna Thomas <jlthoma4@Jennas-MBP-2.attlocal.net>
2020-11-23 13:27:02 -0500

    src/main.js

--- src/main.js
+++ src/main.js
@@ -7,12 +7,22 @@
 */
 function HashTable() {
   this.SIZE = 16;
-  
   this.storage = new Array(this.SIZE);
 }
 
+// implement a hash function
+function hashString(input) {
+  // assign hash to 0
+  let hash = 0;
+  // iterate through the input length
+  for (let i = 0; i < input.length; i++) {
+    // return hash
+    return hash;
+  }
+}
+
 /**
-* set - Adds given value to the hash table with specified key.
+* set - Adds given value dhto the hash table with specified key.
 *
 * - If the provided key has already been used to store another value, simply overwrite
 *   the existing value with the new value.
@@ -24,7 +34,16 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
+// create a const index and assign it to the value of passing in key inyot te hash func
+const index = hash(key);
+// create a cond to determine if index at this.sorage exist,
+if(!this.storage[index]){
+  // if so, assign it to an empty array
+  this.storage[index] = []
+}
+// else push the passed in key value pairs onto the storage arr at the index
+this.storage[index].push(key, value);
+) return index
 };
 
 /**
@@ -38,7 +57,18 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+// create a var index and assign it to the value of passing key into the hash func
+const index = hash(key)
+// create a conditional to determine if index at this.sotrage exist, if so return null
+if(!this.storage[index]) return null
+// iterate thtouth this.storage at the index
+for (let storage of this.storage[index]) {
+  // create a conditional to determine if the first el in storage is equal to key
+  if(storage [0] === key) {
+    // if so return storage at the 2nd el (1 index)
+    return storage [1]
+  }
+}
 };
 
 /**
@@ -50,6 +80,16 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
+// create a var value and assign it to this.hash with the key passed in
+let value = this.hash(key);
+// declare a var hashkey and assign it to 
+  let hashKey = hash(key, this.SIZE);
+  // if value exist, delete this.storage at the key index
+  if (value) {
+  delete this.storage[key];
+  }
+  // return value
+  return value;
 
 };
 
