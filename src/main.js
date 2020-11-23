2500719d3474a5da71f01fb5f52fbf109eb60082
Windu Sayles <windusayles@Windus-iMac.local>
2020-11-23 10:11:57 -0800

    main looks good, no time for extension

--- src/main.js
+++ src/main.js
@@ -7,9 +7,10 @@
 */
 function HashTable() {
   this.SIZE = 16;
-  
+  //add a hashtotal to keep track of our length without iterating
+  this.hashtotal = 0;
   this.storage = new Array(this.SIZE);
-}
+};
 
 /**
 * set - Adds given value to the hash table with specified key.
@@ -24,7 +25,19 @@ function HashTable() {
 * @return {number} The new number of items stored in the hash table
 */
 HashTable.prototype.set = function(key, value) {
-
+  //we need to place the key and value into a bucket determined by its hashcode
+  //---reminder, hashcode is a digit that will represent the index of our hashtable---//
+  const currenthash = hashCode(key, this.SIZE);
+  //if our hash location is empty, create an empty object to set our item in, so we can look up by keys
+  if (!this[currenthash]) this[currenthash] = {};
+  //if our key exists, overwrite value rather than add new one
+    //otherwise add this k/v pair to our bucket (same code for either case)
+  this[currenthash][key] = value;
+  
+  //increment the local total
+  this.hashtotal += 1;
+  //return the updated number for hashtotal
+  return this.hashtotal;
 };
 
 /**
@@ -38,7 +51,13 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  //determine hashcode
+  const currenthash = hashCode(key, this.SIZE); 
+  //if bucket is empty, return undefined
+    //or if key is missing from bucket, return undefined
+  if (!this[currenthash] || !this[currenthash][key]) return undefined;
+  //look up and return value by the key we're given
+  return this[currenthash][key];
 };
 
 /**
@@ -50,7 +69,14 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
-
+  //establish current hashcode/index
+  const currenthash = hashCode(key, this.SIZE);
+  //if bucket is empty, or k/v pair is missing, return undefined
+  if (!this[currenthash] || !this[currenthash][key]) return undefined;
+//**do we need to return anything otherwise, or just delete?**//
+//** if we're returning the deleted entry, I would save the value, first, then return it after deletion**/
+  //remove key/value of pair passed in
+  delete this[currenthash][key];
 };
 
 
@@ -69,6 +95,16 @@ function hashCode(string, size) {
   
   return Math.abs(hash) % size;
 }
+
+//let's test!
+//create a hashtable
+const testTable = new HashTable();
+//add an entry
+testTable.set(5, 'test')
+console.log(testTable, testTable.get(5))
+//remove it and check
+testTable.remove(5)
+console.log(testTable, testTable.get(5))
 
 // Do not remove!!
 module.exports = HashTable;
