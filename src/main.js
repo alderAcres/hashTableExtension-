96f2f45aafe2131dc9610b899786b844bfca27f6
Matthew Marchand <m-marchand@Matthews-Air.lan1>
2020-11-23 10:01:12 -0800

    complete cs fundamentals assessment

--- src/main.js
+++ src/main.js
@@ -23,9 +23,30 @@ function HashTable() {
 * @param {string|number|boolean} value - value to be stored in hash table
 * @return {number} The new number of items stored in the hash table
 */
-HashTable.prototype.set = function(key, value) {
 
-};
+//pass the key to the hash function to get a hashed version of input. Insert
+//the provided value into the storage array at the index represented by this hash code.
+//collisions: If multiple values result in same hash code, account for this by saving 
+// values in an object like so - { key: value }
+
+HashTable.prototype.set = function(key, value) {
+  let hash = hashCode(key, this.SIZE);
+  //check if there is already a value stored at the current hash code
+  //if not, initialize that index of storage to an empty object
+  if (!this.storage[hash]) {
+    this.storage[hash] = {};
+    //store the value associated with that code in the object
+    this.storage[hash][key] = value;
+  }
+  //if there is already a value at that hash code, simply store it's value in the object
+  else this.storage[hash][key] = value;
+}
+
+let hashTable = new HashTable();
+hashTable.set('hello', true);
+hashTable.set(4, false)
+hashTable.set('gross', false)
+console.log(hashTable.storage) 
 
 /**
 * get - Retrieves a value stored in the hash table with a specified key
@@ -38,8 +59,14 @@ HashTable.prototype.set = function(key, value) {
 * hash table
 */
 HashTable.prototype.get = function(key) {
-
+  let hash = hashCode(key, this.SIZE);
+  //each key is an object containing passed values and their hash codes
+  //acces the index of storage with hash, and access value of hash key in object, return 
+  return this.storage[hash][key];
 };
+
+console.log(hashTable.get('hello'))
+console.log(hashTable)
 
 /**
 * remove - delete a key/value pair from the hash table
@@ -50,8 +77,15 @@ HashTable.prototype.get = function(key) {
 * @return {string|number|boolean} The value deleted from the hash table
 */
 HashTable.prototype.remove = function(key) {
-
+  let hash = hashCode(key, this.SIZE);
+  //remove the entry stored at hash code 
+  delete this.storage[hash];
 };
+
+console.log(hashTable.remove(4))
+console.log(hashTable)
+
+
 
 
 // Do not modify
