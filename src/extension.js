39ab6eb913b7fa2a9bbe44d0ff2f83294241a397
Shelby Cotton <hello@shelbycotton.com>
2020-12-14 08:10:20 -0700

    Completed assessment

--- src/extension.js
+++ src/extension.js
@@ -14,7 +14,79 @@
 */
 
 // PASTE AND MODIFY YOUR CODE BELOW
+function HashTable() {
+  this.SIZE = 16;
+  
+  this.storage = new Array(this.SIZE);
+  this.numItems = 0;
+}
 
+HashTable.prototype.set = function(key, value) {
+  const bucket = hashCode(key, this.SIZE);
+  let isNewKey = true;
+
+  if (this.storage[bucket]) {
+    if (this.storage[bucket][key] !== undefined) {
+      isNewKey = false;
+    }
+    this.storage[bucket][key] = value;
+  } else {
+    this.storage[bucket] = {
+      [key]: value
+    };
+  }
+
+  if (isNewKey) this.numItems++;
+
+  if (this.numItems > this.SIZE * 0.75) {
+    this.expandTable();
+  }
+
+  return this.numItems;
+};
+
+HashTable.prototype.remove = function(key) {
+  const bucket = hashCode(key, this.SIZE);
+  if (!this.storage[bucket] || this.storage[bucket][key] === undefined) return;
+
+  const valueToDelete = this.storage[bucket][key];
+  delete this.storage[bucket][key];
+  this.numItems--;
+
+  if (this.SIZE > 16 && numItems < Math.floor(this.SIZE * 0.25)) {
+    this.reduceTable();
+  }
+
+  return valueToDelete;
+};
+
+// didn't finish
+HashTable.prototype.expandTable = function() {
+  const oldSize = this.SIZE;
+  this.SIZE *= 2;
+
+  for (let i = oldSize - 1; i >= 0; i--) {
+    const bucket = this.storage[i];
+
+    Object.entries.forEach
+  }
+}
+
+HashTable.prototype.reduceTable = function() {
+  const newSize = this.SIZE / 2;
+
+  this.storage.forEach(bucket => {
+    if (bucket) {
+      Object.entries(bucket).forEach(([key, value]) => {
+        const newBucket = hashCode(key, newSize);
+        this.storage[newBucket][key] = value;
+        delete this.storage[bucket][key]; // don't know if this will work with bucket
+      });
+    }
+  });
+
+  this.SIZE = newSize;
+}
 
 
 // YOUR CODE ABOVE
@@ -36,3 +108,22 @@ function hashCode(string, size) {
 
 // Do not remove!!
 module.exports = HashTable;
+
+const myTable = new HashTable();
+myTable.set('a', '1');
+myTable.set('b', '1');
+myTable.set('c', '1');
+myTable.set('d', '1');
+myTable.set('e', '1');
+myTable.set('f', '1');
+myTable.set('g', '1');
+myTable.set('h', '1');
+myTable.set('i', '1');
+myTable.set('j', '1');
+myTable.set('k', '1');
+myTable.set('l', '1');
+myTable.set('m', '1');
+myTable.set('n', '1');
+myTable.set('o', '1');
+
+console.log(myTable.numItems);
